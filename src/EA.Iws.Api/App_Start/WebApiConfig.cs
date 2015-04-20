289125666da7d2b47e9d0efe8cf000ab3e09e606
David Raine <DavidR@sfwltd.co.uk>
2015-04-20 17:49:28 +0100

    Initial project structure and nuget packages.

---
+++ src/EA.Iws.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace EA.Iws.Api
+{
+    using System.Web.Http;
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+        }
+    }
+}
