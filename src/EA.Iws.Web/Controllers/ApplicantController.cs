b480e9c9a217622b965b06376e07db482f260194
Chirag Dani <ChiragD@sfwltd.co.uk>
2015-04-24 09:29:59 +0100

    Created Applicant's home page with link to create a new notification

---
+++ src/EA.Iws.Web/Controllers/ApplicantController.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Iws.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class ApplicantController : Controller
+    {
+        // GET: Applicant
+        public async Task<ActionResult> Home()
+        {
+            return View();
+        }
+    }
+}
