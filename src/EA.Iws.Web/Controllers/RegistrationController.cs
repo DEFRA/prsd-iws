2e292dc5f9497027a702d9ff625839ed2c16a30e
Michael Murray <MichaelM@sfwltd.co.uk>
2015-04-28 16:46:56 +0100

    User now logged in after registration

--- src/EA.Iws.Web/Controllers/RegistrationController.cs
+++ src/EA.Iws.Web/Controllers/RegistrationController.cs
@@ -3,21 +3,29 @@
     using System;
     using System.Collections.Generic;
     using System.Net.Http;
+    using System.Security.Claims;
     using System.Threading.Tasks;
     using System.Web.Mvc;
     using Api.Client;
     using Api.Client.Entities;
     using Infrastructure;
+    using Microsoft.Owin.Security;
     using Services;
+    using Thinktecture.IdentityModel.Client;
     using ViewModels.Registration;
+    using TokenResponse = Api.Client.Entities.TokenResponse;
 
     public class RegistrationController : Controller
     {
         private readonly AppConfiguration config;
+        private readonly IAuthenticationManager authenticationManager;
+        private readonly Func<IwsOAuthClient> oauthClient;
 
-        public RegistrationController(AppConfiguration config)
+        public RegistrationController(AppConfiguration config, Func<IwsOAuthClient> oauthClient, IAuthenticationManager authenticationManager)
         {
             this.config = config;
+            this.oauthClient = oauthClient;
+            this.authenticationManager = authenticationManager;
         }
 
         public ActionResult ApplicantRegistration()
@@ -45,12 +53,17 @@
                         ConfirmPassword = model.ConfirmPassword
                     };
 
-                    response =
-                        await client.Registration.RegisterApplicantAsync(applicantRegistrationData);
+                    response = await client.Registration.RegisterApplicantAsync(applicantRegistrationData);
                 }
 
                 if (response.IsSuccessStatusCode)
                 {
+                    //Registration is successful so sign the user in
+                    var client = oauthClient();
+                    var signInResponse = await client.GetAccessTokenAsync(model.Email, model.Password);
+                    var identity = GenerateUserIdentity(signInResponse);
+                    authenticationManager.SignIn(new AuthenticationProperties(), identity);
+
                     return RedirectToAction("SelectOrganisation", new { organisationName = model.OrganisationName });
                 }
             }
@@ -132,6 +145,19 @@
             }
 
             return View("CreateNewOrganisation", model);
+        }
+
+        private static ClaimsIdentity GenerateUserIdentity(TokenResponse response)
+        {
+            var identity = new ClaimsIdentity(Constants.IwsAuthType);
+            identity.AddClaim(new Claim(OAuth2Constants.AccessToken, response.AccessToken));
+            if (response.IdentityToken != null)
+            {
+                identity.AddClaim(new Claim(OAuth2Constants.IdentityToken, response.IdentityToken));
+            }
+            identity.AddClaim(new Claim(IwsClaimTypes.ExpiresAt,
+                DateTimeOffset.Now.AddSeconds(response.ExpiresIn).ToString()));
+            return identity;
         }
     }
 }
