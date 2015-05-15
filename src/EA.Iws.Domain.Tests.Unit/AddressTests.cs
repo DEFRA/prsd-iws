9745ac0d9b5bf32d80cb12ea1e43bb955d566c74
Charlie Lusty <CharlesL@sfwltd.co.uk>
2015-05-15 14:02:06 +0100

    Refactored Address unit tests to separate test class

---
+++ src/EA.Iws.Domain.Tests.Unit/AddressTests.cs
@@ -0,0 +1,23 @@
+﻿namespace EA.Iws.Domain.Tests.Unit
+{
+    using Xunit;
+
+    public class AddressTests
+    {
+        [Fact]
+        public void IsUkAddress()
+        {
+            var addressUk = new Address(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, "United Kingdom");
+
+            Assert.True(addressUk.IsUkAddress);
+        }
+
+        [Fact]
+        public void IsNotUkAddress()
+        {
+            var addressNonUk = new Address(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, "Thailand");
+
+            Assert.False(addressNonUk.IsUkAddress);
+        }
+    }
+}
