0f1fc8fde14be2cfa98329681a89e15a58156d61
Michael Murray <MichaelM@sfwltd.co.uk>
2015-05-22 14:16:49 +0100

    add claims for users with organisations

---
+++ src/EA.Iws.Database/scripts/Update/Sprint8/20150522-123533-Add-OrganisationId-Claims-To-ApplicationUsers.sql
@@ -0,0 +1,34 @@
+﻿DECLARE @id uniqueidentifier
+DECLARE @orgid uniqueidentifier
+DECLARE @getid CURSOR
+
+SET @getid = CURSOR FOR
+SELECT
+  u.Id,
+  u.OrganisationId
+FROM [Identity].AspNetUsers u
+LEFT OUTER JOIN [Identity].AspNetUserClaims c
+  ON c.UserId = u.Id
+WHERE NOT EXISTS (SELECT
+  *
+FROM [Identity].AspNetUserClaims
+WHERE userid = u.Id
+AND ClaimType = 'organisation_id')
+
+OPEN @getid
+FETCH NEXT
+FROM @getid INTO @id, @orgid
+WHILE @@FETCH_STATUS = 0
+BEGIN
+  IF @orgid IS NOT NULL
+    INSERT INTO [Identity].[AspNetUserClaims] ([UserId]
+    , [ClaimType]
+    , [ClaimValue])
+      VALUES (@id, 'organisation_id', convert(nvarchar(50), @orgid))
+  FETCH NEXT
+  FROM @getid INTO @id, @orgid
+
+END
+
+CLOSE @getid
+DEALLOCATE @getid
