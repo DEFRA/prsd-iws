bf172c15d83645f48fe9d70f776844d067a7ebfc
Michael Murray <MichaelM@sfwltd.co.uk>
2015-05-29 12:42:07 +0100

    Added database script

--- src/EA.Iws.Web/ViewModels/Shared/CheckBoxCollectionViewModel.cs
+++ src/EA.Iws.Web/ViewModels/Shared/CheckBoxCollectionViewModel.cs
@@ -13,11 +13,8 @@
         public IList<SelectListItem> PossibleValues { get; set; }
 
         /// <summary>
-        /// Creates a radio button string collection based on values in an enum.
+        /// Creates SelectListItem collection based on values in an enum.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="selectedValue"></param>
-        /// <returns></returns>
         public static CheckBoxCollectionViewModel CreateFromEnum<T>(string selectedValue = null)
         {
             if (!(typeof(Enum).IsAssignableFrom(typeof(T))))
@@ -31,18 +28,11 @@
 
             foreach (var field in fields)
             {
-                bool selectThisValue = selectedValue != null && selectedValue.Equals(field.Name, StringComparison.InvariantCultureIgnoreCase);
-
                 // Get the display attributes for the enum.
                 var displayAttribute = (DisplayAttribute)field.GetCustomAttributes(typeof(DisplayAttribute)).SingleOrDefault();
 
                 // Set field name to either the enum name or the display name.
                 var name = (displayAttribute == null) ? field.Name : displayAttribute.Name;
-
-                if (selectThisValue)
-                {
-                    selectedValue = name;
-                }
 
                 fieldNames.Add(new SelectListItem{Text = name, Value = ((int)field.GetValue(null)).ToString() });
             }
