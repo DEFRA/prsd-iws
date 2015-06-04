1882cf1a3b80a1ca2e48ca17aa1da564c3751542
Michael Murray <MichaelM@sfwltd.co.uk>
2015-06-04 14:07:06 +0100

    Added validation attribute

---
+++ src/EA.Prsd.Core/Validation/RequiredIfPropertiesEqualAttribute.cs
@@ -0,0 +1,39 @@
+﻿namespace EA.Prsd.Core.Validation
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class RequiredIfPropertiesEqualAttribute : ValidationAttribute
+    {
+        private String DependantPropertyName { get; set; }
+        private new String ErrorMessage { get; set; }
+        private String PropertyName { get; set; }
+
+        public RequiredIfPropertiesEqualAttribute(String dependentPropertyName, String propertyName, String errorMessage)
+        {
+            DependantPropertyName = dependentPropertyName;
+            PropertyName = propertyName;
+            ErrorMessage = errorMessage;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            var instance = context.ObjectInstance;
+            var type = instance.GetType();
+
+            var dependantPropertyValue = type.GetProperty(DependantPropertyName).GetValue(instance, null);
+            var propertyValue = type.GetProperty(PropertyName).GetValue(instance, null);
+            var contextPropertyValue = type.GetProperty(context.MemberName).GetValue(instance, null);
+
+            if (dependantPropertyValue != null && (dependantPropertyValue.ToString().Equals(propertyValue.ToString())))
+            {
+                if (contextPropertyValue == null || string.IsNullOrEmpty(contextPropertyValue.ToString()))
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
