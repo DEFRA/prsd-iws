f87aaa1a6405595e413272877e62bd5c2044cf8c
Eliot Jones <eliotj@sfwltd.co.uk>
2015-06-17 09:55:51 +0100

    Rename CQRS test project, disable test parallelization in the web tests project.

--- src/EA.Iws.RequestHandlers.Tests.Unit/Properties/AssemblyInfo.cs
+++ src/EA.Iws.RequestHandlers.Tests.Unit/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("EA.Iws.Cqrs.Tests.Unit")]
+[assembly: AssemblyTitle("EA.Iws.RequestHandlers.Tests.Unit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EA.Iws.Cqrs.Tests.Unit")]
+[assembly: AssemblyProduct("EA.Iws.RequestHandlers.Tests.Unit")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
