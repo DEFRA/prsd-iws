55a604358ce7bd91f8014a58dca923efdb1b7db8
Michael Murray <MichaelM@sfwltd.co.uk>
2015-06-29 16:42:36 +0100

    Added missing files

---
+++ src/EA.Iws.Requests/WasteType/SetEnergyAndOptionalInformation.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Iws.Requests.WasteType
+{
+    using System;
+    using Prsd.Core.Mediator;
+
+    public class SetEnergyAndOptionalInformation : IRequest<Guid>
+    {
+        public SetEnergyAndOptionalInformation(string energyInformation, string optionalInformation, Guid notificationId)
+        {
+            EnergyInformation = energyInformation;
+            NotificationId = notificationId;
+            OptionalInformation = optionalInformation;
+        }
+
+        public string EnergyInformation { get; private set; }
+
+        public string OptionalInformation { get; private set; }
+
+        public Guid NotificationId { get; private set; }
+    }
+}
---
+++ src/EA.Iws.Requests/WasteType/SetWoodTypeDescription.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Iws.Requests.WasteType
+{
+    using System;
+    using Prsd.Core.Mediator;
+
+    public class SetWoodTypeDescription : IRequest<Guid>
+    {
+        public SetWoodTypeDescription(string woodTypeDescription, Guid notificationId)
+        {
+            WoodTypeDescription = woodTypeDescription;
+            NotificationId = notificationId;
+        }
+
+        public string WoodTypeDescription { get; private set; }
+
+        public Guid NotificationId { get; private set; }
+    }
+}
