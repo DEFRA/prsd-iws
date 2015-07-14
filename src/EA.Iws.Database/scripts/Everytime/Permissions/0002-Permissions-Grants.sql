3b4b8bcdf2f24dce865025e33cf7df63934b0ab4
David Raine <DavidR@sfwltd.co.uk>
2015-07-14 12:00:37 +0100

    Add db permission scripts

---
+++ src/EA.Iws.Database/scripts/Everytime/Permissions/0002-Permissions-Grants.sql
@@ -0,0 +1,9 @@
+﻿GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::[Business] TO [iws_application]
+GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::[Notification] TO [iws_application]
+GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::[Identity] TO [iws_application]
+
+GRANT SELECT, INSERT ON SCHEMA::[Auditing] TO [iws_application]
+
+GRANT SELECT ON SCHEMA::[Lookup] TO [iws_application]
+
+GO
