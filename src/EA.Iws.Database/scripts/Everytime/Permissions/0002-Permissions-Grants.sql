c5662f9c7146f093dc16a20be4b6c33dc2e685cb
David Raine <DavidR@sfwltd.co.uk>
2015-07-15 09:07:53 +0100

    Temporarily comment out failing db script

--- src/EA.Iws.Database/scripts/Everytime/Permissions/0002-Permissions-Grants.sql
+++ src/EA.Iws.Database/scripts/Everytime/Permissions/0002-Permissions-Grants.sql
@@ -1,9 +1,9 @@
-﻿GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::[Business] TO [iws_application]
-GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::[Notification] TO [iws_application]
-GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::[Identity] TO [iws_application]
+﻿--GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::[Business] TO [iws_application]
+--GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::[Notification] TO [iws_application]
+--GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::[Identity] TO [iws_application]
 
-GRANT SELECT, INSERT ON SCHEMA::[Auditing] TO [iws_application]
+--GRANT SELECT, INSERT ON SCHEMA::[Auditing] TO [iws_application]
 
-GRANT SELECT ON SCHEMA::[Lookup] TO [iws_application]
+--GRANT SELECT ON SCHEMA::[Lookup] TO [iws_application]
 
-GO
+--GO
