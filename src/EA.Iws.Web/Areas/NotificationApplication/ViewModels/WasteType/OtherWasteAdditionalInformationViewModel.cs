1c693647f8716eb6044f46ac66877afaf9bd53af
Michael Murray <MichaelM@sfwltd.co.uk>
2015-07-15 17:14:37 +0100

    changed required to required if

--- src/EA.Iws.Web/Areas/NotificationApplication/ViewModels/WasteType/OtherWasteAdditionalInformationViewModel.cs
+++ src/EA.Iws.Web/Areas/NotificationApplication/ViewModels/WasteType/OtherWasteAdditionalInformationViewModel.cs
@@ -1,13 +1,13 @@
 ﻿namespace EA.Iws.Web.Areas.NotificationApplication.ViewModels.WasteType
 {
     using System;
-    using System.ComponentModel.DataAnnotations;
+    using Prsd.Core.Validation;
 
     public class OtherWasteAdditionalInformationViewModel
     {
         public Guid NotificationId { get; set; }
 
-        [Required]
+        [RequiredIf("HasAttachement", false, "Please enter a description or check the box")]
         public string Description { get; set; }
 
         public bool HasAttachement { get; set; }
