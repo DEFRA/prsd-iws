05f476fa651617f1d9e29b4d28f066c97ca4403f
Charlie Lusty <CharlesL@sfwltd.co.uk>
2015-07-28 08:16:49 +0100

    Remove waste type from search on EA home page

--- src/EA.Iws.RequestHandlers/Admin/Search/GetBasicSearchResultsHandler.cs
+++ src/EA.Iws.RequestHandlers/Admin/Search/GetBasicSearchResultsHandler.cs
@@ -8,7 +8,6 @@
     using Core.Admin.Search;
     using Core.WasteType;
     using DataAccess;
-    using Prsd.Core.Helpers;
     using Prsd.Core.Mediator;
     using Requests.Admin;
 
@@ -23,34 +22,10 @@
 
         public async Task<IList<BasicSearchResult>> HandleAsync(GetBasicSearchResults query)
         {
-            int wasteTypeQueryTerm;
-
-            if (ChemicalCompositionType.RDF.ToString().Equals(query.SearchTerm, StringComparison.InvariantCultureIgnoreCase))
-            {
-                wasteTypeQueryTerm = (int)ChemicalCompositionType.RDF;
-            }
-            else if (ChemicalCompositionType.SRF.ToString().Equals(query.SearchTerm, StringComparison.InvariantCultureIgnoreCase))
-            {
-                wasteTypeQueryTerm = (int)ChemicalCompositionType.SRF;
-            }
-            else if (ChemicalCompositionType.Wood.ToString().Equals(query.SearchTerm, StringComparison.InvariantCultureIgnoreCase))
-            {
-                wasteTypeQueryTerm = (int)ChemicalCompositionType.Wood;
-            }
-            else if (ChemicalCompositionType.Other.ToString().Equals(query.SearchTerm, StringComparison.InvariantCultureIgnoreCase))
-            {
-                wasteTypeQueryTerm = (int)ChemicalCompositionType.Other;
-            }
-            else
-            {
-                wasteTypeQueryTerm = EnumHelper.GetValues(typeof(ChemicalCompositionType)).Count + 1;
-            }
-
             var results = await(from n in context.NotificationApplications
                                 where (n.NotificationNumber.Contains(query.SearchTerm) ||
                                        n.NotificationNumber.Replace(" ", string.Empty).Contains(query.SearchTerm) ||
-                                       n.Exporter.Business.Name.Contains(query.SearchTerm) ||
-                                       n.WasteType.ChemicalCompositionType.Value == wasteTypeQueryTerm)
+                                       n.Exporter.Business.Name.Contains(query.SearchTerm))
                                     select new { n.Id, n.NotificationNumber, ExporterName = n.Exporter.Business.Name, WasteType = (int?)n.WasteType.ChemicalCompositionType.Value }).ToListAsync();
             return results.Select(r => ConvertToSearchResults(r.Id, r.NotificationNumber, r.ExporterName, r.WasteType)).ToList();
         }
