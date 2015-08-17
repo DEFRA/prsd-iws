0d7033941395e2c21ea90b8adfc08e5cfd1ccc4f
Michael Murray <MichaelM@sfwltd.co.uk>
2015-08-17 09:45:55 +0100

    add new y codes screen

---
+++ src/EA.Iws.Web/Areas/NotificationApplication/Controllers/YCodesController.cs
@@ -0,0 +1,155 @@
+﻿namespace EA.Iws.Web.Areas.NotificationApplication.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Api.Client;
+    using Core.WasteCodes;
+    using Infrastructure;
+    using Prsd.Core.Web.ApiClient;
+    using Prsd.Core.Web.Mvc.Extensions;
+    using Requests.WasteCodes;
+    using ViewModels.WasteCodes;
+
+    [Authorize]
+    public class YCodesController : Controller
+    {
+        private readonly Func<IIwsClient> apiClient;
+
+        public YCodesController(Func<IIwsClient> apiClient)
+        {
+            this.apiClient = apiClient;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EnterYCodes(Guid id)
+        {
+            var model = new YCodesViewModel
+            {
+                NotificationId = id
+            };
+
+            using (var client = apiClient())
+            {
+                model.Ycodes = await client.SendAsync(User.GetAccessToken(), new GetWasteCodesByType(CodeType.Y));
+                model.SelectedYcodesList = new List<WasteCodeData>(await client.SendAsync(User.GetAccessToken(), new GetWasteCodesForNotification(model.NotificationId, CodeType.Y)));
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EnterYCodes(YCodesViewModel model, string command, string remove)
+        {
+            if (remove != null)
+            {
+                await InitializeYcodeHcodeAndUnClassViewModel(model);
+                ModelState.Clear();
+
+                if (model.SelectedYcodesList.Any(c => c.Id.ToString() == remove))
+                {
+                    model.SelectedYcodesList.RemoveAll(c => c.Id.ToString() == remove);
+                }
+
+                return View(model);
+            }
+
+            if (command.Equals("addYcode"))
+            {
+                await InitializeYcodeHcodeAndUnClassViewModel(model);
+                WasteCodeData codeToAdd;
+
+                try
+                {
+                    codeToAdd = model.Ycodes.Single(c => c.Id.ToString() == model.SelectedYcode);
+                }
+                catch (Exception)
+                {
+                    return View(model);
+                }
+
+                if (codeToAdd.Code.Equals("Not applicable"))
+                {
+                    model.SelectedYcodesList.Clear();
+                    model.SelectedYcodesList.Add(codeToAdd);
+                    return View(model);
+                }
+
+                if (model.SelectedYcodesList == null)
+                {
+                    model.SelectedYcodesList = new List<WasteCodeData>();
+                }
+
+                if (model.SelectedYcodesList.All(c => c.Id != codeToAdd.Id))
+                {
+                    if (model.SelectedYcodesList.Any(c => c.Code == "Not applicable"))
+                    {
+                        model.SelectedYcodesList.RemoveAll(c => c.Code.Equals("Not applicable"));
+                    }
+
+                    model.SelectedYcodesList.Add(codeToAdd);
+                }
+
+                return View(model);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await InitializeYcodeHcodeAndUnClassViewModel(model);
+                return View(model);
+            }
+
+            if (command == "continue")
+            {
+                using (var client = apiClient())
+                {
+                    if (model.SelectedYcode != null)
+                    {
+                        var selectedYWasteCode = new WasteCodeData { Id = new Guid(model.SelectedYcode) };
+                        if (model.SelectedYcodesList == null)
+                        {
+                            model.SelectedYcodesList = new List<WasteCodeData>();
+                            model.SelectedYcodesList.Add(selectedYWasteCode);
+                        }
+                        else
+                        {
+                            if (model.SelectedYcodesList.All(x => x.Id != selectedYWasteCode.Id))
+                            {
+                                model.SelectedYcodesList.Add(selectedYWasteCode);
+                            }
+                        }
+                    }
+
+                    try
+                    {
+                        //await client.SendAsync(User.GetAccessToken(), new SetYHUnWasteCodes(model.NotificationId, model.SelectedYcodesList.Select(p => p.Id)));
+                        return RedirectToAction("UnNumber", new { id = model.NotificationId });
+                    }
+                    catch (ApiBadRequestException ex)
+                    {
+                        this.HandleBadRequest(ex);
+                        if (ModelState.IsValid)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+
+            await InitializeYcodeHcodeAndUnClassViewModel(model);
+            return View(model);
+        }
+
+        private async Task<YCodesViewModel> InitializeYcodeHcodeAndUnClassViewModel(YCodesViewModel model)
+        {
+            using (var client = apiClient())
+            {
+                model.Ycodes = await client.SendAsync(User.GetAccessToken(), new GetWasteCodesByType(CodeType.Y));
+                return model;
+            }
+        }
+    }
+}
---
+++ src/EA.Iws.Web/Areas/NotificationApplication/ViewModels/WasteCodes/YCodesViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace EA.Iws.Web.Areas.NotificationApplication.ViewModels.WasteCodes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using Core.WasteCodes;
+
+    public class YCodesViewModel : IValidatableObject
+    {
+        public Guid NotificationId { get; set; }
+
+        public IEnumerable<WasteCodeData> Ycodes { get; set; }
+
+        public string SelectedYcode { get; set; }
+
+        public List<WasteCodeData> SelectedYcodesList { get; set; }
+
+        [Display(Name = "Not applicable")]
+        public bool IsNotApplicable { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(SelectedYcode) && (SelectedYcodesList == null || !SelectedYcodesList.Any()) && !IsNotApplicable)
+            {
+                yield return new ValidationResult("Please enter a Y code or select not applicable", new[] { "SelectedYcode" });
+            }
+        }
+    }
+}
