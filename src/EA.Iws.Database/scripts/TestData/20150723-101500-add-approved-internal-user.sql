1b5371f3ab87bd2b5c8d73d71668df0dc8162078
Eliot Jones <eliotj@sfwltd.co.uk>
2015-08-18 09:11:33 +0100

    Fix internal user test data script

--- src/EA.Iws.Database/scripts/TestData/20150723-101500-add-approved-internal-user.sql
+++ src/EA.Iws.Database/scripts/TestData/20150723-101500-add-approved-internal-user.sql
@@ -11,16 +11,11 @@ INSERT INTO [Identity].[AspNetUsers]
        AccessFailedCount,
        UserName,
        FirstName,
-       Surname,
-       JobTitle,
-       CompetentAuthority,
-       LocalArea,
-       IsInternal,
-       InternalUserStatus
+       Surname
 )
 VALUES
 (
-	   NEWID(),
+	   'E7D9AB70-84FC-45C3-99A7-B437D1336402',
        'superuser@environment-agency.gov.uk', 
        1, 
        'ANubTHWcySaAyFA68WgviJdTq0KOF4/iv5MTPvVPwRgmo0+CuzLTEszU0BcUYtOOTA==', 
@@ -31,10 +26,26 @@ VALUES
        0,
        'superuser@environment-agency.gov.uk',
        'Super',
-       'User',
-       'IWS Administrator',
-       'EA',
-       'North',
-       1,
-       1
+       'User'
+);
+GO
+
+INSERT INTO [Person].[InternalUser]
+(
+	[Id],
+	[UserId],
+	[JobTitle],
+	[CompetentAuthority],
+	[LocalAreaId],
+	[Status]
 )
+VALUES
+(
+	NEWID(),
+	'E7D9AB70-84FC-45C3-99A7-B437D1336402',
+    'IWS Administrator',
+	1,
+	(SELECT TOP 1 [Id] FROM [Lookup].[LocalArea] WHERE Name = 'Head Office'),
+	1
+);
+GO
