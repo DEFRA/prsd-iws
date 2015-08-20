890ef3c5d0aaef272d5b1b4b45258206be938b59
David Raine <DavidR@sfwltd.co.uk>
2015-08-20 14:29:58 +0100

    Create handler to set payment received date

---
+++ src/EA.Iws.RequestHandlers/Admin/NotificationAssessment/SetPaymentReceivedDateHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace EA.Iws.RequestHandlers.Admin.NotificationAssessment
+{
+    using System.Data.Entity;
+    using System.Threading.Tasks;
+    using DataAccess;
+    using Prsd.Core.Mediator;
+    using Requests.Admin.NotificationAssessment;
+
+    internal class SetPaymentReceivedDateHandler : IRequestHandler<SetPaymentReceivedDate, bool>
+    {
+        private readonly IwsContext context;
+
+        public SetPaymentReceivedDateHandler(IwsContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<bool> HandleAsync(SetPaymentReceivedDate message)
+        {
+            var assessment =
+                await
+                    context.NotificationAssessments.SingleAsync(
+                        p => p.NotificationApplicationId == message.NotificationId);
+
+            assessment.Dates.PaymentReceivedDate = message.PaymentReceivedDate;
+
+            await context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
