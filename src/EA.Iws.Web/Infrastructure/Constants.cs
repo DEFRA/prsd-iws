b4d762b7a5d3ed6e37ab92ef7f25e7b353cce3db
Michael Murray <MichaelM@sfwltd.co.uk>
2015-08-20 16:14:58 +0100

    changed button text

--- src/EA.Iws.Web/Infrastructure/Constants.cs
+++ src/EA.Iws.Web/Infrastructure/Constants.cs
@@ -5,5 +5,7 @@
         public static readonly string IwsAuthType = "IWS";
 
         public static readonly string AntiForgeryCookieName = "request_verification_token";
+
+        public const string ContinueButtonText = "Save and continue";
     }
 }
