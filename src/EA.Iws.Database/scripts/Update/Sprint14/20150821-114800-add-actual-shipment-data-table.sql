ad41ae5346ffb372cd0aee7a9bae2a099720336b
Charlie Lusty <CharlesL@sfwltd.co.uk>
2015-08-21 12:13:47 +0100

    Add actual shipment data table

---
+++ src/EA.Iws.Database/scripts/Update/Sprint14/20150821-114800-add-actual-shipment-data-table.sql
@@ -0,0 +1,16 @@
+﻿CREATE SCHEMA Shipment;
+
+GO
+
+CREATE TABLE [Shipment].[Data]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL, 
+    [ShipmentNumber] INT NOT NULL,
+    [NotificationId] UNIQUEIDENTIFIER NOT NULL, 
+    [Date] DATE NULL, 
+    [Quantity] DECIMAL(18, 4) NULL, 
+    [Units] INT NULL, 
+    [NumberOfPackages] INT NULL,
+    CONSTRAINT [PK_Data_Shipment] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Data_Notification] FOREIGN KEY ([NotificationId]) REFERENCES [Notification].[Notification] (Id)
+)
