60fcaf3a9df79f917619b3c6f5aa680b6e258d3b
Michael Murray <MichaelM@sfwltd.co.uk>
2015-08-25 16:46:25 +0100

    add migration scripts

--- src/EA.Iws.Database/scripts/Update/Sprint14/20150825-092345-add-address-table.sql
+++ src/EA.Iws.Database/scripts/Update/Sprint14/20150825-092345-add-address-table.sql
@@ -1,13 +1,4 @@
-﻿ALTER TABLE [Notification].[Organisation] DROP COLUMN [Address1] 
-ALTER TABLE [Notification].[Organisation] DROP COLUMN [TownOrCity] 
-ALTER TABLE [Notification].[Organisation] DROP COLUMN [Address2] 
-ALTER TABLE [Notification].[Organisation] DROP COLUMN [Region] 
-ALTER TABLE [Notification].[Organisation] DROP COLUMN [PostalCode]
-ALTER TABLE [Notification].[Organisation] DROP COLUMN [Country]
-
-GO
-
-
+﻿
 CREATE TABLE [Person].[Address](
 	[Id] [uniqueidentifier] NOT NULL,
 	[Address1] [nvarchar](1024) NOT NULL,
@@ -28,3 +19,53 @@ GO
 
 ALTER TABLE [Identity].[AspNetUsers] ADD [AddressId] [uniqueidentifier] NULL
 GO
+
+DECLARE @tempAddresses TABLE 
+([Id] UNIQUEIDENTIFIER, [Address1] NVARCHAR(max)
+      ,[TownOrCity] NVARCHAR(max)
+      ,[Address2] NVARCHAR(max)
+      ,[Region] NVARCHAR(max)
+      ,[PostalCode] NVARCHAR(max)
+      ,[Country] NVARCHAR(max)
+	  ,[OrganisationId] UNIQUEIDENTIFIER)
+
+INSERT INTO @tempAddresses
+SELECT newid()
+	   ,[Address1]
+      ,[TownOrCity]
+      ,[Address2]
+      ,[Region]
+      ,[PostalCode]
+      ,[Country],
+	  Id FROM [Notification].[Organisation]
+
+
+INSERT INTO [Person].[Address] ([Id], [Address1]
+      ,[TownOrCity]
+      ,[Address2]
+      ,[Region]
+      ,[PostalCode]
+      ,[Country])
+SELECT Id
+	   ,[Address1]
+      ,[TownOrCity]
+      ,[Address2]
+      ,[Region]
+      ,[PostalCode]
+      ,[Country] FROM @tempAddresses
+
+UPDATE u SET u.AddressId = a.Id
+FROM [Identity].[AspNetUsers] u
+INNER JOIN @tempAddresses a ON u.OrganisationId = a.OrganisationId
+
+GO
+
+ALTER TABLE [Notification].[Organisation] DROP COLUMN [Address1] 
+ALTER TABLE [Notification].[Organisation] DROP COLUMN [TownOrCity] 
+ALTER TABLE [Notification].[Organisation] DROP COLUMN [Address2] 
+ALTER TABLE [Notification].[Organisation] DROP COLUMN [Region] 
+ALTER TABLE [Notification].[Organisation] DROP COLUMN [PostalCode]
+ALTER TABLE [Notification].[Organisation] DROP COLUMN [Country]
+
+GO
+
