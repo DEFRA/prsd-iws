68d8467519d4ad8347157454e0999ec2e7f4d9b2
Chirag Dani <ChiragD@sfwltd.co.uk>
2015-09-22 12:40:46 +0530

    Added cookies page and relevant changes

---
+++ src/EA.Iws.Web/Controllers/NewUserController.cs
@@ -0,0 +1,14 @@
+﻿namespace EA.Iws.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class NewUserController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Cookies()
+        {
+            return View();
+        }
+    }
+}
