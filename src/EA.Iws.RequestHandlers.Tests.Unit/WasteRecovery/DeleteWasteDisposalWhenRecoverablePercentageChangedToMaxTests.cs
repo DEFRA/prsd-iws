891b7ca805b7bcc7f8447ec1a31fe428bd9a9aa9
Matthew Smith <matthews@sfwltd.co.uk>
2015-10-06 14:57:00 +0100

    Added handler to delete waste disposal when recoverable percentage is set to 100

---
+++ src/EA.Iws.RequestHandlers.Tests.Unit/WasteRecovery/DeleteWasteDisposalWhenRecoverablePercentageChangedToMaxTests.cs
@@ -0,0 +1,62 @@
+﻿namespace EA.Iws.RequestHandlers.Tests.Unit.WasteRecovery
+{
+    using System;
+    using System.Threading.Tasks;
+    using Core.Shared;
+    using Domain;
+    using Domain.NotificationApplication.WasteRecovery;
+    using FakeItEasy;
+    using RequestHandlers.WasteRecovery;
+    using Xunit;
+
+    public class DeleteWasteDisposalWhenRecoverablePercentageChangedToMaxTests
+    {
+        private readonly TestIwsContext context;
+        private readonly IWasteDisposalRepository repository;
+        private readonly DeleteWasteDisposalWhenRecoverablePercentageChangedToMax handler;
+        private static readonly Guid NotificationId = Guid.NewGuid();
+
+        public DeleteWasteDisposalWhenRecoverablePercentageChangedToMaxTests()
+        {
+            context = new TestIwsContext();
+            repository = A.Fake<IWasteDisposalRepository>();
+            handler = new DeleteWasteDisposalWhenRecoverablePercentageChangedToMax(context, repository);
+        }
+
+        [Fact]
+        public async Task PercentageChangedTo100_DeletesDisposal()
+        {
+            var wasteDisposal = new WasteDisposal(NotificationId, "Test", new DisposalCost(ValuePerWeightUnits.Kilogram, 10m));
+
+            A.CallTo(() => repository.GetByNotificationId(NotificationId)).Returns(wasteDisposal);
+
+            await handler.HandleAsync(new PercentageChangedEvent(NotificationId, new Percentage(100)));
+
+            A.CallTo(() => repository.Delete(wasteDisposal)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Fact]
+        public async Task PercentageChangedToLessThan100_DoesNotDeleteDisposal()
+        {
+            var wasteDisposal = new WasteDisposal(NotificationId, "Test", new DisposalCost(ValuePerWeightUnits.Kilogram, 10m));
+
+            A.CallTo(() => repository.GetByNotificationId(NotificationId)).Returns(wasteDisposal);
+
+            await handler.HandleAsync(new PercentageChangedEvent(NotificationId, new Percentage(50)));
+
+            A.CallTo(() => repository.Delete(A<WasteDisposal>.Ignored)).MustNotHaveHappened();
+        }
+
+        [Fact]
+        public async Task DisposalDeleted_CallsSaveChanges()
+        {
+            var wasteDisposal = new WasteDisposal(NotificationId, "Test", new DisposalCost(ValuePerWeightUnits.Kilogram, 10m));
+
+            A.CallTo(() => repository.GetByNotificationId(NotificationId)).Returns(wasteDisposal);
+
+            await handler.HandleAsync(new PercentageChangedEvent(NotificationId, new Percentage(100)));
+
+            Assert.Equal(1, context.SaveChangesCount);
+        }
+    }
+}
---
+++ src/EA.Iws.RequestHandlers/WasteRecovery/DeleteWasteDisposalWhenRecoverablePercentageChangedToMax.cs
@@ -0,0 +1,35 @@
+﻿namespace EA.Iws.RequestHandlers.WasteRecovery
+{
+    using System.Threading.Tasks;
+    using DataAccess;
+    using Domain.NotificationApplication.WasteRecovery;
+    using Prsd.Core.Domain;
+
+    internal class DeleteWasteDisposalWhenRecoverablePercentageChangedToMax : IEventHandler<PercentageChangedEvent>
+    {
+        private readonly IwsContext context;
+        private readonly IWasteDisposalRepository repository;
+
+        public DeleteWasteDisposalWhenRecoverablePercentageChangedToMax(IwsContext context,
+            IWasteDisposalRepository repository)
+        {
+            this.repository = repository;
+            this.context = context;
+        }
+
+        public async Task HandleAsync(PercentageChangedEvent @event)
+        {
+            if (@event.NewPercentage.Value == 100)
+            {
+                var wasteDisposal = await repository.GetByNotificationId(@event.NotificationId);
+
+                if (wasteDisposal != null)
+                {
+                    repository.Delete(wasteDisposal);
+                }
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
