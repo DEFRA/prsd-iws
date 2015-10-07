0b5c4344c855108d8955d69a107bf4f28ba983c3
Charlie Lusty <CharlesL@sfwltd.co.uk>
2015-10-07 08:47:12 +0100

    Add required validation to method of disposal field

--- src/EA.Iws.Web/Areas/NotificationApplication/ViewModels/WasteRecovery/DisposalMethodViewModel.cs
+++ src/EA.Iws.Web/Areas/NotificationApplication/ViewModels/WasteRecovery/DisposalMethodViewModel.cs
@@ -1,12 +1,14 @@
 ﻿namespace EA.Iws.Web.Areas.NotificationApplication.ViewModels.WasteRecovery
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
     using Core.Shared;
 
     public class DisposalMethodViewModel
     {
         public Guid NotificationId { get; set; }
 
+        [Required(ErrorMessage = "Please enter the method of disposal")]
         public string DisposalMethod { get; set; }
 
         public string Amount { get; set; }
