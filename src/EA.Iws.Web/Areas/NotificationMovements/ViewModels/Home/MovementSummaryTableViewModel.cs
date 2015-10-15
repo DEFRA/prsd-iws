6b9b537068299973c879031ae67fc2abcf896d9e
Charlie Lusty <CharlesL@sfwltd.co.uk>
2015-10-15 09:12:51 +0100

    Refactor movement summary to use gds display date for

--- src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/Home/MovementSummaryTableViewModel.cs
+++ src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/Home/MovementSummaryTableViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace EA.Iws.Web.Areas.NotificationMovements.ViewModels.Home
 {
+    using System;
     using Core.Movement;
     using Prsd.Core.Helpers;
 
@@ -11,23 +12,23 @@
 
         public string PreNotification { get; set; }
 
-        public string ShipmentDate { get; set; }
+        public DateTime? ShipmentDate { get; set; }
 
-        public string Received { get; set; }
+        public DateTime? Received { get; set; }
 
         public string Quantity { get; set; }
 
-        public string RecoveredOrDisposedOf { get; set; }
+        public DateTime? RecoveredOrDisposedOf { get; set; }
 
         public MovementSummaryTableViewModel(MovementSummaryTableData data)
         {
             Number = data.Number;
-            Status = "Indetermination";
+            Status = "In determination";
             PreNotification = "- -";
-            ShipmentDate = data.ShipmentDate != null ? data.ShipmentDate.GetValueOrDefault().ToString("dd-MM-yyyy") : "- -";
-            Received = data.Received != null ? data.Received.GetValueOrDefault().ToString("dd-MM-yyyy") : "- -";
+            ShipmentDate = data.ShipmentDate;
+            Received = data.Received;
             Quantity = data.Quantity.HasValue ? data.Quantity.Value.ToString("G29") + " " + EnumHelper.GetDisplayName(data.QuantityUnits.GetValueOrDefault()) : "- -";
-            RecoveredOrDisposedOf = data.RecoveredOrDisposedOf != null ? data.RecoveredOrDisposedOf.GetValueOrDefault().ToString("dd-MM-yyyy") : "- -";
+            RecoveredOrDisposedOf = data.RecoveredOrDisposedOf;
         }
     }
 }
