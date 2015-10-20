addb3b25a3609957a34f05c0b9a23a8a3f8eb09f
Matthew Smith <matthews@sfwltd.co.uk>
2015-10-20 16:52:26 +0100

    Removed movement IwsContextExtenstions

--- src/EA.Iws.RequestHandlers/Movement/CreateMovementForNotificationByIdHandler.cs
+++ src/EA.Iws.RequestHandlers/Movement/CreateMovementForNotificationByIdHandler.cs
@@ -6,34 +6,45 @@
     using DataAccess;
     using Domain.Movement;
     using Domain.NotificationApplication.Shipment;
+    using Domain.NotificationApplication;
     using Prsd.Core.Mediator;
     using RequestHandlers.Notification;
     using Requests.Movement;
+    using Domain.NotificationAssessment;
+    using System.Linq;
 
     public class CreateMovementForNotificationByIdHandler : IRequestHandler<CreateMovementForNotificationById, Guid>
     {
+        private readonly INotificationAssessmentRepository assessmentRepository;
+        private readonly IMovementRepository movementRepository;
+        private readonly INotificationApplicationRepository notificationRepository;
         private readonly IwsContext context;
         private readonly MovementFactory movementFactory;
         private readonly IShipmentInfoRepository shipmentInfoRepository;
 
-        public CreateMovementForNotificationByIdHandler(IwsContext context, 
-            MovementFactory movementFactory, 
+        public CreateMovementForNotificationByIdHandler(IwsContext context,
+            MovementFactory movementFactory,
+            IMovementRepository movementRepository,
+            INotificationApplicationRepository notificationRepository,
+            INotificationAssessmentRepository assessmentRepository,
             IShipmentInfoRepository shipmentInfoRepository)
         {
             this.context = context;
             this.movementFactory = movementFactory;
             this.shipmentInfoRepository = shipmentInfoRepository;
+            this.notificationRepository = notificationRepository;
+            this.movementRepository = movementRepository;
+            this.assessmentRepository = assessmentRepository;
         }
 
         public async Task<Guid> HandleAsync(CreateMovementForNotificationById message)
         {
-            var notification = await context.GetNotificationApplication(message.Id);
-            var notificationAssessment =
-                await context.NotificationAssessments.SingleAsync(na => na.NotificationApplicationId == message.Id);
-            var movements = await context.GetMovementsForNotificationAsync(message.Id);
+            var notification = await notificationRepository.GetById(message.Id);
+            var notificationAssessment = await assessmentRepository.GetByNotificationId(message.Id);
+            var movements = await movementRepository.GetAllMovements(message.Id);
             var shipmentInfo = await shipmentInfoRepository.GetByNotificationId(message.Id);
 
-            var movement = movementFactory.Create(notification, notificationAssessment, shipmentInfo, movements);
+            var movement = movementFactory.Create(notification, notificationAssessment, shipmentInfo, movements.ToArray());
 
             context.Movements.Add(movement);
 
