2bf0497e7772b78139f7a05d6eb166c5d5ec27b0
Charlie Lusty <CharlesL@sfwltd.co.uk>
2015-10-20 15:05:43 +0100

    Make the movement operation receipt table fileid column not null

---
+++ src/EA.Iws.Database/scripts/Update/Sprint18/20151020-145845-update-movement-operation-receipt-fileid-to-not-null.sql
@@ -0,0 +1,14 @@
+﻿GO
+
+DELETE FROM [Notification].[MovementOperationReceipt]
+      WHERE FileId IS NULL;
+
+GO
+
+
+GO
+
+ALTER TABLE [Notification].[MovementOperationReceipt]
+ALTER COLUMN [FileId] UNIQUEIDENTIFIER NOT NULL;
+
+GO
