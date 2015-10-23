637f4170169973d9474c0a2216a68f3eb7c38955
Eliot Jones <eliotj@sfwltd.co.uk>
2015-10-23 16:13:52 +0100

    add ui for internal shipment receipt

---
+++ src/EA.Iws.Web/Areas/NotificationAssessment/Controllers/MovementCertificateController.cs
@@ -0,0 +1,33 @@
+﻿namespace EA.Iws.Web.Areas.NotificationAssessment.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Prsd.Core.Mediator;
+    using ViewModels;
+
+    public class MovementCertificateController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public MovementCertificateController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public ActionResult Index(Guid id)
+        {
+            return View(new MovementSearchViewModel());
+        }
+
+        public async Task<ActionResult> Search(MovementSearchViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            throw new NotImplementedException();
+        } 
+    }
+}
---
+++ src/EA.Iws.Web/Areas/NotificationAssessment/ViewModels/MovementSearchViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EA.Iws.Web.Areas.NotificationAssessment.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class MovementSearchViewModel
+    {
+        [Required]
+        public string Number { get; set; }
+    }
+}
