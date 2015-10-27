4bd573bf1b60df60ad543e44f449098bbb390760
David Raine <DavidR@sfwltd.co.uk>
2015-10-27 11:33:09 +0000

    Add smoke test request to test mediator pipeline

---
+++ src/EA.Iws.Web/Areas/Admin/Controllers/SmokeTestController.cs
@@ -0,0 +1,25 @@
+﻿namespace EA.Iws.Web.Areas.Admin.Controllers
+{
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Prsd.Core.Mediator;
+    using Requests.Admin;
+
+    [AllowAnonymous]
+    public class SmokeTestController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public SmokeTestController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            var info = await mediator.SendAsync(new SmokeTest());
+            return Json(info, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
