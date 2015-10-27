b5a348dd4517774e501d3ce1b85afcc5db38238b
David Raine <DavidR@sfwltd.co.uk>
2015-10-27 16:15:14 +0000

    Add admin page to send a test email

---
+++ src/EA.Iws.Web/Areas/Admin/Controllers/TestEmailController.cs
@@ -0,0 +1,51 @@
+﻿namespace EA.Iws.Web.Areas.Admin.Controllers
+{
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Prsd.Core.Mediator;
+    using Requests.Admin;
+    using ViewModels.TestEmail;
+
+    [Authorize(Roles = "internal")]
+    public class TestEmailController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public TestEmailController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index(TestEmailViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await mediator.SendAsync(new SendTestEmail(model.EmailTo));
+
+            if (result)
+            {
+                return RedirectToAction("Success");
+            }
+
+            ModelState.AddModelError(string.Empty, TestEmailResources.ErrorSendingEmail);
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/EA.Iws.Web/Areas/Admin/ViewModels/TestEmail/TestEmailViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Iws.Web.Areas.Admin.ViewModels.TestEmail
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class TestEmailViewModel
+    {
+        [Required(ErrorMessageResourceName = "EmailAddressRequired", ErrorMessageResourceType = typeof(TestEmailViewModelResources))]
+        [EmailAddress(ErrorMessageResourceName = "EmailAddressInvalid", ErrorMessageResourceType = typeof(TestEmailViewModelResources))]
+        [Display(Name = "EmailTo", ResourceType = typeof(TestEmailViewModelResources))]
+        public string EmailTo { get; set; }
+    }
+}
