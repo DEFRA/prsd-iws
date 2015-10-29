092f1126fae6d0919ec072df5f0d2ed956c7b96c
Matthew Smith <matthews@sfwltd.co.uk>
2015-10-29 16:48:53 +0000

    Added internal shipment entry screen (import notification)

---
+++ src/EA.Iws.Web/Areas/ImportNotification/Controllers/ShipmentController.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Iws.Web.Areas.ImportNotification.Controllers
+{
+    using System.Web.Mvc;
+    using ViewModels;
+
+    [Authorize(Roles = "internal")]
+    public class ShipmentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new ShipmentViewModel());
+        }
+    }
+}
---
+++ src/EA.Iws.Web/Areas/ImportNotification/ViewModels/ShipmentViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace EA.Iws.Web.Areas.ImportNotification.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+    using Core.Shared;
+    using Web.ViewModels.Shared;
+
+    public class ShipmentViewModel
+    {
+        public ShipmentViewModel()
+        {
+            UnitsSelectList = new SelectList(Prsd.Core.Helpers.EnumHelper.GetValues(typeof(ShipmentQuantityUnits)), "Key", "Value");
+            StartDate = new OptionalDateInputViewModel();
+            EndDate = new OptionalDateInputViewModel();
+        }
+
+        [Display(Name = "Number of shipments")]
+        public int? TotalShipments { get; set; }
+
+        [Display(Name = "Total intended quantity")]
+        public decimal? TotalQuantity { get; set; }
+
+        public ShipmentQuantityUnits? Units { get; set; }
+        public SelectList UnitsSelectList { get; set; }
+        public OptionalDateInputViewModel StartDate { get; set; }
+        public OptionalDateInputViewModel EndDate { get; set; }
+    }
+}
