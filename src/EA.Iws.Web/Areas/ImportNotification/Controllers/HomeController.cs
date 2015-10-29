7f8303eee8bd4d0bd5518e84518a42fdc0972481
David Raine <DavidR@sfwltd.co.uk>
2015-10-29 12:13:28 +0000

    Create area for import notification

---
+++ src/EA.Iws.Web/Areas/ImportNotification/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Iws.Web.Areas.ImportNotification.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    [Authorize(Roles = "internal")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(Guid id)
+        {
+            return HttpNotFound();
+        }
+    }
+}
---
+++ src/EA.Iws.Web/Areas/ImportNotification/ImportNotificationAreaRegistration.cs
@@ -0,0 +1,23 @@
+﻿namespace EA.Iws.Web.Areas.ImportNotification
+{
+    using Infrastructure;
+    using System.Web.Mvc;
+    using Controllers;
+
+    public class ImportNotificationAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get { return "ImportNotification"; }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapLowercaseDashedRoute(
+                name: "ImportNotification_default",
+                url: "Import-Notification/{id}/{controller}/{action}/{entityId}",
+                defaults: new { action = "Index", controller = "Home", entityId = UrlParameter.Optional },
+                namespaces: new[] { typeof(HomeController).Namespace });
+        }
+    }
+}
