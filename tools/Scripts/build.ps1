31d719f07f3803da9aa41f05145787b2275a5cd0
Eliot Jones <eliotj@sfwltd.co.uk>
2015-10-30 11:37:42 +0000

    fix build script to account for spaces in the path names

--- tools/Scripts/build.ps1
+++ tools/Scripts/build.ps1
@@ -29,7 +29,7 @@ if (-not ($outDir.EndsWith("\"))) {
 } 
   
 if ($outDir.Contains(" ")) { 
-    $outDir="""$($outDir)\""" #read comment from Johannes Rudolph here: http://www.markhneedham.com/blog/2008/08/14/msbuild-use-outputpath-instead-of-outdir/ 
+    $outDir="$($outDir)\" #read comment from Johannes Rudolph here: http://www.markhneedham.com/blog/2008/08/14/msbuild-use-outputpath-instead-of-outdir/ 
 }
 
 ###
@@ -40,29 +40,43 @@ $minorVersion = 0;
 $buildDate = Get-Date;
 $buildVersion = [string]::Format("{0}.{1}.{2}.{3}", $majorVersion, $minorVersion, $buildDate.ToString("yy") + $buildDate.DayOfYear, $BuildNumber);
 
-.\update-assembly-versions.ps1 -NewVersion $buildVersion -SourceDirectory $srcDir
+$iexUpdateAssemblyVersion = "& '$toolsDir\Scripts\update-assembly-versions.ps1' -NewVersion $buildVersion -SourceDirectory '$srcDir'"
+
+&iex $iexUpdateAssemblyVersion
 
 ###
 # Restore NuGet packages
+# Quiet verbosity because the restore gives a warning for the maintenance project which causes Jenkins to
+# mark the build as unstable
 ###
-&$toolsDir\NuGet\nuget.exe restore $srcDir\EA.Iws.sln
+
+&$toolsDir\NuGet\nuget.exe restore $srcDir\EA.Iws.sln -Verbosity quiet
 
 ###
 # Transform config files
 ###
+write-host "Transform config files"
+
 &$toolsDir\WebConfigTransform\wct.exe $srcDir\EA.Iws.Web\Web.config $srcDir\EA.Iws.Web\Web.Release.config $srcDir\EA.Iws.Web\Web.config
 
 &$toolsDir\WebConfigTransform\wct.exe $srcDir\EA.Iws.Api\Web.config $srcDir\EA.Iws.Api\Web.Release.config $srcDir\EA.Iws.Api\Web.config
 
+write-host "Beginning build"
+
+$buildTarget = $srcDir + "\EA.Iws.sln" 
 ###
 # Build the solution
+# Because all paths may contain spaces they have to be enclosed with the ' character and the string for iex-ing must start with &
 ###
-&$msbuild $srcDir\EA.Iws.sln /p:Configuration=Release /p:Platform=x64 /p:OutDir="$outDir" 2>$msbuildErrOutput
+$iexBuild = "& '$msbuild' '$buildTarget' /p:Configuration=Release /p:Platform=x64 /p:OutDir='$outDir'"
+&iex "$iexBuild"
 
 if ($lastExitCode -ne 0) { 
     write-error "Error while running MSBuild. Details:`n$msbuildErrorOutput" 
     exit 1 
 }
+
+write-host "Finish build"
 
 ###
 # Copy database scripts
@@ -70,9 +84,26 @@ if ($lastExitCode -ne 0) {
 New-Item $outDir\Database\ -ItemType Directory -Force | Out-Null
 Copy-Item $srcDir\EA.Iws.Database\scripts\* $outDir\Database\ -Force -Recurse -Container
 
+write-host "Copied database scripts"
+
 ###
 # Run unit tests
 ###
 $xunit = dir $packagesDir -recurse | where { $_.PSIsContainer -eq $false -and $_.Name -eq "xunit.console.exe" } | foreach { $_.FullName } | sort -descending
 $testDlls = dir $outDir | where { $_.Name -like "*.Tests.Unit.dll" } | foreach { "`"" + $_.FullName +"`"" }
-&$xunit ([string]::Join(" ", $testDlls)) -parallel none -nunit $outDir\xunit-test-results.xml
+
+write-host "Found xunit test dlls"
+
+# As with build the paths may contain spaces and must be enclosed by ' for the iex to work
+$testDllString = ([string]::Join(" ", $testDlls))
+$testOutDir = $outDir + "\xunit-test-results.xml"
+
+$testConsole = $xunit
+
+if($xunit -is [system.array])
+{
+    $testConsole = $xunit[0]
+}
+
+$iexTest = "& '$testConsole' '$testDllString' -parallel none -nunit '$testOutDir'"
+&iex $iexTest
