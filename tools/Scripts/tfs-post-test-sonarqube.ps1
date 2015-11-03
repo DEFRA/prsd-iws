0b496b2e6f58a850971b180b9630e9f2f1c1afc0
David Raine <DavidR@sfwltd.co.uk>
2015-11-03 14:46:06 +0000

    Update SonarQube runner version

--- tools/Scripts/tfs-post-test-sonarqube.ps1
+++ tools/Scripts/tfs-post-test-sonarqube.ps1
@@ -1,1 +1,1 @@
-& C:\SonarCube\SonarQube.MSBuild.Runner-0.9\SonarQube.MSBuild.Runner.exe
+& C:\SonarCube\SonarQube.MSBuild.Runner-1.0.1\MSBuild.SonarQube.Runner.exe end
--- tools/Scripts/tfs-pre-build-sonarqube.ps1
+++ tools/Scripts/tfs-pre-build-sonarqube.ps1
@@ -1,3 +1,3 @@
 & $env:TF_BUILD_SOURCESDIRECTORY\tools\NuGet\nuget.exe restore $env:TF_BUILD_SOURCESDIRECTORY\src\EA.Iws.sln
 
-& C:\SonarCube\SonarQube.MSBuild.Runner-0.9\SonarQube.MSBuild.Runner.exe /key:EA.Iws /name:EA.Iws /version:version1.0
+& C:\SonarCube\SonarQube.MSBuild.Runner-1.0.1\MSBuild.SonarQube.Runner.exe begin /key:EA.Iws /name:EA.Iws /version:version1.0
