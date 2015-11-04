5879f57ba2b5d49a11e9f041ebd49352a43fdede
David Raine <DavidR@sfwltd.co.uk>
2015-11-04 16:41:15 +0000

    Update display of dates in shipment summary

--- src/EA.Iws.Web/Areas/NotificationAssessment/ViewModels/ShipmentSummary/MovementSummaryTableViewModel.cs
+++ src/EA.Iws.Web/Areas/NotificationAssessment/ViewModels/ShipmentSummary/MovementSummaryTableViewModel.cs
@@ -2,13 +2,13 @@
 {
     using System;
     using Core.Movement;
-    using Prsd.Core.Helpers;
+    using Core.Shared;
 
     public class MovementSummaryTableViewModel
     {
         public int Number { get; set; }
 
-        public string Status { get; set; }
+        public MovementStatus Status { get; set; }
 
         public DateTime? PreNotification { get; set; }
 
@@ -16,18 +16,21 @@
 
         public DateTime? Received { get; set; }
 
-        public string Quantity { get; set; }
+        public decimal? Quantity { get; set; }
+
+        public ShipmentQuantityUnits? Unit { get; set; }
 
         public DateTime? RecoveredOrDisposedOf { get; set; }
 
         public MovementSummaryTableViewModel(MovementSummaryTableData data)
         {
             Number = data.Number;
-            Status = EnumHelper.GetDisplayName(data.Status);
+            Status = data.Status;
             PreNotification = data.SubmittedDate;
             ShipmentDate = data.ShipmentDate;
             Received = data.ReceivedDate;
-            Quantity = data.Quantity.HasValue ? data.Quantity.Value.ToString("G29") + " " + EnumHelper.GetDisplayName(data.QuantityUnits.GetValueOrDefault()) : "- -";
+            Quantity = data.Quantity;
+            Unit = data.QuantityUnits;
             RecoveredOrDisposedOf = data.CompletedDate;
         }
     }
--- src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/Home/MovementSummaryTableViewModel.cs
+++ src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/Home/MovementSummaryTableViewModel.cs
@@ -3,15 +3,14 @@
     using System;
     using System.Linq;
     using Core.Movement;
+    using Core.Shared;
     using Prsd.Core.Helpers;
 
     public class MovementSummaryTableViewModel
     {
-        private static readonly string[] HideQuantityStatus = new[] { "New", "Cancelled" };
-
         public int Number { get; set; }
 
-        public string Status { get; set; }
+        public MovementStatus Status { get; set; }
 
         public DateTime? PreNotification { get; set; }
 
@@ -19,20 +18,21 @@
 
         public DateTime? Received { get; set; }
 
-        public string Quantity { get; set; }
+        public decimal? Quantity { get; set; }
+
+        public ShipmentQuantityUnits? Unit { get; set; }
 
         public DateTime? RecoveredOrDisposedOf { get; set; }
 
         public MovementSummaryTableViewModel(MovementSummaryTableData data)
         {
             Number = data.Number;
-            Status = EnumHelper.GetDisplayName(data.Status);
+            Status = data.Status;
             PreNotification = data.SubmittedDate;
             ShipmentDate = data.ShipmentDate;
             Received = data.ReceivedDate;
-            Quantity = data.Quantity.HasValue && !HideQuantityStatus.Contains(Status) 
-                ? data.Quantity.Value.ToString("G29") + " " + EnumHelper.GetDisplayName(data.QuantityUnits.GetValueOrDefault()) 
-                : "- -";
+            Quantity = data.Quantity;
+            Unit = data.QuantityUnits;
             RecoveredOrDisposedOf = data.CompletedDate;
         }
     }
