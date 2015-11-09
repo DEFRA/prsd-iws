187cc6159ae617b92752db676a380552604877f3
Matthew Smith <matthews@sfwltd.co.uk>
2015-11-09 14:29:35 +0000

    Add new create multiple prenote screen

---
+++ src/EA.Iws.Web/Areas/NotificationMovements/Controllers/CreateController.cs
@@ -0,0 +1,26 @@
+﻿namespace EA.Iws.Web.Areas.NotificationMovements.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+    using ViewModels.Create;
+
+    public class CreateController : Controller
+    {
+        private const string CreateMovementKey = "CreateMovementKey";
+
+        [HttpGet]
+        public ActionResult Index(Guid notificationId)
+        {
+            return View(new CreateViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Guid notificationId, CreateViewModel model)
+        {
+            TempData.Add(CreateMovementKey, model.NumberToCreate);
+
+            return HttpNotFound();
+        }
+    }
+}
