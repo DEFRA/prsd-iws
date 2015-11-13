5710ffad602d48d2dacdbbfbdffc1a3d91ed9f28
Matthew Smith <matthews@sfwltd.co.uk>
2015-11-13 11:26:48 +0000

    Implement movement details repository

--- src/EA.Iws.DataAccess/Repositories/MovementDetailsRepository.cs
+++ src/EA.Iws.DataAccess/Repositories/MovementDetailsRepository.cs
@@ -1,14 +1,37 @@
 ﻿namespace EA.Iws.DataAccess.Repositories
 {
     using System;
+    using System.Data.Entity;
+    using System.Linq;
     using System.Threading.Tasks;
     using Domain.Movement;
+    using Domain.Security;
 
     internal class MovementDetailsRepository : IMovementDetailsRepository
     {
-        public Task<MovementDetails> GetByMovementId(Guid movementId)
+        private readonly INotificationApplicationAuthorization authorization;
+        private readonly IwsContext context;
+
+        public MovementDetailsRepository(IwsContext context,
+            INotificationApplicationAuthorization authorization)
         {
-            throw new NotImplementedException();
+            this.context = context;
+            this.authorization = authorization;
+        }
+
+        public async Task<MovementDetails> GetByMovementId(Guid movementId)
+        {
+            var result = await context.MovementDetails
+                .Join(
+                    context.Movements,
+                    md => md.MovementId,
+                    m => m.Id,
+                    (md, m) => new { Movement = m, MovementDetails = md })
+                .SingleOrDefaultAsync(j => j.Movement.Id == movementId);
+
+            await authorization.EnsureAccessAsync(result.Movement.NotificationId);
+
+            return result.MovementDetails;
         }
     }
 }
