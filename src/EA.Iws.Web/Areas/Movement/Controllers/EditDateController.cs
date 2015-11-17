7a2cf85c6871fdfd490a8b053d2720b2246fd7eb
Matthew Smith <matthews@sfwltd.co.uk>
2015-11-17 15:59:02 +0000

    Add edit date submission (non saving)

--- src/EA.Iws.Web/Areas/Movement/Controllers/EditDateController.cs
+++ src/EA.Iws.Web/Areas/Movement/Controllers/EditDateController.cs
@@ -1,15 +1,35 @@
 ﻿namespace EA.Iws.Web.Areas.Movement.Controllers
 {
     using System;
+    using System.Threading.Tasks;
     using System.Web.Mvc;
+    using NotificationMovements.ViewModels.Edit;
+    using Prsd.Core.Mediator;
+    using Requests.Movement;
 
     [Authorize]
     public class EditDateController : Controller
     {
+        private readonly IMediator mediator;
+
+        public EditDateController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
         [HttpGet]
         public ActionResult Index(Guid id)
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index(Guid id, EditViewModel model)
+        {
+            var notificationId = await mediator.SendAsync(new GetNotificationIdByMovementId(id));
+
+            return RedirectToAction("Index", "Home", new { area = "NotificationMovements", notificationId });
         }
     }
 }
