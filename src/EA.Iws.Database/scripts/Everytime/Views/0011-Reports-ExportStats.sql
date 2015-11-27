b5d31b7921aef972a5159f8fa0d8879f9504a617
David Raine <DavidR@sfwltd.co.uk>
2015-11-27 17:25:31 +0000

    Left join on transit states. Add space after comma.

--- src/EA.Iws.Database/scripts/Everytime/Views/0011-Reports-ExportStats.sql
+++ src/EA.Iws.Database/scripts/Everytime/Views/0011-Reports-ExportStats.sql
@@ -32,31 +32,31 @@ FROM (
 		(SELECT TOP 1 WC.Description
 			FROM [Reports].[WasteCodes] WC
 			WHERE WC.CodeType IN (1, 2) AND WC.NotificationId = M.NotificationId) AS [BaselOecdDescription],
-		STUFF(( SELECT ',' + WC.Code AS [text()]
+		STUFF(( SELECT ', ' + WC.Code AS [text()]
 				   FROM [Reports].[WasteCodes] WC
 				   WHERE WC.NotificationId = M.NotificationId AND WC.CodeType = 3
 				   order by 1
 				   FOR XML PATH('')
 				 ), 1, 1, '' ) AS [EWC],
-		STUFF(( SELECT ',' + WC.Code AS [text()]
+		STUFF(( SELECT ', ' + WC.Code AS [text()]
 				   FROM [Reports].[WasteCodes] WC
 				   WHERE WC.NotificationId = M.NotificationId AND WC.CodeType = 4
 				   order by 1
 				   FOR XML PATH('')
 				 ), 1, 1, '' ) AS [Ycode],
-		STUFF(( SELECT ',' + WC.Code AS [text()]
+		STUFF(( SELECT ', ' + WC.Code AS [text()]
 				   FROM [Reports].[WasteCodes] WC
 				   WHERE WC.NotificationId = M.NotificationId AND WC.CodeType = 5
 				   order by 1
 				   FOR XML PATH('')
 				 ), 1, 1, '' ) AS [Hcode],
-		STUFF(( SELECT ',' + WC.Description AS [text()]
+		STUFF(( SELECT ', ' + WC.Description AS [text()]
 				   FROM [Reports].[WasteCodes] WC
 				   WHERE WC.NotificationId = M.NotificationId AND WC.CodeType = 5
 				   order by 1
 				   FOR XML PATH('')
 				 ), 1, 1, '' ) AS [HcodeDescription],
-		STUFF(( SELECT ',' + WC.Code AS [text()]
+		STUFF(( SELECT ', ' + WC.Code AS [text()]
 				   FROM [Reports].[WasteCodes] WC
 				   WHERE WC.NotificationId = M.NotificationId AND WC.CodeType = 6
 				   order by 1
@@ -66,7 +66,7 @@ FROM (
 	FROM
 		[Reports].[Movements] M
 		INNER JOIN [Reports].[TransportRoute] TR ON TR.NotificationId = M.NotificationId
-		INNER JOIN [Reports].[TransitStatesConcat] TS ON TS.NotificationId = M.NotificationId
+		LEFT JOIN [Reports].[TransitStatesConcat] TS ON TS.NotificationId = M.NotificationId
 		INNER JOIN [Reports].[OperationCodesConcat] OCC ON OCC.NotificationId = M.NotificationId
 		INNER JOIN [Reports].[WasteType] WT ON WT.NotificationId = M.NotificationId
 	WHERE
