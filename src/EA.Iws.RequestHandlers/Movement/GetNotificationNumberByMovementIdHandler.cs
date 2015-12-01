b9eae5a55bed89419bbf242b84b1f3f7f71c36c9
David Raine <DavidR@sfwltd.co.uk>
2015-12-01 11:06:43 +0000

    Add switcher to internal movement capture

---
+++ src/EA.Iws.RequestHandlers/Movement/GetNotificationNumberByMovementIdHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace EA.Iws.RequestHandlers.Movement
+{
+    using System.Threading.Tasks;
+    using Domain.NotificationApplication;
+    using Prsd.Core.Mediator;
+    using Requests.Movement;
+
+    internal class GetNotificationNumberByMovementIdHandler : IRequestHandler<GetNotificationNumberByMovementId, string>
+    {
+        private readonly INotificationApplicationRepository repository;
+
+        public GetNotificationNumberByMovementIdHandler(INotificationApplicationRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<string> HandleAsync(GetNotificationNumberByMovementId message)
+        {
+            var notification = await repository.GetByMovementId(message.MovementId);
+            return notification.NotificationNumber;
+        }
+    }
+}
---
+++ src/EA.Iws.Requests/Movement/GetNotificationNumberByMovementId.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Iws.Requests.Movement
+{
+    using System;
+    using Prsd.Core.Mediator;
+
+    public class GetNotificationNumberByMovementId : IRequest<string>
+    {
+        public Guid MovementId { get; private set; }
+
+        public GetNotificationNumberByMovementId(Guid movementId)
+        {
+            MovementId = movementId;
+        }
+    }
+}
