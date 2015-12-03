85f792855c9c4b7dfed785f551668e219e290fb8
Matthew Smith <matthews@sfwltd.co.uk>
2015-12-03 14:23:02 +0000

    Add contact validator

---
+++ src/EA.Iws.RequestHandlers.Tests.Unit/ImportNotification/Validate/ContactValidatorTests.cs
@@ -0,0 +1,91 @@
+﻿namespace EA.Iws.RequestHandlers.Tests.Unit.ImportNotification.Validate
+{
+    using Core.ImportNotification.Draft;
+    using RequestHandlers.ImportNotification.Validate;
+    using Xunit;
+
+    public class ContactValidatorTests
+    {
+        private readonly ContactValidator validator;
+
+        public ContactValidatorTests()
+        {
+            validator = new ContactValidator();
+        }
+
+        [Fact]
+        public void ValidContact_ReturnsSuccess()
+        {
+            var contact = GetValidContact();
+
+            var result = validator.Validate(contact);
+
+            Assert.True(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ContactNameMissing_ReturnsFailure(string contactName)
+        {
+            var invalidContact = GetValidContact();
+            invalidContact.ContactName = contactName;
+
+            var result = validator.Validate(invalidContact);
+
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void EmailMissing_ReturnsFailure(string email)
+        {
+            var invalidContact = GetValidContact();
+            invalidContact.Email = email;
+
+            var result = validator.Validate(invalidContact);
+
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("test")]
+        [InlineData("test@test")]
+        public void EmailInvalid_ReturnsFailure(string email)
+        {
+            var invalidContact = GetValidContact();
+            invalidContact.Email = email;
+
+            var result = validator.Validate(invalidContact);
+
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void TelephoneMissing_ReturnsFailure(string telephone)
+        {
+            var invalidContact = GetValidContact();
+            invalidContact.Telephone = telephone;
+
+            var result = validator.Validate(invalidContact);
+
+            Assert.False(result.IsValid);
+        }
+
+        private Contact GetValidContact()
+        {
+            return new Contact
+            {
+                ContactName = "Mike Merry",
+                Email = "mike@merry.com",
+                Telephone = "01234 567890"
+            };
+        }
+    }
+}
---
+++ src/EA.Iws.RequestHandlers/ImportNotification/Validate/ContactValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Iws.RequestHandlers.ImportNotification.Validate
+{
+    using Core.ImportNotification.Draft;
+    using FluentValidation;
+
+    public class ContactValidator : AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(x => x.ContactName).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Telephone).NotEmpty();
+        }
+    }
+}
