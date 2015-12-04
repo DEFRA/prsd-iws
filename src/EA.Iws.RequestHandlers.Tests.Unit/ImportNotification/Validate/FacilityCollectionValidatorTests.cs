3ec05023a0b729cd8dab6b01be0de34b8618aa40
David Raine <DavidR@sfwltd.co.uk>
2015-12-04 13:49:47 +0000

    Add validator for facility collection

---
+++ src/EA.Iws.RequestHandlers.Tests.Unit/ImportNotification/Validate/FacilityCollectionValidatorTests.cs
@@ -0,0 +1,63 @@
+﻿namespace EA.Iws.RequestHandlers.Tests.Unit.ImportNotification.Validate
+{
+    using System.Collections.Generic;
+    using Core.ImportNotification.Draft;
+    using FakeItEasy;
+    using FluentValidation;
+    using FluentValidation.TestHelper;
+    using RequestHandlers.ImportNotification.Validate;
+    using Xunit;
+
+    public class FacilityCollectionValidatorTests
+    {
+        private readonly FacilityCollectionValidator validator;
+
+        public FacilityCollectionValidatorTests()
+        {
+            var facilityValidator = A.Fake<IValidator<Facility>>();
+            validator = new FacilityCollectionValidator(facilityValidator);
+        }
+
+        [Fact]
+        public void SiteOfTreatmentNotSet_HasValidationError()
+        {
+            var facilityCollection = new FacilityCollection
+            {
+                Facilities = new List<Facility>()
+                {
+                    new Facility
+                    {
+                        IsActualSite = false
+                    },
+                    new Facility
+                    {
+                        IsActualSite = false
+                    }
+                }
+            };
+
+            validator.ShouldHaveValidationErrorFor(x => x.Facilities, facilityCollection);
+        }
+
+        [Fact]
+        public void SiteOfTreatmentSet_HasNoValidationError()
+        {
+            var facilityCollection = new FacilityCollection
+            {
+                Facilities = new List<Facility>()
+                {
+                    new Facility
+                    {
+                        IsActualSite = true
+                    },
+                    new Facility
+                    {
+                        IsActualSite = false
+                    }
+                }
+            };
+
+            validator.ShouldNotHaveValidationErrorFor(x => x.Facilities, facilityCollection);
+        }
+    }
+}
---
+++ src/EA.Iws.RequestHandlers/ImportNotification/Validate/FacilityCollectionValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace EA.Iws.RequestHandlers.ImportNotification.Validate
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Core.ImportNotification.Draft;
+    using FluentValidation;
+
+    internal class FacilityCollectionValidator : AbstractValidator<FacilityCollection>
+    {
+        public FacilityCollectionValidator(IValidator<Facility> facilityValidator)
+        {
+            RuleFor(x => x.Facilities)
+                .SetCollectionValidator(facilityValidator)
+                .Must(HaveSiteOfTreatment);
+        }
+
+        private static bool HaveSiteOfTreatment(IEnumerable<Facility> facilities)
+        {
+            return facilities.Count(p => p.IsActualSite) == 1;
+        }
+    }
+}
