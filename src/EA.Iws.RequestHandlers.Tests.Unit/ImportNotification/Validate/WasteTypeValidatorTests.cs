3bd6bd7be43dd433a92ea53443222aa8b8163d0b
David Raine <DavidR@sfwltd.co.uk>
2015-12-04 15:26:23 +0000

    Add validator for waste type

---
+++ src/EA.Iws.RequestHandlers.Tests.Unit/ImportNotification/Validate/WasteTypeValidatorTests.cs
@@ -0,0 +1,171 @@
+﻿namespace EA.Iws.RequestHandlers.Tests.Unit.ImportNotification.Validate
+{
+    using System;
+    using System.Collections.Generic;
+    using Core.ImportNotification.Draft;
+    using FluentValidation.TestHelper;
+    using RequestHandlers.ImportNotification.Validate;
+    using Xunit;
+
+    public class WasteTypeValidatorTests
+    {
+        private readonly WasteTypeValidator validator;
+
+        public WasteTypeValidatorTests()
+        {
+            validator = new WasteTypeValidator();
+        }
+
+        [Fact]
+        public void ValidWasteTypeWithCodes_HasNoValidationErrors()
+        {
+            var validWasteType = new WasteType
+            {
+                Name = "Waste",
+                SelectedBaselCode = Guid.NewGuid(),
+                SelectedEwcCodes = new List<Guid>() { Guid.NewGuid() },
+                SelectedHCodes = new List<Guid>() { Guid.NewGuid() },
+                SelectedUnClasses = new List<Guid>() { Guid.NewGuid() },
+                SelectedYCodes = new List<Guid>() { Guid.NewGuid() },
+            };
+
+            var result = validator.Validate(validWasteType);
+
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ValidWasteTypeWithoutCodes_HasNoValidationErrors()
+        {
+            var validWasteType = new WasteType
+            {
+                Name = "Waste",
+                SelectedEwcCodes = new List<Guid> { Guid.NewGuid() },
+                BaselCodeNotListed = true,
+                HCodeNotApplicable = true,
+                UnClassNotApplicable = true,
+                YCodeNotApplicable = true
+            };
+
+            var result = validator.Validate(validWasteType);
+
+            Assert.True(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void InvalidName_HasValidationError(string input)
+        {
+            validator.ShouldHaveValidationErrorFor(x => x.Name, input);
+        }
+
+        [Fact]
+        public void InvalidSelectedBaselCode_HasValidationError()
+        {
+            var wasteType = new WasteType
+            {
+                BaselCodeNotListed = false,
+                SelectedBaselCode = null
+            };
+
+            validator.ShouldHaveValidationErrorFor(x => x.SelectedBaselCode, wasteType);
+        }
+
+        [Fact]
+        public void InvalidSelectedEwcCodes_HasValidationError()
+        {
+            var wasteType = new WasteType
+            {
+                SelectedEwcCodes = new List<Guid>()
+            };
+
+            validator.ShouldHaveValidationErrorFor(x => x.SelectedEwcCodes, wasteType);
+        }
+
+        [Fact]
+        public void InvalidSelectedHCodes_HasValidationError()
+        {
+            var wasteType = new WasteType
+            {
+                HCodeNotApplicable = false,
+                SelectedHCodes = new List<Guid>()
+            };
+
+            validator.ShouldHaveValidationErrorFor(x => x.SelectedHCodes, wasteType);
+        }
+
+        [Fact]
+        public void InvalidSelectedUnClasses_HasValidationError()
+        {
+            var wasteType = new WasteType
+            {
+                UnClassNotApplicable = false,
+                SelectedUnClasses = new List<Guid>()
+            };
+
+            validator.ShouldHaveValidationErrorFor(x => x.SelectedUnClasses, wasteType);
+        }
+
+        [Fact]
+        public void InvalidSelectedYCodes_HasValidationError()
+        {
+            var wasteType = new WasteType
+            {
+                YCodeNotApplicable = false,
+                SelectedYCodes = new List<Guid>()
+            };
+
+            validator.ShouldHaveValidationErrorFor(x => x.SelectedYCodes, wasteType);
+        }
+
+        [Fact]
+        public void BaselCodeNotListed_HasNoValidationError()
+        {
+            var wasteType = new WasteType
+            {
+                BaselCodeNotListed = true,
+                SelectedBaselCode = null
+            };
+
+            validator.ShouldNotHaveValidationErrorFor(x => x.SelectedBaselCode, wasteType);
+        }
+
+        [Fact]
+        public void HCodeNotApplicable_HasNoValidationError()
+        {
+            var wasteType = new WasteType
+            {
+                HCodeNotApplicable = true,
+                SelectedHCodes = new List<Guid>()
+            };
+
+            validator.ShouldNotHaveValidationErrorFor(x => x.SelectedHCodes, wasteType);
+        }
+
+        [Fact]
+        public void UnClassesNotApplicable_HasNoValidationError()
+        {
+            var wasteType = new WasteType
+            {
+                UnClassNotApplicable = true,
+                SelectedUnClasses = new List<Guid>()
+            };
+
+            validator.ShouldNotHaveValidationErrorFor(x => x.SelectedUnClasses, wasteType);
+        }
+
+        [Fact]
+        public void YCodeNotApplicable_HasNoValidationError()
+        {
+            var wasteType = new WasteType
+            {
+                YCodeNotApplicable = true,
+                SelectedYCodes = new List<Guid>()
+            };
+
+            validator.ShouldNotHaveValidationErrorFor(x => x.SelectedYCodes, wasteType);
+        }
+    }
+}
---
+++ src/EA.Iws.RequestHandlers/ImportNotification/Validate/WasteTypeValidator.cs
@@ -0,0 +1,52 @@
+﻿namespace EA.Iws.RequestHandlers.ImportNotification.Validate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Core.ImportNotification.Draft;
+    using FluentValidation;
+
+    internal class WasteTypeValidator : AbstractValidator<WasteType>
+    {
+        public WasteTypeValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty();
+
+            RuleFor(x => x.SelectedBaselCode)
+                .Must(HaveBaselCodeOrNotListed);
+
+            RuleFor(x => x.SelectedEwcCodes)
+                .NotEmpty();
+
+            RuleFor(x => x.SelectedHCodes)
+                .Must(HaveHCodesOrNotApplicable);
+
+            RuleFor(x => x.SelectedUnClasses)
+                .Must(HaveUnClassesOrNotApplicable);
+
+            RuleFor(x => x.SelectedYCodes)
+                .Must(HaveYCodesOrNotApplicable);
+        }
+
+        private bool HaveYCodesOrNotApplicable(WasteType instance, List<Guid> selectedYCodes)
+        {
+            return instance.YCodeNotApplicable || (selectedYCodes != null && selectedYCodes.Any());
+        }
+
+        private bool HaveUnClassesOrNotApplicable(WasteType instance, List<Guid> selectedUnClasses)
+        {
+            return instance.UnClassNotApplicable || (selectedUnClasses != null && selectedUnClasses.Any());
+        }
+
+        private static bool HaveHCodesOrNotApplicable(WasteType instance, List<Guid> selectedHCodes)
+        {
+            return instance.HCodeNotApplicable || (selectedHCodes != null && selectedHCodes.Any());
+        }
+
+        private static bool HaveBaselCodeOrNotListed(WasteType instance, Guid? selectedBaselCode)
+        {
+            return instance.BaselCodeNotListed || selectedBaselCode.GetValueOrDefault() != default(Guid);
+        }
+    }
+}
