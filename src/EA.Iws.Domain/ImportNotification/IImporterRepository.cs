c78d812c3ebfaa1a2cdf54a505a03eda7625cac7
David Raine <DavidR@sfwltd.co.uk>
2015-12-07 16:19:09 +0000

    Create repository interface

---
+++ src/EA.Iws.Domain/ImportNotification/IImporterRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Iws.Domain.ImportNotification
+{
+    using System;
+    using System.Threading.Tasks;
+
+    internal interface IImporterRepository
+    {
+        Task<Importer> GetByNotificationId(Guid notificationId);
+
+        void Add(Importer importer);
+    }
+}
