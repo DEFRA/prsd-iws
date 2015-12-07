6b535057760424f9f16290dd540cc7d12525afc0
David Raine <DavidR@sfwltd.co.uk>
2015-12-07 13:30:33 +0000

    Create EmailAddress value object

---
+++ src/EA.Iws.Domain.Tests.Unit/EmailAddressTests.cs
@@ -0,0 +1,115 @@
+﻿namespace EA.Iws.Domain.Tests.Unit
+{
+    using System;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class EmailAddressTests
+    {
+        public static IEnumerable<object[]> ValidEmails()
+        {
+            return new[]
+            {
+                new object[] { "email@example.com" },
+                new object[] { "firstname.lastname@example.com" },
+                new object[] { "email@subdomain.example.com" },
+                new object[] { "firstname+lastname@example.com" },
+                new object[] { "“email”@example.com" },
+                new object[] { "1234567890@example.com" },
+                new object[] { "email@example-one.com" },
+                new object[] { "_______@example.com" },
+                new object[] { "email@example.name" },
+                new object[] { "email@example.museum" },
+                new object[] { "email@example.co.jp" },
+                new object[] { "firstname-lastname@example.com" }
+            };
+        }
+
+        public static IEnumerable<object[]> InvalidEmails()
+        {
+            return new[]
+            {
+                new object[] { "plainaddress" },
+                new object[] { "#@%^%#$@#$@#.com" },
+                new object[] { "@example.com" },
+                new object[] { "Joe Smith < email@example.com >" },
+                new object[] { "email.example.com" },
+                new object[] { "email @example@example.com" },
+                new object[] { ".email @example.com" },
+                new object[] { "email.@example.com" },
+                new object[] { "email..email@example.com" },
+                new object[] { "email@example.com(Joe Smith)" },
+                new object[] { "email @example" },
+                new object[] { "email@-example.com" },
+                new object[] { "email @example.web" },
+                new object[] { "email@111.222.333.44444" },
+                new object[] { "email @example..com" },
+                new object[] { "Abc..123@example.com" },
+                new object[] { @"“(),:;<>[\]@example.com" },
+                new object[] { @"just""not""right@example.com" },
+                new object[] { @"this\ is""really""not\allowed@example.com" }
+            };
+        }
+            
+        [Theory]
+        [MemberData("ValidEmails")]
+        public void ValidEmailAddresses(string input)
+        {
+            var email = new EmailAddress(input);
+
+            Assert.IsType<EmailAddress>(email);
+        }
+            
+        [Theory]
+        [MemberData("InvalidEmails")]
+        public void InvalidEmailAddresses(string input)
+        {
+            Action createEmail = () => new EmailAddress(input);
+
+            Assert.Throws<ArgumentException>(createEmail);
+        }
+
+        [Fact]
+        public void EqualsOperator()
+        {
+            var email1 = new EmailAddress("email@address.com");
+            var email2 = new EmailAddress("email@address.com");
+
+            Assert.True(email1 == email2);
+        }
+
+        [Fact]
+        public void EqualsMethod()
+        {
+            var email1 = new EmailAddress("email@address.com");
+            var email2 = new EmailAddress("email@address.com");
+
+            Assert.True(email1.Equals(email2));
+        }
+
+        [Fact]
+        public void ToStringReturnsValue()
+        {
+            var email = new EmailAddress("email@address.com");
+
+            Assert.Equal("email@address.com", email.ToString());
+        }
+
+        [Fact]
+        public void NotEqualsOperator()
+        {
+            var email1 = new EmailAddress("email@address.com");
+            var email2 = new EmailAddress("email2@address.com");
+
+            Assert.True(email1 != email2);
+        }
+
+        [Fact]
+        public void StringOperator()
+        {
+            var email = new EmailAddress("email@address.com");
+
+            Assert.True("email@address.com" == email);
+        }
+    }
+}
---
+++ src/EA.Iws.Domain/EmailAddress.cs
@@ -0,0 +1,111 @@
+﻿namespace EA.Iws.Domain
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class EmailAddress
+    {
+        private static Regex emailAddressRegex = CreateRegex();
+        private readonly string emailAddress;
+        public EmailAddress(string emailAddress)
+        {
+            if (!emailAddressRegex.IsMatch(emailAddress))
+            {
+                throw new ArgumentException("Email address was not valid", "emailAddress");
+            }
+
+            this.emailAddress = emailAddress;
+        }
+
+        public string Value
+        {
+            get { return emailAddress; }
+        }
+
+        public override string ToString()
+        {
+            return emailAddress;
+        }
+
+        public static implicit operator string(EmailAddress rhs)
+        {
+            return rhs.Value;
+        }
+
+        public bool Equals(EmailAddress other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return string.Equals(emailAddress, other.emailAddress);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((EmailAddress)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return emailAddress.GetHashCode();
+        }
+
+        public static bool operator ==(EmailAddress left, EmailAddress right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(EmailAddress left, EmailAddress right)
+        {
+            return !Equals(left, right);
+        }
+
+        private static Regex CreateRegex()
+        {
+            // Regex copied from EmailAddressAttribute
+            return
+                new Regex(
+                    "^((([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|" +
+                    "[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+" +
+                    "(\\.([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|" +
+                    "[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+)*)|" +
+                    "((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?" +
+                    "(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|" +
+                    "[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|" +
+                    "(\\\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|" +
+                    "[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF]))))*" +
+                    "(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))" +
+                    "@((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|" +
+                    "(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])" +
+                    "([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*" +
+                    "([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+" +
+                    "(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|" +
+                    "(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])" +
+                    "([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*" +
+                    "([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.?$",
+                    RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+        }
+    }
+}
