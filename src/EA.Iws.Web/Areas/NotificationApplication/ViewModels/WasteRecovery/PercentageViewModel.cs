83a63fa536eb0b675774683600d70a2898408e74
Eliot Jones <eliotj@sfwltd.co.uk>
2015-12-07 09:33:07 +0000

    fix modelstate validation bugs for waste recovery screens

--- src/EA.Iws.Web/Areas/NotificationApplication/ViewModels/WasteRecovery/PercentageViewModel.cs
+++ src/EA.Iws.Web/Areas/NotificationApplication/ViewModels/WasteRecovery/PercentageViewModel.cs
@@ -6,7 +6,7 @@
     public class PercentageViewModel
     {
         [Required(ErrorMessageResourceName = "ValueRequired", ErrorMessageResourceType = typeof(PercentageResources))]
-        [Range(0, 100, ErrorMessageResourceName = "PercentageRange", ErrorMessageResourceType = typeof(PercentageResources))]
+        [Range(0d, 100d, ErrorMessageResourceName = "PercentageRange", ErrorMessageResourceType = typeof(PercentageResources))]
         [RegularExpression(@"\d+(\.\d{1,2})?", ErrorMessageResourceName = "PercentageRecoverableValid", ErrorMessageResourceType = typeof(PercentageResources))]
         [Display(Name = "PercentageRecoverable", ResourceType = typeof(PercentageResources))]
         public decimal? PercentageRecoverable { get; set; }
--- src/EA.Iws.Web/Infrastructure/Validation/IsValidMoneyDecimalAttribute.cs
+++ src/EA.Iws.Web/Infrastructure/Validation/IsValidMoneyDecimalAttribute.cs
@@ -3,14 +3,14 @@
     using System;
     using System.ComponentModel.DataAnnotations;
 
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Property)]
     public class IsValidMoneyDecimalAttribute : ValidationAttribute
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var validationResult = new ValidationResult("Please enter a number with a maximum of 2 decimal places");
-
-            if (value.ToString().IsValidMoneyDecimal())
+            
+            if (value != null && value.ToString().IsValidMoneyDecimal())
             {
                 validationResult = ValidationResult.Success;
             }
