7afb7c3b122f09bb922ca8b4934ecc2b0bcadd8d
David Raine <DavidR@sfwltd.co.uk>
2015-12-09 11:12:47 +0000

    Update validation summary links to focus input elements

--- src/EA.Iws.Web/Scripts/application.js
+++ src/EA.Iws.Web/Scripts/application.js
@@ -177,3 +177,19 @@ $(document).ready(function () {
         $(".current").siblings("ul").slideDown();
     });
 });
+
+$(window).load(function () {
+    // If there is an error summary, set focus to the summary
+    if ($(".error-summary").length) {
+        $(".error-summary").focus();
+        $(".error-summary a").click(function (e) {
+            e.preventDefault();
+            var href = $(this).attr("href");
+            $(href).focus();
+        });
+    }
+    // Otherwise, set focus to the field with the error
+    else {
+        $(".error input:first").focus();
+    }
+});
