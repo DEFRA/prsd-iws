9c0a17ed27fa5b3e7680683ec2da57aaeebe27a8
David Raine <DavidR@sfwltd.co.uk>
2015-12-10 09:58:42 +0000

    Create database table for import producer

---
+++ src/EA.Iws.DataAccess/Mappings/Imports/ProducerMapping.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Iws.DataAccess.Mappings.Imports
+{
+    using System.Data.Entity.ModelConfiguration;
+    using Domain.ImportNotification;
+
+    internal class ProducerMapping : EntityTypeConfiguration<Producer>
+    {
+        public ProducerMapping()
+        {
+            ToTable("Producer", "ImportNotification");
+
+            Property(x => x.ImportNotificationId).IsRequired();
+            Property(x => x.Name).HasColumnName("Name").IsRequired().HasMaxLength(3000);
+        }
+    }
+}
---
+++ src/EA.Iws.Database/scripts/Update/Sprint22/20151210-095700-create-import-producer-table.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [ImportNotification].[Producer](
+	[Id] [uniqueidentifier] NOT NULL CONSTRAINT PK_ImportNotificationProducer PRIMARY KEY,
+	[ImportNotificationId] [uniqueidentifier] NOT NULL 
+		CONSTRAINT FK_ImportNotificationProducer_ImportNotification FOREIGN KEY REFERENCES [ImportNotification].[Notification]([Id]),
+	[Name] [nvarchar](3000) NOT NULL,
+	[Address1] [nvarchar](1024) NOT NULL,
+	[Address2] [nvarchar](1024) NULL,
+	[TownOrCity] [nvarchar](1024) NOT NULL,
+	[PostalCode] [nvarchar](64) NULL,
+	[CountryId] [uniqueidentifier] NOT NULL
+		CONSTRAINT FK_ImportNotificationProducer_Country FOREIGN KEY REFERENCES [Lookup].[Country]([Id]),
+	[ContactName] [nvarchar](1024) NOT NULL,
+	[Telephone] [nvarchar](150) NOT NULL,
+	[Email] [nvarchar](256) NOT NULL,
+	[RowVersion] [timestamp] NULL
+)
