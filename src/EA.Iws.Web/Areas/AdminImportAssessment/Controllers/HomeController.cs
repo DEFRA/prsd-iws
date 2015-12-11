9796df7a727db3b35dcdce2284c5278d425ff8e8
Charlie Lusty <CharlesL@sfwltd.co.uk>
2015-12-11 16:54:03 +0000

    Add notification number to internal side menus

---
+++ src/EA.Iws.Web/Areas/AdminImportAssessment/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿namespace EA.Iws.Web.Areas.AdminImportAssessment.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+    using Prsd.Core.Mediator;
+    using Requests.ImportNotification;
+
+    [Authorize(Roles = "internal")]
+    public class HomeController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public HomeController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
+        public ActionResult InternalMenu(Guid id)
+        {
+            var data = mediator.SendAsync(new GetNotificationDetails(id)).GetAwaiter().GetResult();
+
+            return PartialView("_InternalMenu", data);
+        }
+    }
+}
