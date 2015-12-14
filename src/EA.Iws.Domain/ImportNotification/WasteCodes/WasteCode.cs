aef8c44893cc39a3ed45068c7a4db62b8d9115ba
Matthew Smith <matthews@sfwltd.co.uk>
2015-12-14 16:37:26 +0000

    Add import notificationm waste type to domain

--- src/EA.Iws.Domain/ImportNotification/WasteCodes/WasteCode.cs
+++ src/EA.Iws.Domain/ImportNotification/WasteCodes/WasteCode.cs
@@ -1,23 +1,19 @@
 ﻿namespace EA.Iws.Domain.ImportNotification.WasteCodes
 {
     using System;
-    using Core.WasteCodes;
     using Prsd.Core.Domain;
 
     public class WasteCode : Entity
     {
         public Guid WasteCodeId { get; private set; }
 
-        public CodeType Type { get; private set; }
-
         protected WasteCode()
         {
         }
 
-        public WasteCode(Guid wasteCodeId, CodeType type)
+        public WasteCode(Guid wasteCodeId)
         {
             WasteCodeId = wasteCodeId;
-            Type = type;
         }
     }
 }
---
+++ src/EA.Iws.RequestHandlers/Mappings/ImportNotification/WasteTypeMap.cs
@@ -0,0 +1,36 @@
+﻿namespace EA.Iws.RequestHandlers.Mappings.ImportNotification
+{
+    using System;
+    using Core.ImportNotification.Draft;
+    using Domain.ImportNotification.WasteCodes;
+    using Core.WasteCodes;
+    using Prsd.Core.Mapper;
+
+    internal class WasteTypeMap : IMap<WasteType, Domain.ImportNotification.WasteType>
+    {
+        public Domain.ImportNotification.WasteType Map(WasteType source)
+        {
+            BaselOecdCode baselCode;
+            if (source.BaselCodeNotListed)
+            {
+                baselCode = BaselOecdCode.CreateNotListed();
+            }
+            else
+            {
+                baselCode = BaselOecdCode.CreateFor(GenerateWasteCode(source.SelectedBaselCode.Value, FUCK));
+            }
+
+            var ewcCode = EwcCode.CreateFor()
+
+            return new Domain.ImportNotification.WasteType(source.ImportNotificationId,
+                source.Name,
+                baselCode,)
+            throw new NotImplementedException();
+        }
+
+        private WasteCode GenerateWasteCode(Guid wasteCodeId, CodeType type)
+        {
+            return new WasteCode(wasteCodeId, type);
+        }
+    }
+}
