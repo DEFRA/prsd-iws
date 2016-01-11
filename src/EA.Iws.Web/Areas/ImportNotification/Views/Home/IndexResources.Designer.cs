f0b16e538cfed84b835230dcfe1b12633b6392fa
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-01-11 14:25:14 +0000

    Change internal import overview to a table

--- src/EA.Iws.Web/Areas/ImportNotification/Views/Home/IndexResources.Designer.cs
+++ src/EA.Iws.Web/Areas/ImportNotification/Views/Home/IndexResources.Designer.cs
@@ -61,11 +61,137 @@ namespace EA.Iws.Web.Areas.ImportNotification.Views.Home {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Basel or OECD code.
+        /// </summary>
+        public static string BaselTitle {
+            get {
+                return ResourceManager.GetString("BaselTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Not applicable.
+        /// </summary>
+        public static string CodesNotApplicableText {
+            get {
+                return ResourceManager.GetString("CodesNotApplicableText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Competent authority:.
+        /// </summary>
+        public static string CompetentAuthorityText {
+            get {
+                return ResourceManager.GetString("CompetentAuthorityText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Country:.
+        /// </summary>
+        public static string CountryText {
+            get {
+                return ResourceManager.GetString("CountryText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Entry point:.
+        /// </summary>
+        public static string EntryPointText {
+            get {
+                return ResourceManager.GetString("EntryPointText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to EWC Codes.
+        /// </summary>
+        public static string EWCTitle {
+            get {
+                return ResourceManager.GetString("EWCTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Exit point:.
+        /// </summary>
+        public static string ExitPointText {
+            get {
+                return ResourceManager.GetString("ExitPointText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Exporter-notifier.
+        /// </summary>
+        public static string ExporterTitle {
+            get {
+                return ResourceManager.GetString("ExporterTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Recovery facilities.
+        /// </summary>
+        public static string FacilityTitle {
+            get {
+                return ResourceManager.GetString("FacilityTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to No.
+        /// </summary>
+        public static string False {
+            get {
+                return ResourceManager.GetString("False", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to H or HP Codes.
+        /// </summary>
+        public static string HTitle {
+            get {
+                return ResourceManager.GetString("HTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Importer-consignee.
+        /// </summary>
+        public static string ImporterTitle {
+            get {
+                return ResourceManager.GetString("ImporterTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Are there mulitple producers?.
+        /// </summary>
+        public static string MultipleProducersQuestion {
+            get {
+                return ResourceManager.GetString("MultipleProducersQuestion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to There are no transit states.
         /// </summary>
         public static string NoTransitStatesChoice {
             get {
                 return ResourceManager.GetString("NoTransitStatesChoice", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to codes.
+        /// </summary>
+        public static string OperationCodesTitle {
+            get {
+                return ResourceManager.GetString("OperationCodesTitle", resourceCulture);
             }
         }
         
@@ -79,16 +205,7 @@ namespace EA.Iws.Web.Areas.ImportNotification.Views.Home {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The facilities are not pre-consented.
-        /// </summary>
-        public static string PreconsentedFalse {
-            get {
-                return ResourceManager.GetString("PreconsentedFalse", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Pre-consented facilities.
+        ///   Looks up a localized string similar to Are all facilities pre-consented?.
         /// </summary>
         public static string PreconsentedTitle {
             get {
@@ -97,11 +214,11 @@ namespace EA.Iws.Web.Areas.ImportNotification.Views.Home {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The facilities are pre-consented.
+        ///   Looks up a localized string similar to Generator-producer.
         /// </summary>
-        public static string PreconsentedTrue {
+        public static string ProducerTitle {
             get {
-                return ResourceManager.GetString("PreconsentedTrue", resourceCulture);
+                return ResourceManager.GetString("ProducerTitle", resourceCulture);
             }
         }
         
@@ -115,11 +232,56 @@ namespace EA.Iws.Web.Areas.ImportNotification.Views.Home {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Intended dates of departure:.
+        /// </summary>
+        public static string ShipmentDatesTitle {
+            get {
+                return ResourceManager.GetString("ShipmentDatesTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Amounts and dates.
         /// </summary>
         public static string ShipmentInfoTitle {
             get {
                 return ResourceManager.GetString("ShipmentInfoTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Not entered.
+        /// </summary>
+        public static string ShipmentNotEnteredText {
+            get {
+                return ResourceManager.GetString("ShipmentNotEnteredText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Total intended number of shipments:.
+        /// </summary>
+        public static string ShipmentNumberTitle {
+            get {
+                return ResourceManager.GetString("ShipmentNumberTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Total intended quantity:.
+        /// </summary>
+        public static string ShipmentQuantityTitle {
+            get {
+                return ResourceManager.GetString("ShipmentQuantityTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Intended shipments.
+        /// </summary>
+        public static string ShipmentsTitle {
+            get {
+                return ResourceManager.GetString("ShipmentsTitle", resourceCulture);
             }
         }
         
@@ -151,6 +313,15 @@ namespace EA.Iws.Web.Areas.ImportNotification.Views.Home {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Technology employed.
+        /// </summary>
+        public static string TechnologyTitle {
+            get {
+                return ResourceManager.GetString("TechnologyTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Notification overview.
         /// </summary>
         public static string Title {
@@ -178,6 +349,15 @@ namespace EA.Iws.Web.Areas.ImportNotification.Views.Home {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Yes.
+        /// </summary>
+        public static string True {
+            get {
+                return ResourceManager.GetString("True", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Not selected.
         /// </summary>
         public static string UnansweredQuestionPlaceholder {
@@ -187,11 +367,29 @@ namespace EA.Iws.Web.Areas.ImportNotification.Views.Home {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to UN Classes.
+        /// </summary>
+        public static string UNTitle {
+            get {
+                return ResourceManager.GetString("UNTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Waste codes.
         /// </summary>
         public static string WasteCodeTitle {
             get {
                 return ResourceManager.GetString("WasteCodeTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Y Codes.
+        /// </summary>
+        public static string YTitle {
+            get {
+                return ResourceManager.GetString("YTitle", resourceCulture);
             }
         }
     }
