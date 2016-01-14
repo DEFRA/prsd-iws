71fa7411b5e0c8f4d25de3568e667d4550781c75
Matthew Smith <matthews@sfwltd.co.uk>
2016-01-14 16:54:51 +0000

    Refactor status color helper into HtmlHelper extension method

---
+++ src/EA.Iws.Web/Infrastructure/ExportStatus.cs
@@ -0,0 +1,46 @@
+﻿namespace EA.Iws.Web.Infrastructure
+{
+    using System.Web.Mvc;
+    using Core.NotificationAssessment;
+
+    public class ExportStatus
+    {
+        private readonly HtmlHelper htmlHelper;
+
+        public ExportStatus(HtmlHelper htmlHelper)
+        {
+            this.htmlHelper = htmlHelper;
+        }
+
+        public MvcHtmlString DisplayColorClass(NotificationStatus status)
+        {
+            string statusColor;
+            switch (status)
+            {
+                case NotificationStatus.InAssessment:
+                case NotificationStatus.ReadyToTransmit:
+                case NotificationStatus.Transmitted:
+                case NotificationStatus.DecisionRequiredBy:
+                case NotificationStatus.Unlocked:
+                    statusColor = "s-orange";
+                    break;
+
+                case NotificationStatus.Consented:
+                    statusColor = "s-green";
+                    break;
+
+                case NotificationStatus.Objected:
+                case NotificationStatus.Withdrawn:
+                case NotificationStatus.ConsentWithdrawn:
+                    statusColor = "s-red";
+                    break;
+
+                default:
+                    statusColor = "s-blue";
+                    break;
+            }
+
+            return new MvcHtmlString(statusColor);
+        }
+    }
+}
---
+++ src/EA.Iws.Web/Infrastructure/ExportStatusExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.Iws.Web.Infrastructure
+{
+    using System.Web.Mvc;
+
+    public static class ExportStatusExtensions
+    {
+        public static ExportStatus ExportStatus(this HtmlHelper htmlHelper)
+        {
+            return new ExportStatus(htmlHelper);
+        }
+    }
+}
