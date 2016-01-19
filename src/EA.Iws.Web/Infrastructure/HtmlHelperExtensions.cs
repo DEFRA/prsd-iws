96a11fa97c35d8f22ae57edcccf3fe30854c36ab
David Raine <DavidR@sfwltd.co.uk>
2016-01-19 15:46:15 +0000

    Refactor date display logic into helper class

---
+++ src/EA.Iws.Web/Infrastructure/HtmlHelperExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace EA.Iws.Web.Infrastructure
+{
+    using System.Web.Mvc;
+    using Prsd.Core;
+
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// Displays todays date in the format 'dddd dd MMMM' including day suffix.
+        /// 
+        /// For example, 'Tuesday 19th January'.
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <returns></returns>
+        public static MvcHtmlString DisplayTodaysDate(this HtmlHelper helper)
+        {
+            var today = SystemTime.Now.Date;
+            string suffix;
+            switch (today.Day)
+            {
+                case 1:
+                case 21:
+                case 31:
+                    suffix = "st";
+                    break;
+                case 2:
+                case 22:
+                    suffix = "nd";
+                    break;
+                case 3:
+                case 23:
+                    suffix = "rd";
+                    break;
+                default:
+                    suffix = "th";
+                    break;
+            }
+
+            return new MvcHtmlString(string.Format("{0:dddd dd}{1} {0:MMMM}", today, suffix));
+        }
+    }
+}
