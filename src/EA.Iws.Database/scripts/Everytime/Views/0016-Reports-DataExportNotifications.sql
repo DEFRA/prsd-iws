cc2dafebf9a23ab652e30df335daee610ea3d34f
David Raine <DavidR@sfwltd.co.uk>
2016-01-21 11:38:14 +0000

    Don't bring back non-submitted notifications

--- src/EA.Iws.Database/scripts/Everytime/Views/0016-Reports-DataExportNotifications.sql
+++ src/EA.Iws.Database/scripts/Everytime/Views/0016-Reports-DataExportNotifications.sql
@@ -4,25 +4,26 @@ GO
 
 ALTER VIEW [Reports].[DataExportNotifications]
 AS
-	SELECT
-		N.[NotificationNumber],
-		N.[TypeId] AS [NotificationType],
-		N.[CompetentAuthorityId],
-		N.[Preconsented],
-		NA.[StatusId] AS [Status],
-		NA.[ReceivedDate] AS [NotificationReceived],
-		NA.[PaymentReceivedDate] AS [PaymentReceived],
-		NA.[CommencementDate] AS [AssessmentStarted],
-		NA.[CompleteDate] AS [ApplicationCompleted],
-		NA.[TransmittedDate] AS [Transmitted],
-		NA.[AcknowlegedDate] AS [Acknowledged],
-		-- Decision date will be the date it was withdrawn, objected or consented and it will only be one of these.
-		CAST(COALESCE(NA.WithdrawnDate, COALESCE(NA.ObjectedDate, NA.ConsentedDate)) AS DATE) AS [DecisionDate]
+    SELECT
+        N.[NotificationNumber],
+        N.[TypeId] AS [NotificationType],
+        N.[CompetentAuthorityId],
+        N.[Preconsented],
+        NA.[StatusId] AS [Status],
+        NA.[ReceivedDate] AS [NotificationReceived],
+        NA.[PaymentReceivedDate] AS [PaymentReceived],
+        NA.[CommencementDate] AS [AssessmentStarted],
+        NA.[CompleteDate] AS [ApplicationCompleted],
+        NA.[TransmittedDate] AS [Transmitted],
+        NA.[AcknowlegedDate] AS [Acknowledged],
+        -- Decision date will be the date it was withdrawn, objected or consented and it will only be one of these.
+        CAST(COALESCE(NA.WithdrawnDate, COALESCE(NA.ObjectedDate, NA.ConsentedDate)) AS DATE) AS [DecisionDate]
 
-	FROM		[Reports].[NotificationAssessment] AS NA
+    FROM		[Reports].[NotificationAssessment] AS NA
 
-	INNER JOIN	[Reports].[Notification] AS N
-	ON			[N].[Id] = [NA].[NotificationId]
+    INNER JOIN	[Reports].[Notification] AS N
+    ON			[N].[Id] = [NA].[NotificationId]
 
-	WHERE		[NA].[ImportOrExport] = 'Export'
+    WHERE		[NA].[ImportOrExport] = 'Export'
+    AND			[NA].[StatusId] <> 1
 GO
