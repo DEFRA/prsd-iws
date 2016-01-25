4bc76d4d06cc1500f860ead528f3cbee63aed3dc
David Raine <DavidR@sfwltd.co.uk>
2016-01-25 10:22:05 +0000

    Hide pre-consented facilities from overview for disposal notifications

--- src/EA.Iws.Web/Areas/ImportNotification/ViewModels/Home/SummaryTableContainerViewModel.cs
+++ src/EA.Iws.Web/Areas/ImportNotification/ViewModels/Home/SummaryTableContainerViewModel.cs
@@ -1,11 +1,17 @@
 ﻿namespace EA.Iws.Web.Areas.ImportNotification.ViewModels.Home
 {
     using Core.ImportNotification.Summary;
+    using Core.Shared;
 
     public class SummaryTableContainerViewModel
     {
         public ImportNotificationSummary Details { get; set; }
 
         public bool ShowChangeLinks { get; set; }
+
+        public bool ShowPreconsentedLinks
+        {
+            get { return Details.Type == NotificationType.Recovery; }
+        }
     }
 }
