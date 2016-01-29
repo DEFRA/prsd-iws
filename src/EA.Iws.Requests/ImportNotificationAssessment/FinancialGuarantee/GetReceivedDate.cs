76e6d96645fb5ae0bf2d728f0bb6098f2f45a558
Eliot Jones <eliotj@sfwltd.co.uk>
2016-01-29 11:48:55 +0000

    add import financial guarantee dates screens

---
+++ src/EA.Iws.Requests/ImportNotificationAssessment/FinancialGuarantee/GetReceivedDate.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Iws.Requests.ImportNotificationAssessment.FinancialGuarantee
+{
+    using System;
+    using Prsd.Core.Mediator;
+
+    public class GetReceivedDate : IRequest<DateTime?>
+    {
+        public Guid ImportNotificationId { get; private set; }
+
+        public GetReceivedDate(Guid importNotificationId)
+        {
+            ImportNotificationId = importNotificationId;
+        }
+    }
+}
---
+++ src/EA.Iws.Web/Areas/AdminImportAssessment/Controllers/FinancialGuaranteeController.cs
@@ -0,0 +1,22 @@
+﻿namespace EA.Iws.Web.Areas.AdminImportAssessment.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Prsd.Core.Mediator;
+
+    public class FinancialGuaranteeController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public FinancialGuaranteeController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public async Task<ActionResult> ReceivedDate(Guid id)
+        {
+            var receivedDate = await mediator.SendAsync(new GetReceivedDate(id));
+        }
+    }
+}
