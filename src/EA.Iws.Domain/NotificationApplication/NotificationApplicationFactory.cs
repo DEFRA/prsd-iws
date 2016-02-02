db21cfba015658bf54bed038c6e1b7e2c76da1a2
David Raine <DavidR@sfwltd.co.uk>
2016-02-02 12:14:29 +0000

    Validate legacy notification number is less than system default

--- src/EA.Iws.Domain/NotificationApplication/NotificationApplicationFactory.cs
+++ src/EA.Iws.Domain/NotificationApplication/NotificationApplicationFactory.cs
@@ -1,15 +1,22 @@
 ﻿namespace EA.Iws.Domain.NotificationApplication
 {
+    using System;
     using System.Threading.Tasks;
     using Core.ComponentRegistration;
     using Core.Shared;
+    using Prsd.Core;
     using Prsd.Core.Domain;
 
     [AutoRegister]
     public class NotificationApplicationFactory
     {
-        private readonly IUserContext userContext;
+        private const int EaNumberSystemStart = 5000;
+        private const int SepaNumberSystemStart = 500;
+        private const int NieaNumberSystemStart = 1000;
+        private const int NrwNumberSystemStart = 100;
+
         private readonly INotificationNumberGenerator numberGenerator;
+        private readonly IUserContext userContext;
 
         public NotificationApplicationFactory(IUserContext userContext, INotificationNumberGenerator numberGenerator)
         {
@@ -17,9 +24,19 @@
             this.numberGenerator = numberGenerator;
         }
 
-        public Task<NotificationApplication> CreateLegacy(NotificationType notificationType, UKCompetentAuthority competentAuthority, int number)
+        public Task<NotificationApplication> CreateLegacy(NotificationType notificationType,
+            UKCompetentAuthority competentAuthority, int number)
         {
-            var notification = new NotificationApplication(userContext.UserId, notificationType, competentAuthority, number);
+            Guard.ArgumentNotZeroOrNegative(() => number, number);
+
+            if (!IsNumberValid(competentAuthority, number))
+            {
+                throw new ArgumentOutOfRangeException("number",
+                    string.Format("{0} is out of range for a notification number for {1}", number, competentAuthority));
+            }
+
+            var notification = new NotificationApplication(userContext.UserId, notificationType, competentAuthority,
+                number);
             return Task.FromResult(notification);
         }
 
@@ -27,8 +44,17 @@
             UKCompetentAuthority competentAuthority)
         {
             var nextNotificationNumber = await numberGenerator.GetNextNotificationNumber(competentAuthority);
-            var notification = new NotificationApplication(userContext.UserId, notificationType, competentAuthority, nextNotificationNumber);
+            var notification = new NotificationApplication(userContext.UserId, notificationType, competentAuthority,
+                nextNotificationNumber);
             return notification;
+        }
+
+        private static bool IsNumberValid(UKCompetentAuthority competentAuthority, int number)
+        {
+            return (competentAuthority == UKCompetentAuthority.England && number < EaNumberSystemStart)
+                || (competentAuthority == UKCompetentAuthority.Scotland && number < SepaNumberSystemStart)
+                || (competentAuthority == UKCompetentAuthority.NorthernIreland && number < NieaNumberSystemStart)
+                || (competentAuthority == UKCompetentAuthority.Wales && number < NrwNumberSystemStart);
         }
     }
 }
