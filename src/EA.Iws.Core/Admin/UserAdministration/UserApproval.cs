a854e7c6a7ba84695d6ed58db43d8fdd412500f1
David Raine <DavidR@sfwltd.co.uk>
2016-02-05 17:29:55 +0000

    Set user role when approving internal user
    Renamed TeamLeader role to Administrator. Currently adding Administrator role claim in the handler to user if selected so they will have both the Internal and Administrator role claims.

---
+++ src/EA.Iws.Core/Admin/UserAdministration/UserApproval.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Iws.Core.Admin.UserAdministration
+{
+    using System;
+    using Authorization;
+
+    public class UserApproval
+    {
+        public UserApproval(Guid userId, ApprovalAction approvalAction, UserRole? assignedRole)
+        {
+            UserId = userId;
+            ApprovalAction = approvalAction;
+            AssignedRole = assignedRole;
+        }
+
+        public Guid UserId { get; private set; }
+
+        public ApprovalAction ApprovalAction { get; private set; }
+
+        public UserRole? AssignedRole { get; private set; }
+    }
+}
