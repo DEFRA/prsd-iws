eee11e0a6cb1eed42dad22790e455a3feffc9157
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-02-22 16:06:02 +0000

    Remove no longer used ApprovedNotification actions, view model and view

--- src/EA.Iws.Web/Controllers/ApplicantController.cs
+++ src/EA.Iws.Web/Controllers/ApplicantController.cs
@@ -1,13 +1,8 @@
 ﻿namespace EA.Iws.Web.Controllers
 {
-    using System;
-    using System.Threading.Tasks;
     using System.Web.Mvc;
-    using Core.NotificationAssessment;
     using Prsd.Core.Mediator;
     using Requests.Notification;
-    using Requests.NotificationAssessment;
-    using ViewModels.Applicant;
 
     [Authorize]
     public class ApplicantController : Controller
@@ -34,52 +29,6 @@
                 mediator.SendAsync(new GetNotificationsByUser()).Result;
 
             return PartialView(response);
-        }
-
-        [HttpGet]
-        public async Task<ActionResult> ApprovedNotification(Guid id)
-        {
-            var notificationInfo = await mediator.SendAsync(new GetNotificationAssessmentSummaryInformation(id));
-            var model = new ApprovedNotificationViewModel(notificationInfo);
-            
-            model.NotificationId = id;
-            return View(model);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> ApprovedNotification(ApprovedNotificationViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-
-            if (model.UserChoices.SelectedValue == 1)
-            {
-                var notificationAssessmentInfo = await mediator.SendAsync(new GetNotificationAssessmentSummaryInformation(model.NotificationId));
-
-                if (notificationAssessmentInfo.Status == NotificationStatus.NotSubmitted)
-                {
-                    return RedirectToAction("Index", "Exporter", new { id = model.NotificationId, area = "NotificationApplication" });
-                }
-
-                return RedirectToAction("Index", "Home", new { id = model.NotificationId, area = "NotificationApplication" });
-            }
-            if (model.UserChoices.SelectedValue == 2)
-            {
-                return RedirectToAction("GenerateNotificationDocument", "Home", new { id = model.NotificationId, area = "NotificationApplication" });
-            }
-            if (model.UserChoices.SelectedValue == 3)
-            {
-                return RedirectToAction("Index", "Home", new { notificationId = model.NotificationId, area = "NotificationMovements" });
-            }
-            if (model.UserChoices.SelectedValue == 4)
-            {
-                return RedirectToAction("Index", "KeyDates", new { id = model.NotificationId, area = "NotificationApplication" });
-            }
-            
-            throw new InvalidOperationException("Invalid user choice to view applicant's notification");
         }
     }
 }
