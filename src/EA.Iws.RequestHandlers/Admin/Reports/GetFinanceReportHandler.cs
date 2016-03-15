f0bc5e5214c06aa56f53de3500a279d5e92082f4
David Raine <DavidR@sfwltd.co.uk>
2016-03-15 10:38:15 +0000

    Filter finance report by CA

--- src/EA.Iws.RequestHandlers/Admin/Reports/GetFinanceReportHandler.cs
+++ src/EA.Iws.RequestHandlers/Admin/Reports/GetFinanceReportHandler.cs
@@ -3,7 +3,9 @@
     using System.Linq;
     using System.Threading.Tasks;
     using Core.Admin.Reports;
+    using Domain;
     using Domain.Reports;
+    using Prsd.Core.Domain;
     using Prsd.Core.Mapper;
     using Prsd.Core.Mediator;
     using Requests.Admin.Reports;
@@ -12,16 +14,22 @@
     {
         private readonly IFinanceReportRepository financeReportRepository;
         private readonly IMapper mapper;
+        private readonly IInternalUserRepository internalUserRepository;
+        private readonly IUserContext userContext;
 
-        public GetFinanceReportHandler(IFinanceReportRepository financeReportRepository, IMapper mapper)
+        public GetFinanceReportHandler(IFinanceReportRepository financeReportRepository, IMapper mapper,
+            IInternalUserRepository internalUserRepository, IUserContext userContext)
         {
             this.financeReportRepository = financeReportRepository;
             this.mapper = mapper;
+            this.internalUserRepository = internalUserRepository;
+            this.userContext = userContext;
         }
 
         public async Task<FinanceReportData[]> HandleAsync(GetFinanceReport message)
         {
-            var report = await financeReportRepository.GetFinanceReport(message.EndDate);
+            var user = await internalUserRepository.GetByUserId(userContext.UserId);
+            var report = await financeReportRepository.GetFinanceReport(message.EndDate, user.CompetentAuthority);
 
             return report.Select(f => mapper.Map<FinanceReportData>(f)).ToArray();
         }
