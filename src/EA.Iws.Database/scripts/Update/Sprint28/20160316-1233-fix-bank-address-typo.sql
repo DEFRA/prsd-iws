c0b8e1f6c8b427e5f96a7f8503b83b636b726eac
Chris Smith <chriss@sfwltd.co.uk>
2016-03-16 12:47:20 +0000

    Remove incorrect comment from script

--- src/EA.Iws.Database/scripts/Update/Sprint28/20160316-1233-fix-bank-address-typo.sql
+++ src/EA.Iws.Database/scripts/Update/Sprint28/20160316-1233-fix-bank-address-typo.sql
@@ -1,4 +1,3 @@
-﻿-- Set SEPA address
-UPDATE [Lookup].[UnitedKingdomCompetentAuthority]
+﻿UPDATE [Lookup].[UnitedKingdomCompetentAuthority]
 SET [BacsBankAddress]='Royal Bank of Scotland plc, London Corporate Service Centre, CPB Services 2nd Floor, 280 Bishopsgate, London, EC2M 4RB'
 WHERE [Id] = 1
