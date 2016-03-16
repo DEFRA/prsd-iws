6a1fa2adf4bfd4b5b8b9d4dcea78e5fd825713e9
Chris Smith <chriss@sfwltd.co.uk>
2016-03-16 12:45:47 +0000

    #47971 - Fix typo in EA bank details

---
+++ src/EA.Iws.Database/scripts/Update/Sprint28/20160316-1233-fix-bank-address-typo.sql
@@ -0,0 +1,4 @@
+﻿-- Set SEPA address
+UPDATE [Lookup].[UnitedKingdomCompetentAuthority]
+SET [BacsBankAddress]='Royal Bank of Scotland plc, London Corporate Service Centre, CPB Services 2nd Floor, 280 Bishopsgate, London, EC2M 4RB'
+WHERE [Id] = 1
