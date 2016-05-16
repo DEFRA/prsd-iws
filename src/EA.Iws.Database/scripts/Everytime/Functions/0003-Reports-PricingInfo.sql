47a9b3ccd1635b1bebf498c8cd40be8c170bde0e
David Raine <DavidR@sfwltd.co.uk>
2016-05-16 16:01:45 +0100

    Add import data to notification view. Rewrite potential refund function to return price and refund amount.

---
+++ src/EA.Iws.Database/scripts/Everytime/Functions/0003-Reports-PricingInfo.sql
@@ -0,0 +1,113 @@
+﻿IF OBJECT_ID('[Reports].[PricingInfo]') IS NULL
+    EXEC('CREATE FUNCTION [Reports].[PricingInfo]() RETURNS @PricingInfo TABLE (Price MONEY NULL, PotentialRefund MONEY NULL) AS BEGIN RETURN END;')
+GO	
+
+ALTER FUNCTION [Reports].[PricingInfo](
+    @notificationId		UNIQUEIDENTIFIER)
+RETURNS @PricingInfo TABLE
+(
+    Price MONEY NULL,
+    PotentialRefund MONEY NULL
+)
+AS
+BEGIN
+
+    DECLARE @numberOfShipments INT;
+    DECLARE @competentAuthority INT;
+    DECLARE @notificationType INT;
+    DECLARE @tradeDirection INT;
+    DECLARE @isInterim BIT;
+
+    SELECT
+        @numberOfShipments = NumberOfShipments,
+        @competentAuthority = CompetentAuthority,
+        @notificationType = NotificationType,
+        @tradeDirection = TradeDirection,
+        @isInterim = CASE WHEN IsInterim IS NOT NULL THEN IsInterim ELSE CASE WHEN NumberOfFacilities > 1 THEN 1 ELSE 0 END END
+    FROM (
+        SELECT
+            N.Id,
+            S.NumberOfShipments,
+            N.CompetentAuthority,
+            N.NotificationType,
+            1 AS TradeDirection,
+            FC.IsInterim,
+            COUNT(F.Id) AS NumberOfFacilities
+        FROM 
+            [Notification].[Notification] N
+            INNER JOIN [Notification].[ShipmentInfo] S ON S.NotificationId = N.Id
+            INNER JOIN [Notification].[FacilityCollection] FC ON FC.NotificationId = N.Id
+            INNER JOIN [Notification].[Facility] F ON F.FacilityCollectionId = FC.Id
+        WHERE 
+            N.Id = @notificationId
+        GROUP BY 
+            N.Id,
+            S.NumberOfShipments,
+            N.CompetentAuthority,
+            N.NotificationType,
+            FC.IsInterim
+
+        UNION
+
+        SELECT
+            N.Id,
+            S.NumberOfShipments,
+            N.CompetentAuthority,
+            N.NotificationType,
+            2 AS TradeDirection,
+            I.IsInterim,
+            COUNT(F.Id) AS NumberOfFacilities
+        FROM 
+            [ImportNotification].[Notification] N
+            INNER JOIN [ImportNotification].[Shipment] S ON S.ImportNotificationId = N.Id
+            INNER JOIN [ImportNotification].[FacilityCollection] FC ON FC.ImportNotificationId = N.Id
+            INNER JOIN [ImportNotification].[Facility] F ON F.FacilityCollectionId = FC.Id
+            LEFT JOIN [ImportNotification].[InterimStatus] I ON I.ImportNotificationId = N.Id
+        WHERE 
+            N.Id = @notificationId
+        GROUP BY 
+            N.Id,
+            S.NumberOfShipments,
+            N.CompetentAuthority,
+            N.NotificationType,
+            I.IsInterim) AS DATA;
+
+    DECLARE @activityId UNIQUEIDENTIFIER;
+    DECLARE @shipmentQuantityRangeId UNIQUEIDENTIFIER;
+
+    SELECT 
+        @activityId = Id
+    FROM 
+        [Lookup].[Activity]
+    WHERE
+        TradeDirection = @tradeDirection
+        AND NotificationType = @notificationType
+        AND IsInterim = @isInterim;
+        
+    SELECT
+        @shipmentQuantityRangeId = Id
+    FROM
+        [Lookup].[ShipmentQuantityRange]
+    WHERE
+         @numberOfShipments >= RangeFrom
+         AND (RangeTo IS NULL OR @numberOfShipments <= RangeTo);
+
+    DECLARE @price MONEY;
+    DECLARE @potentialRefund MONEY;
+
+    SELECT
+        @price = [Price],
+        @potentialRefund = [PotentialRefund]
+    FROM
+        [Lookup].[PricingStructure]
+    WHERE
+        CompetentAuthority = @competentAuthority
+        AND ShipmentQuantityRangeId = @shipmentQuantityRangeId
+        AND ActivityId = @activityId;
+
+    INSERT @PricingInfo
+    SELECT @price, @potentialRefund;
+
+    RETURN;
+END;
+GO
