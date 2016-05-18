63822a05588034eac9abed21f82409b8ded9e6e8
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-05-18 09:36:25 +0100

    Remove logic from consented date and make it the consent period from date directly

--- src/EA.Iws.Database/scripts/Everytime/Views/0020-Reports-DataImportNotifications.sql
+++ src/EA.Iws.Database/scripts/Everytime/Views/0020-Reports-DataImportNotifications.sql
@@ -15,12 +15,7 @@ AS
         NA.[CommencementDate] AS [AssessmentStarted],
         NA.[CompleteDate] AS [ApplicationCompleted],
         NA.[AcknowlegedDate] AS [Acknowledged],
-		-- If the notification is consented before the consent period begins
-		-- then the consented date is the start of the consent period
-		CASE
-			WHEN NA.[ConsentedDate] < C.[From] THEN C.[From]
-			ELSE NA.[ConsentedDate]
-		END AS [Consented],
+		C.[From] AS [Consented],
 		NA.[Officer],
         -- Decision date will be the date it was withdrawn, objected or consented and it will only be one of these.
         CAST(COALESCE(NA.WithdrawnDate, COALESCE(NA.ObjectedDate, NA.ConsentedDate)) AS DATE) AS [DecisionDate]
