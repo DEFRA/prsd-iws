8f99e7e368e6091ed139f61d1501a9f6f5d3c388
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-05-18 08:54:47 +0100

    Add DB view for Data Import Notification report

---
+++ src/EA.Iws.Database/scripts/Everytime/Views/0020-Reports-DataImportNotifications.sql
@@ -0,0 +1,38 @@
+﻿IF OBJECT_ID('[Reports].[DataImportNotifications]') IS NULL
+    EXEC('CREATE VIEW [Reports].[DataImportNotifications] AS SELECT 1 AS [NOTHING];')
+GO
+
+ALTER VIEW [Reports].[DataImportNotifications]
+AS
+    SELECT
+        N.[NotificationNumber],
+        N.[TypeId] AS [NotificationType],
+        N.[CompetentAuthorityId],
+        N.[Preconsented],
+        NA.[ImportStatusId] AS [Status],
+        NA.[ReceivedDate] AS [NotificationReceived],
+        NA.[PaymentReceivedDate] AS [PaymentReceived],
+        NA.[CommencementDate] AS [AssessmentStarted],
+        NA.[CompleteDate] AS [ApplicationCompleted],
+        NA.[AcknowlegedDate] AS [Acknowledged],
+		-- If the notification is consented before the consent period begins
+		-- then the consented date is the start of the consent period
+		CASE
+			WHEN NA.[ConsentedDate] < C.[From] THEN C.[From]
+			ELSE NA.[ConsentedDate]
+		END AS [Consented],
+		NA.[Officer],
+        -- Decision date will be the date it was withdrawn, objected or consented and it will only be one of these.
+        CAST(COALESCE(NA.WithdrawnDate, COALESCE(NA.ObjectedDate, NA.ConsentedDate)) AS DATE) AS [DecisionDate]
+
+    FROM		[Reports].[NotificationAssessment] AS NA
+
+    INNER JOIN	[Reports].[Notification] AS N
+    ON			[N].[Id] = [NA].[NotificationId]
+
+	LEFT JOIN	[ImportNotification].[Consent] AS C
+	ON			[NA].[NotificationId] = [C].[NotificationId]
+
+    WHERE		[NA].[ImportOrExport] = 'Import'
+    AND			[NA].[ImportStatusId] <> 1
+GO
