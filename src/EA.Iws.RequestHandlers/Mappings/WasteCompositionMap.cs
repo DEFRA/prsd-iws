d7134d09524e1fe2b222e3bd0bef2a7b9a7b8faa
David Raine <DavidR@sfwltd.co.uk>
2016-09-16 10:55:29 +0100

    Apply ordering to waste composition data

--- src/EA.Iws.RequestHandlers/Mappings/WasteCompositionMap.cs
+++ src/EA.Iws.RequestHandlers/Mappings/WasteCompositionMap.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Core.WasteType;
     using Domain.NotificationApplication;
     using Prsd.Core.Mapper;
@@ -14,64 +15,46 @@
     {
         public IList<WasteComposition> Map(IList<WasteTypeCompositionData> source)
         {
-            var wasteCompositions = new List<WasteComposition>();
-            foreach (var item in source)
-            {
-                wasteCompositions.Add(
-                    WasteComposition.CreateWasteComposition(
-                        item.Constituent, 
-                        Convert.ToDecimal(item.MinConcentration),
-                        Convert.ToDecimal(item.MaxConcentration), 
-                        item.ChemicalCompositionCategory));
-            }
-            return wasteCompositions;
+            return source.OrderBy(x => x.ChemicalCompositionCategory)
+                .Select(item => WasteComposition.CreateWasteComposition(
+                    item.Constituent, 
+                    Convert.ToDecimal(item.MinConcentration), 
+                    Convert.ToDecimal(item.MaxConcentration), 
+                    item.ChemicalCompositionCategory)).ToList();
         }
-        
+
         public IList<WasteAdditionalInformation> Map(IList<WoodInformationData> source)
         {
-            var wasteCompositions = new List<WasteAdditionalInformation>();
-            foreach (var item in source)
-            {
-                WasteAdditionalInformation.CreateWasteAdditionalInformation(
-                    item.Constituent,
-                    Convert.ToDecimal(item.MinConcentration),
-                    Convert.ToDecimal(item.MaxConcentration),
-                    item.WasteInformationType);
-            }
-
-            return wasteCompositions;
+            return source.OrderBy(x => x.WasteInformationType)
+                .Select(item => WasteAdditionalInformation.CreateWasteAdditionalInformation(
+                    item.Constituent, 
+                    Convert.ToDecimal(item.MinConcentration), 
+                    Convert.ToDecimal(item.MaxConcentration), 
+                    item.WasteInformationType)).ToList();
         }
 
         public IList<WasteCompositionData> Map(IEnumerable<WasteComposition> source)
         {
-            var wasteCompositions = new List<WasteCompositionData>();
-            foreach (var item in source)
-            {
-                wasteCompositions.Add(new WasteCompositionData
+            return source.OrderBy(x => x.ChemicalCompositionType)
+                .Select(item => new WasteCompositionData
                 {
                     ChemicalCompositionCategory = item.ChemicalCompositionType,
                     MinConcentration = item.MinConcentration,
                     MaxConcentration = item.MaxConcentration,
                     Constituent = item.Constituent
-                });
-            }
-            return wasteCompositions;
+                }).ToList();
         }
 
         public IList<WoodInformationData> Map(IEnumerable<WasteAdditionalInformation> source)
         {
-            var wasteCompositions = new List<WoodInformationData>();
-            foreach (var item in source)
-            {
-                wasteCompositions.Add(new WoodInformationData
+            return source.OrderBy(x => x.WasteInformationType)
+                .Select(item => new WoodInformationData
                 {
                     Constituent = item.Constituent,
                     WasteInformationType = item.WasteInformationType,
                     MinConcentration = item.MinConcentration.ToString(),
                     MaxConcentration = item.MaxConcentration.ToString()
-                });
-            }
-            return wasteCompositions;
+                }).ToList();
         }
     }
 }
