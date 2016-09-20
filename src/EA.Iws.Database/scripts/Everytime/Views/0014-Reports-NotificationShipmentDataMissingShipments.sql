b1f105b6bdb951ce1ee965920b47db85b8e3f90b
David Raine <DavidR@sfwltd.co.uk>
2016-09-20 16:10:35 +0100

    Rewrite report to not join on other views
    This report was having severe performance issues as it was joining on so many views which are not indexed. Rewritten to only select the data it needs which has sped it up massively.

--- src/EA.Iws.Database/scripts/Everytime/Views/0014-Reports-NotificationShipmentDataMissingShipments.sql
+++ src/EA.Iws.Database/scripts/Everytime/Views/0014-Reports-NotificationShipmentDataMissingShipments.sql
@@ -6,37 +6,140 @@ ALTER VIEW [Reports].[NotificationShipmentDataMissingShipments]
 AS
 	
 	SELECT	
-		N.Id AS [NotificationId],
+		M.NotificationId,
 		N.NotificationNumber,
-		N.CompetentAuthorityId,
-		O.Exporter,
-		O.Importer,
-		O.Facility,
-		M.ShipmentNumber,
-		M.ActualDateOfShipment,
-		N.ConsentFrom,
-		N.ConsentTo,
+		N.CompetentAuthority AS CompetentAuthorityId,
+		E.Name AS Exporter,
+		I.Name AS Importer,
+		F.Name AS Facility,
+		M.Number AS ShipmentNumber,
+		M.Date AS ActualDateOfShipment,
+		C.[From] AS [ConsentFrom],
+        C.[To] AS [ConsentTo],
 		M.PrenotificationDate,
-		M.ReceivedDate,
-		M.CompletedDate,
-		M.QuantityReceived,
-		M.QuantityReceivedUnit,
-		M.QuantityReceivedUnitId,
+		MR.Date AS ReceivedDate,
+		MOR.Date AS CompletedDate,
+		MR.Quantity AS QuantityReceived,
+		MR_U.Description AS [QuantityReceivedUnit],
+		MR_U.Id AS [QuantityReceivedUnitId],
 		CASE
-			WHEN W.ChemicalCompositionDescription IS NULL THEN W.ChemicalCompositionType
-			ELSE W.ChemicalCompositionType + ' - ' + W.ChemicalCompositionDescription
+			WHEN WT.ChemicalCompositionDescription IS NULL THEN CCT.Description
+			ELSE CCT.Description + ' - ' + WT.ChemicalCompositionDescription
 		END AS [ChemicalComposition],
-		N.LocalArea
+		LA.[Name] AS [LocalArea]
+	
+	FROM [Notification].[Movement] AS M
 
-	FROM		[Reports].[Notification] AS N
+	INNER JOIN [Notification].[Notification] AS N
+	ON M.NotificationId = N.Id
 
-	INNER JOIN	[Reports].[Movements] AS M
-	ON			[M].[NotificationId] = [N].[Id]
+	INNER JOIN	[Notification].[WasteType] AS WT 
+    ON			[WT].[NotificationId] = M.NotificationId
 
-	INNER JOIN	[Reports].[NotificationOrganisations] AS O
-	ON			[O].[Id] = [N].[Id]
+	INNER JOIN	[Lookup].[ChemicalCompositionType] AS CCT 
+    ON			[WT].[ChemicalCompositionType] = [CCT].[Id]
 
-	LEFT JOIN	[Reports].[WasteType] AS W
-	ON			[N].[Id] = [W].[NotificationId]
+	LEFT JOIN [Notification].[Exporter] AS E
+    ON E.[NotificationId] = M.NotificationId
+
+	LEFT JOIN [Notification].[Importer] AS I
+    ON I.[NotificationId] = M.NotificationId
+
+	LEFT JOIN	[Notification].[Facility] AS F
+    ON			F.Id = 
+                (
+                    SELECT TOP 1 F1.Id
+
+                    FROM		[Notification].[FacilityCollection] AS FC
+
+                    INNER JOIN	[Notification].[Facility] AS F1
+                    ON			FC.Id = F1.FacilityCollectionId
+
+                    WHERE		NotificationId = M.NotificationId
+                    ORDER BY	F1.IsActualSiteOfTreatment DESC
+                )
+
+	LEFT JOIN	[Notification].[MovementReceipt] AS MR
+	ON			[M].[Id] = [MR].[MovementId]
+
+	LEFT JOIN	[Notification].[MovementOperationReceipt] AS MOR
+	ON			[M].[Id] = [MOR].[MovementId]
+
+	LEFT JOIN	[Lookup].[ShipmentQuantityUnit] AS MR_U 
+	ON			[MR].[Unit] = [MR_U].[Id]
+
+	LEFT JOIN	[Notification].[Consent] AS C
+    ON			M.NotificationId = [C].[NotificationApplicationId]
+
+	LEFT JOIN	[Notification].[Consultation] AS CON
+	ON			[CON].[NotificationId] = M.NotificationId
+
+    LEFT JOIN	[Lookup].[LocalArea] AS LA
+    ON			[CON].[LocalAreaId] = [LA].[Id]
+
+	UNION 
+
+		SELECT	
+		M.NotificationId,
+		N.NotificationNumber,
+		N.CompetentAuthority AS CompetentAuthorityId,
+		E.Name AS Exporter,
+		I.Name AS Importer,
+		F.Name AS Facility,
+		M.Number AS ShipmentNumber,
+		M.ActualShipmentDate AS ActualDateOfShipment,
+		C.[From] AS [ConsentFrom],
+        C.[To] AS [ConsentTo],
+		M.PrenotificationDate,
+		MR.Date AS ReceivedDate,
+		MOR.Date AS CompletedDate,
+		MR.Quantity AS QuantityReceived,
+		MR_U.Description AS [QuantityReceivedUnit],
+		MR_U.Id AS [QuantityReceivedUnitId],
+		NULL AS [ChemicalComposition],
+		LA.[Name] AS [LocalArea]
+	
+	FROM [ImportNotification].[Movement] AS M
+
+	INNER JOIN [ImportNotification].[Notification] AS N
+	ON M.NotificationId = N.Id
+
+	LEFT JOIN [ImportNotification].[Exporter] AS E
+    ON E.[ImportNotificationId] = M.NotificationId
+
+	LEFT JOIN [ImportNotification].[Importer] AS I
+    ON I.[ImportNotificationId] = M.NotificationId
+
+	LEFT JOIN	[ImportNotification].[Facility] AS F
+    ON			F.Id = 
+                (
+                    SELECT TOP 1 F1.Id
+
+                    FROM		[ImportNotification].[FacilityCollection] AS FC
+
+                    INNER JOIN	[ImportNotification].[Facility] AS F1
+                    ON			FC.Id = F1.FacilityCollectionId
+
+                    WHERE		NotificationId = M.NotificationId
+                    ORDER BY	F1.IsActualSiteOfTreatment DESC
+                )
+
+	LEFT JOIN	[ImportNotification].[MovementReceipt] AS MR
+	ON			[M].[Id] = [MR].[MovementId]
+
+	LEFT JOIN	[ImportNotification].[MovementOperationReceipt] AS MOR
+	ON			[M].[Id] = [MOR].[MovementId]
+
+	LEFT JOIN	[Lookup].[ShipmentQuantityUnit] AS MR_U 
+	ON			[MR].[Unit] = [MR_U].[Id]
+
+	LEFT JOIN	[ImportNotification].[Consent] AS C
+    ON			M.NotificationId = [C].[NotificationId]
+
+	LEFT JOIN	[ImportNotification].[Consultation] AS CON
+	ON			[CON].[NotificationId] = M.NotificationId
+
+    LEFT JOIN	[Lookup].[LocalArea] AS LA
+    ON			[CON].[LocalAreaId] = [LA].[Id]
 
 GO
