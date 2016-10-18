3899a756f934ab22902d6b7f2879a9046c60631b
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-10-18 15:30:38 +0100

    Order the interim R and D codes frist when listed on the notification document

--- src/EA.Iws.DocumentGeneration.Tests.Unit/Formatters/OperationInfoFormatterTests.cs
+++ src/EA.Iws.DocumentGeneration.Tests.Unit/Formatters/OperationInfoFormatterTests.cs
@@ -43,10 +43,10 @@
             var result = formatter.OperationInfosToCommaDelimitedList(new[]
             {
                 new TestableOperationInfo { OperationCode = OperationCode.R1 },
-                new TestableOperationInfo { OperationCode = OperationCode.R2 }
+                new TestableOperationInfo { OperationCode = OperationCode.R11 }
             });
 
-            Assert.Equal("R1, R2", result);
+            Assert.Equal("R1, R11", result);
         }
 
         [Fact]
@@ -54,12 +54,113 @@
         {
             var result = formatter.OperationInfosToCommaDelimitedList(new[]
             {
-                new TestableOperationInfo { OperationCode = OperationCode.D12 },
+                new TestableOperationInfo { OperationCode = OperationCode.D11 },
                 new TestableOperationInfo { OperationCode = OperationCode.D3 },
                 new TestableOperationInfo { OperationCode = OperationCode.D7 },
             });
 
-            Assert.Equal("D3, D7, D12", result);
+            Assert.Equal("D3, D7, D11", result);
+        }
+
+        [Fact]
+        public void OneR_Interim_OrdersFirst()
+        {
+            var result = formatter.OperationInfosToCommaDelimitedList(new[]
+            {
+                new TestableOperationInfo { OperationCode = OperationCode.R5 },
+                new TestableOperationInfo { OperationCode = OperationCode.R1 },
+                new TestableOperationInfo { OperationCode = OperationCode.R12 },
+            });
+
+            Assert.Equal("R12, R1, R5", result);
+        }
+
+        [Fact]
+        public void TwoR_Interims_OrdersFirst()
+        {
+            var result = formatter.OperationInfosToCommaDelimitedList(new[]
+            {
+                new TestableOperationInfo { OperationCode = OperationCode.R5 },
+                new TestableOperationInfo { OperationCode = OperationCode.R13 },
+                new TestableOperationInfo { OperationCode = OperationCode.R12 },
+            });
+
+            Assert.Equal("R12, R13, R5", result);
+        }
+
+        [Fact]
+        public void OneD_Interim_OrdersFirst()
+        {
+            var result = formatter.OperationInfosToCommaDelimitedList(new[]
+            {
+                new TestableOperationInfo { OperationCode = OperationCode.D5 },
+                new TestableOperationInfo { OperationCode = OperationCode.D1 },
+                new TestableOperationInfo { OperationCode = OperationCode.D12 },
+            });
+
+            Assert.Equal("D12, D1, D5", result);
+        }
+
+        [Fact]
+        public void ThreeD_Interims_OrdersFirst()
+        {
+            var result = formatter.OperationInfosToCommaDelimitedList(new[]
+            {
+                new TestableOperationInfo { OperationCode = OperationCode.D5 },
+                new TestableOperationInfo { OperationCode = OperationCode.D15 },
+                new TestableOperationInfo { OperationCode = OperationCode.D14 },
+                new TestableOperationInfo { OperationCode = OperationCode.D12 }
+            });
+
+            Assert.Equal("D12, D14, D15, D5", result);
+        }
+
+        [Fact]
+        public void AllD_Interims_OrdersFirst()
+        {
+            var result = formatter.OperationInfosToCommaDelimitedList(new[]
+            {
+                new TestableOperationInfo { OperationCode = OperationCode.D5 },
+                new TestableOperationInfo { OperationCode = OperationCode.D4 },
+                new TestableOperationInfo { OperationCode = OperationCode.D7 },
+                new TestableOperationInfo { OperationCode = OperationCode.D13 },
+                new TestableOperationInfo { OperationCode = OperationCode.D2 },
+                new TestableOperationInfo { OperationCode = OperationCode.D10 },
+                new TestableOperationInfo { OperationCode = OperationCode.D3 },
+                new TestableOperationInfo { OperationCode = OperationCode.D1 },
+                new TestableOperationInfo { OperationCode = OperationCode.D15 },
+                new TestableOperationInfo { OperationCode = OperationCode.D8 },
+                new TestableOperationInfo { OperationCode = OperationCode.D12 },
+                new TestableOperationInfo { OperationCode = OperationCode.D9 },
+                new TestableOperationInfo { OperationCode = OperationCode.D11 },
+                new TestableOperationInfo { OperationCode = OperationCode.D14 },
+                new TestableOperationInfo { OperationCode = OperationCode.D6 }
+            });
+
+            Assert.Equal("D12, D13, D14, D15, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11", result);
+        }
+
+        [Fact]
+        public void ALLR_Interim_OrdersFirst()
+        {
+            var result = formatter.OperationInfosToCommaDelimitedList(new[]
+            {
+                new TestableOperationInfo { OperationCode = OperationCode.R5 },
+                new TestableOperationInfo { OperationCode = OperationCode.R1 },
+                new TestableOperationInfo { OperationCode = OperationCode.R12 },
+                new TestableOperationInfo { OperationCode = OperationCode.R9 },
+                new TestableOperationInfo { OperationCode = OperationCode.R6 },
+                new TestableOperationInfo { OperationCode = OperationCode.R8 },
+                new TestableOperationInfo { OperationCode = OperationCode.R3 },
+                new TestableOperationInfo { OperationCode = OperationCode.R7 },
+                new TestableOperationInfo { OperationCode = OperationCode.R10 },
+                new TestableOperationInfo { OperationCode = OperationCode.R4 },
+                new TestableOperationInfo { OperationCode = OperationCode.R13 },
+                new TestableOperationInfo { OperationCode = OperationCode.R2 },
+                new TestableOperationInfo { OperationCode = OperationCode.R11 },
+            });
+
+            Assert.Equal("R12, R13, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11", result);
         }
     }
 }
--- src/EA.Iws.DocumentGeneration/Formatters/OperationInfoFormatter.cs
+++ src/EA.Iws.DocumentGeneration/Formatters/OperationInfoFormatter.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections.Generic;
     using System.Linq;
+    using Core.OperationCodes;
     using Domain.NotificationApplication;
     using Prsd.Core.Helpers;
 
@@ -15,9 +16,44 @@
                 return string.Empty;
             }
 
-            return string.Join(", ", operationInfos
-                .OrderBy(c => c.OperationCode)
-                .Select(v => EnumHelper.GetDisplayName(v.OperationCode)));
+            var operationInfosList = operationInfos.ToList();
+            operationInfosList.Sort(InterimsFirst);
+
+            return string.Join(", ", operationInfosList.Select(v => EnumHelper.GetDisplayName(v.OperationCode)));
         }
+
+        private bool IsInterimCode(OperationCode code)
+        {
+            if (code == OperationCode.R12 || code == OperationCode.R13 || code == OperationCode.D15 || code == OperationCode.D14 || code == OperationCode.D13 || code == OperationCode.D12)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private int InterimsFirst(OperationInfo x, OperationInfo y)
+        {
+            if (IsInterimCode(x.OperationCode) && IsInterimCode(y.OperationCode))
+            {
+                return x.OperationCode.CompareTo(y.OperationCode);
+            }
+
+            if (!IsInterimCode(x.OperationCode) && !IsInterimCode(y.OperationCode))
+            {
+                return x.OperationCode.CompareTo(y.OperationCode);
+            }
+
+            if (IsInterimCode(x.OperationCode) && !IsInterimCode(y.OperationCode))
+            {
+                return -1;
+            }
+
+            if (!IsInterimCode(x.OperationCode) && IsInterimCode(y.OperationCode))
+            {
+                return 1;
+            }
+            
+            return 0;
+        } 
     }
 }
