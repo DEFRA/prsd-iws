6a5db3973c5c1bbde09afaf85c3576fcbd4a3ce9
David Raine <DavidR@sfwltd.co.uk>
2016-10-27 15:44:13 +0100

    Make dates nullable

--- src/EA.Iws.Core/Admin/KeyDates/KeyDatesOverrideData.cs
+++ src/EA.Iws.Core/Admin/KeyDates/KeyDatesOverrideData.cs
@@ -6,24 +6,24 @@
     {
         public Guid NotificationId { get; set; }
 
-        public DateTime NotificationReceivedDate { get; set; }
+        public DateTime? NotificationReceivedDate { get; set; }
 
-        public DateTime CommencementDate { get; set; }
+        public DateTime? CommencementDate { get; set; }
 
-        public DateTime CompleteDate { get; set; }
+        public DateTime? CompleteDate { get; set; }
 
-        public DateTime TransmittedDate { get; set; }
+        public DateTime? TransmittedDate { get; set; }
 
-        public DateTime AcknowledgedDate { get; set; }
+        public DateTime? AcknowledgedDate { get; set; }
 
-        public DateTime WithdrawnDate { get; set; }
+        public DateTime? WithdrawnDate { get; set; }
 
-        public DateTime ObjectedDate { get; set; }
+        public DateTime? ObjectedDate { get; set; }
 
-        public DateTime ConsentedDate { get; set; }
+        public DateTime? ConsentedDate { get; set; }
 
-        public DateTime ConsentValidFromDate { get; set; }
+        public DateTime? ConsentValidFromDate { get; set; }
 
-        public DateTime ConsentValidToDate { get; set; }
+        public DateTime? ConsentValidToDate { get; set; }
     }
 }
