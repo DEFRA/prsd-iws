afc8fe355d6d009122546a0216031afef8325478
David Raine <DavidR@sfwltd.co.uk>
2016-11-01 11:53:59 +0000

    Add logic to override import notification key dates

---
+++ src/EA.Iws.RequestHandlers/Admin/KeyDates/GetImportKeyDatesOverrideDataHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Iws.RequestHandlers.Admin.KeyDates
+{
+    using System.Threading.Tasks;
+    using Core.Admin.KeyDates;
+    using Domain.ImportNotificationAssessment;
+    using Prsd.Core.Mediator;
+    using Requests.Admin.KeyDates;
+
+    public class GetImportKeyDatesOverrideDataHandler :
+        IRequestHandler<GetImportKeyDatesOverrideData, KeyDatesOverrideData>
+    {
+        private readonly IKeyDatesOverrideRepository repository;
+
+        public GetImportKeyDatesOverrideDataHandler(IKeyDatesOverrideRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<KeyDatesOverrideData> HandleAsync(GetImportKeyDatesOverrideData message)
+        {
+            return await repository.GetKeyDatesForNotification(message.NotificationId);
+        }
+    }
+}
