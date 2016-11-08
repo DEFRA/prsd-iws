6695d6d7936fcfe1d55a92a0c8140c14c6afde95
David Raine <DavidR@sfwltd.co.uk>
2016-11-08 17:18:31 +0000

    Check notification status for internally captured movement

--- src/EA.Iws.Domain/Movement/CapturedMovementFactory.cs
+++ src/EA.Iws.Domain/Movement/CapturedMovementFactory.cs
@@ -3,15 +3,19 @@
     using System;
     using System.Threading.Tasks;
     using Core.ComponentRegistration;
+    using Core.NotificationAssessment;
+    using NotificationAssessment;
 
     [AutoRegister]
     public class CapturedMovementFactory : ICapturedMovementFactory
     {
         private readonly IMovementNumberValidator movementNumberValidator;
+        private readonly INotificationAssessmentRepository assessmentRepository;
 
-        public CapturedMovementFactory(IMovementNumberValidator movementNumberValidator)
+        public CapturedMovementFactory(IMovementNumberValidator movementNumberValidator, INotificationAssessmentRepository assessmentRepository)
         {
             this.movementNumberValidator = movementNumberValidator;
+            this.assessmentRepository = assessmentRepository;
         }
 
         public async Task<Movement> Create(Guid notificationId, int number, DateTime? prenotificationDate, DateTime actualShipmentDate)
@@ -19,6 +23,15 @@
             if (!await movementNumberValidator.Validate(notificationId, number))
             {
                 throw new MovementNumberException("Cannot create a movement with a conflicting movement number (" + number + ") for notification: " + notificationId);
+            }
+
+            var notificationStatus = (await assessmentRepository.GetByNotificationId(notificationId)).Status;
+
+            if (notificationStatus != NotificationStatus.Consented)
+            {
+                throw new InvalidOperationException(
+                    string.Format("Cannot create a movement for notification {0} because its status is {1}",
+                        notificationId, notificationStatus));
             }
 
             var movement = Movement.Capture(number, notificationId, actualShipmentDate, prenotificationDate);
