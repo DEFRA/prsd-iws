c96f4c67e8c807433810292c4ec014a0c50506d0
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-11-09 15:20:58 +0000

    Add select movement screen for delete import movement journey

---
+++ src/EA.Iws.Web/Areas/AdminImportNotificationMovements/Controllers/DeleteController.cs
@@ -0,0 +1,49 @@
+﻿namespace EA.Iws.Web.Areas.AdminImportNotificationMovements.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Core.Authorization.Permissions;
+    using Infrastructure.Authorization;
+    using Prsd.Core.Mediator;
+    using Requests.ImportMovement.Capture;
+    using ViewModels.Delete;
+
+    [AuthorizeActivity(UserAdministrationPermissions.CanDeleteMovements)]
+    public class DeleteController : Controller
+    {
+        private const string MovementNumberKey = "MovementNumberKey";
+        private readonly IMediator mediator;
+
+        public DeleteController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public ActionResult Index(Guid id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index(Guid id, IndexViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var movementId = await mediator.SendAsync(new GetImportMovementIdIfExists(id, model.Number.Value));
+
+            if (movementId.HasValue)
+            {
+                TempData[MovementNumberKey] = model.Number;
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
