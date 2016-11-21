cf34180a3d8630deba36dbc034745aeae97b2745
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-11-21 15:52:11 +0000

    Add shipment history tables for import and export notifications

---
+++ src/EA.Iws.Database/scripts/Update/0002-CI/Sprint4/20161121-153500-add-shipment-history-tables.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [ImportNotification].[ShipmentNumberHistory] (
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [NumberOfShipments] INT NOT NULL,
+    [DateChanged] DATE NOT NULL,
+    [ImportNotificationId] UNIQUEIDENTIFIER NOT NULL,
+    [RowVersion] ROWVERSION NOT NULL, 
+	CONSTRAINT [PK_ImportShipmentNumberHistory] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT FK_ImportShipmentNumberHistory_ImportNotification FOREIGN KEY ([ImportNotificationId]) REFERENCES [ImportNotification].[Notification]([Id]),
+);
+
+GO
+
+CREATE TABLE [Notification].[ShipmentNumberHistory] (
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [NumberOfShipments] INT NOT NULL,
+    [DateChanged] DATE NOT NULL,
+    [NotificationId] UNIQUEIDENTIFIER NOT NULL,
+    [RowVersion] ROWVERSION NOT NULL, 
+	CONSTRAINT [PK_ExportShipmentNumberHistory] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT FK_ExportShipmentNumberHistory_Notification FOREIGN KEY ([NotificationId]) REFERENCES [Notification].[Notification]([Id]),
+);
