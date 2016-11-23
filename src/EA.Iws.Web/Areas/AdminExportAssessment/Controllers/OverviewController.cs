947e3a04d5f39212c873bf65aa7fe06c00b9d898
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-11-23 11:48:13 +0000

    Finish adding Change link on notification overview for admin users to change the number of shipments

--- src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/OverviewController.cs
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/OverviewController.cs
@@ -3,26 +3,34 @@
     using System;
     using System.Threading.Tasks;
     using System.Web.Mvc;
+    using Infrastructure.Authorization;
     using NotificationApplication.ViewModels.NotificationApplication;
     using Prsd.Core.Mediator;
     using Requests.Notification;
+    using Requests.NotificationAssessment;
 
     [Authorize(Roles = "internal")]
     public class OverviewController : Controller
     {
+        private readonly AuthorizationService authorizationService;
         private readonly IMediator mediator;
 
-        public OverviewController(IMediator mediator)
+        public OverviewController(IMediator mediator, AuthorizationService authorizationService)
         {
             this.mediator = mediator;
+            this.authorizationService = authorizationService;
         }
 
         [HttpGet]
         public async Task<ActionResult> Index(Guid id)
         {
             var result = await mediator.SendAsync(new GetNotificationOverviewInternal(id));
+            var showLink = Task.Run(() => authorizationService.AuthorizeActivity(typeof(GetOriginalNumberOfShipments))).Result;
 
-            return View(new NotificationOverviewViewModel(result));
+            var model = new NotificationOverviewViewModel(result);
+            model.AmountsAndDatesViewModel.ShowChangeShipmentNumberLink = showLink;
+
+            return View(model);
         }
     }
 }
