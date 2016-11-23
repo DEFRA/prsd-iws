8c9530321b9a4fbb43d2ba2c7ec47c094e7529e3
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-11-23 16:18:08 +0000

    Add change shipment number link for import notifications

---
+++ src/EA.Iws.Web/Areas/ImportNotification/Controllers/ShipmentNumberController.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Iws.Web.Areas.ImportNotification.Controllers
+{
+    using System.Web.Mvc;
+    using Core.Authorization.Permissions;
+    using Infrastructure.Authorization;
+
+    [AuthorizeActivity(ImportNotificationPermissions.CanChangeNumberOfShipmentsOnImportNotification)]
+    public class ShipmentNumberController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
