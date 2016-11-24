d83fa7242b58113a8dbd2d7a2e504b28416ff4f2
David Raine <DavidR@sfwltd.co.uk>
2016-11-24 17:35:32 +0000

    WIP - FG assessment UI

---
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/FinancialGuaranteeAssessmentController.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Iws.Web.Areas.AdminExportAssessment.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    [Authorize(Roles = "internal")]
+    public class FinancialGuaranteeAssessmentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(Guid id)
+        {
+            return View();
+        }
+    }
+}
