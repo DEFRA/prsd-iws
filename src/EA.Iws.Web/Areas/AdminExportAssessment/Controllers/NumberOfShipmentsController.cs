763ca51a14d548dd0ffd535809f02521d20e4d14
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-11-24 14:57:24 +0000

    Add screen to input new number of shipments for export notifications

--- src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/NumberOfShipmentsController.cs
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/NumberOfShipmentsController.cs
@@ -1,14 +1,34 @@
 ﻿namespace EA.Iws.Web.Areas.AdminExportAssessment.Controllers
 {
+    using System;
     using System.Web.Mvc;
     using Core.Authorization.Permissions;
     using Infrastructure.Authorization;
+    using ViewModels.NumberOfShipments;
 
     [AuthorizeActivity(ExportNotificationPermissions.CanChangeNumberOfShipmentsOnExportNotification)]
     public class NumberOfShipmentsController : Controller
     {
         [HttpGet]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Guid id, IndexViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Confirm");
+        }
+
+        [HttpGet]
+        public ActionResult Confirm()
         {
             return View();
         }
