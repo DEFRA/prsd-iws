ee8d695fb870d135a836a052db2dde57a65c8352
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-11-30 15:18:43 +0000

    Add request and handler for import notification confirm number of shipments change

---
+++ src/EA.Iws.Requests/ImportNotification/GetChangeNumberOfShipmentConfrimationData.cs
@@ -0,0 +1,19 @@
+﻿namespace EA.Iws.Requests.ImportNotification
+{
+    using System;
+    using Core.ImportNotification;
+    using Prsd.Core.Mediator;
+
+    public class GetChangeNumberOfShipmentConfrimationData : IRequest<ConfirmNumberOfShipmentsChangeData>
+    {
+        public GetChangeNumberOfShipmentConfrimationData(Guid notificationId, int numberOfShipments)
+        {
+            NotificationId = notificationId;
+            NumberOfShipments = numberOfShipments;
+        }
+
+        public Guid NotificationId { get; private set; }
+
+        public int NumberOfShipments { get; private set; }
+    }
+}
