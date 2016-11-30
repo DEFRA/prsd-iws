20143c5b5cbb925310cb2f578bd0ba5879232387
David Raine <DavidR@sfwltd.co.uk>
2016-11-30 10:13:23 +0000

    WIP - record new financial guarantee

--- src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/FinancialGuaranteeAssessmentController.cs
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/FinancialGuaranteeAssessmentController.cs
@@ -5,6 +5,7 @@
     using System.Web.Mvc;
     using Prsd.Core.Mediator;
     using Requests.Admin.FinancialGuarantee;
+    using ViewModels.FinancialGuaranteeAssessment;
 
     [Authorize(Roles = "internal")]
     public class FinancialGuaranteeAssessmentController : Controller
@@ -22,6 +23,26 @@
             var currentFinancialGuarantee = await mediator.SendAsync(new GetCurrentFinancialGuaranteeDetails(id));
 
             return View(currentFinancialGuarantee);
+        }
+
+        [HttpGet]
+        public ActionResult New(Guid id)
+        {
+            return View(new NewFinancialGuaranteeViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> New(Guid id, NewFinancialGuaranteeViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await Task.Yield();
+
+            return View(model);
         }
     }
 }
---
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/ViewModels/FinancialGuaranteeAssessment/NewFinancialGuaranteeViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace EA.Iws.Web.Areas.AdminExportAssessment.ViewModels.FinancialGuaranteeAssessment
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using Web.ViewModels.Shared;
+
+    public class NewFinancialGuaranteeViewModel : IValidatableObject
+    {
+        [Required]
+        [DisplayName("Guarantee received")]
+        public OptionalDateInputViewModel ReceivedDate { get; set; }
+
+        [Required]
+        [DisplayName("Guarantee complete")]
+        public OptionalDateInputViewModel CompletedDate { get; set; }
+
+        public NewFinancialGuaranteeViewModel()
+        {
+            ReceivedDate = new OptionalDateInputViewModel(allowPastDates: true);
+            CompletedDate = new OptionalDateInputViewModel(allowPastDates: true);
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!ReceivedDate.IsCompleted)
+            {
+                yield return new ValidationResult("Received date is required", new[] { "ReceivedDate.Day" });
+            }
+
+            if (ReceivedDate.IsCompleted && CompletedDate.IsCompleted && ReceivedDate.AsDateTime() > CompletedDate.AsDateTime())
+            {
+                yield return new ValidationResult("Received date must be before completed date", new[] { "ReceivedDate.Day" });
+            }
+        }
+    }
+}
