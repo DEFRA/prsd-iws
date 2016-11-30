d5aced2fd397cdbb76aba00ae0b1ad1163c6c3bc
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-11-30 14:41:13 +0000

    Add request and handler for export notification confirm number of shipments change

---
+++ src/EA.Iws.Requests/NotificationAssessment/GetChangeNumberOfShipmentConfrimationData.cs
@@ -0,0 +1,22 @@
+﻿namespace EA.Iws.Requests.NotificationAssessment
+{
+    using System;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
+    using Core.NotificationAssessment;
+    using Prsd.Core.Mediator;
+
+    [RequestAuthorization(ExportNotificationPermissions.CanChangeNumberOfShipmentsOnExportNotification)]
+    public class GetChangeNumberOfShipmentConfrimationData : IRequest<ConfirmNumberOfShipmentsChangeData>
+    {
+        public GetChangeNumberOfShipmentConfrimationData(Guid notificationId, int numberOfShipments)
+        {
+            NotificationId = notificationId;
+            NumberOfShipments = numberOfShipments;
+        }
+
+        public Guid NotificationId { get; private set; }
+
+        public int NumberOfShipments { get; private set; }
+    }
+}
