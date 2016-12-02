79329556f3bf39d343e0ee2b0c5ac702ae4ecf39
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-12-02 10:14:43 +0000

    Add validation to import notification consent withdrawn decision

--- src/EA.Iws.Web/Areas/AdminImportAssessment/ViewModels/Decision/DecisionViewModel.cs
+++ src/EA.Iws.Web/Areas/AdminImportAssessment/ViewModels/Decision/DecisionViewModel.cs
@@ -4,7 +4,6 @@
     using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Web.Mvc;
-    using AdminExportAssessment.ViewModels.Decision;
     using Core.Admin;
     using Core.ImportNotificationAssessment;
     using Prsd.Core;
