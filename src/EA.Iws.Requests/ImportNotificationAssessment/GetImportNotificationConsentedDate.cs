423742b2567efd6517831d4f79c2cc6c56a37a89
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-12-02 10:13:53 +0000

    Add request and handler to get consented date for import notification

---
+++ src/EA.Iws.Requests/ImportNotificationAssessment/GetImportNotificationConsentedDate.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Iws.Requests.ImportNotificationAssessment
+{
+    using System;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
+    using Prsd.Core.Mediator;
+
+    [RequestAuthorization(ImportNotificationPermissions.CanReadImportNotificationAssessment)]
+    public class GetImportNotificationConsentedDate : IRequest<DateTime?>
+    {
+        public Guid NotificationId { get; private set; }
+
+        public GetImportNotificationConsentedDate(Guid notificationId)
+        {
+            NotificationId = notificationId;
+        }
+    }
+}
