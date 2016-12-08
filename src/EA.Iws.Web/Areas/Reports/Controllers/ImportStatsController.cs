e7a469bedff42085814caffcd7016fe2a2270717
David Raine <DavidR@sfwltd.co.uk>
2016-12-08 15:39:56 +0000

    Add UI for import stats report

---
+++ src/EA.Iws.Web/Areas/Reports/Controllers/ImportStatsController.cs
@@ -0,0 +1,46 @@
+﻿namespace EA.Iws.Web.Areas.Reports.Controllers
+{
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Core.Admin.Reports;
+    using Infrastructure;
+    using Prsd.Core.Mediator;
+    using Requests.Admin.Reports;
+    using ViewModels.ImportStats;
+
+    [Authorize(Roles = "internal")]
+    public class ImportStatsController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public ImportStatsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new IndexViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index(IndexViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var from = model.From.AsDateTime().Value;
+            var to = model.To.AsDateTime().Value;
+
+            var report = await mediator.SendAsync(new GetImportStatsReport(from, to));
+
+            var fileName = string.Format("import-stats-{0}-{1}.xlsx", from.ToShortDateString(), to.ToShortDateString());
+
+            return new XlsxActionResult<ImportStatsData>(report, fileName);
+        }
+    }
+}
---
+++ src/EA.Iws.Web/Areas/Reports/ViewModels/ImportStats/IndexViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace EA.Iws.Web.Areas.Reports.ViewModels.ImportStats
+{
+    using System.ComponentModel.DataAnnotations;
+    using Infrastructure.Validation;
+    using Web.ViewModels.Shared;
+
+    public class IndexViewModel
+    {
+        public IndexViewModel()
+        {
+            From = new OptionalDateInputViewModel(true);
+            To = new OptionalDateInputViewModel(true);
+        }
+
+        [Display(Name = "From", ResourceType = typeof(IndexViewModelResources))]
+        [RequiredDateInput(ErrorMessageResourceName = "FromRequired",
+            ErrorMessageResourceType = typeof(IndexViewModelResources))]
+        public OptionalDateInputViewModel From { get; set; }
+
+        [Display(Name = "To", ResourceType = typeof(IndexViewModelResources))]
+        [RequiredDateInput(ErrorMessageResourceName = "ToRequired",
+            ErrorMessageResourceType = typeof(IndexViewModelResources))]
+        public OptionalDateInputViewModel To { get; set; }
+    }
+}
