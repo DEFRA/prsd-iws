6e6c8783e4fae42af5dd094886094c8856e81a41
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-12-08 12:01:38 +0000

    Add request and handler and update repository to return the notification type

---
+++ src/EA.Iws.Requests/Notification/GetNotificationType.cs
@@ -0,0 +1,19 @@
+﻿namespace EA.Iws.Requests.Notification
+{
+    using System;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
+    using Core.Shared;
+    using Prsd.Core.Mediator;
+
+    [RequestAuthorization(ExportNotificationPermissions.CanReadExportNotification)]
+    public class GetNotificationType : IRequest<NotificationType>
+    {
+        public GetNotificationType(Guid notificationId)
+        {
+            NotificationId = notificationId;
+        }
+
+        public Guid NotificationId { get; private set; }
+    }
+}
