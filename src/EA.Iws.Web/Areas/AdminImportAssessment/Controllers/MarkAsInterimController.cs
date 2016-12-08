2d5fe9e328357cdfaaece35d0a90f36583b6b8b1
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-12-08 16:32:46 +0000

    Add link to import side nav to change the interim status

---
+++ src/EA.Iws.Web/Areas/AdminImportAssessment/Controllers/MarkAsInterimController.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Iws.Web.Areas.AdminImportAssessment.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    [Authorize(Roles = "internal")]
+    public class MarkAsInterimController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(Guid id)
+        {
+            return View();
+        }
+    }
+}
