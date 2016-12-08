8e859176380575d8294dc3707c11b6d3ff68fc0c
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-12-08 10:13:37 +0000

    Add request and handler to update interim status on export notifications

---
+++ src/EA.Iws.Requests/NotificationAssessment/UpdateInterimStatus.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Iws.Requests.NotificationAssessment
+{
+    using System;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
+    using Prsd.Core.Mediator;
+
+    [RequestAuthorization(UserAdministrationPermissions.CanUpdateInterimStatus)]
+    public class UpdateInterimStatus : IRequest<bool>
+    {
+        public UpdateInterimStatus(Guid notificationId, bool isInterim)
+        {
+            NotificationId = notificationId;
+            IsInterim = isInterim;
+        }
+
+        public Guid NotificationId { get; private set; }
+
+        public bool IsInterim { get; private set; }
+    }
+}
