c24b3d75ed19032a38e43f10b5ecdb6855437908
Charlie Lusty <charles.lusty@gmail.com>
2016-12-09 09:34:48 +0000

    Add request and handler and update the repository to enable changing the interim status on import notifications

---
+++ src/EA.Iws.Requests/ImportNotificationAssessment/UpdateInterimStatus.cs
@@ -0,0 +1,21 @@
+﻿namespace EA.Iws.Requests.ImportNotificationAssessment
+{
+    using System;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
+    using Prsd.Core.Mediator;
+
+    [RequestAuthorization(UserAdministrationPermissions.CanUpdateInterimStatus)]
+    public class UpdateInterimStatus : IRequest<bool>
+    {
+        public UpdateInterimStatus(Guid importNotificationId, bool isInterim)
+        {
+            ImportNotificationId = importNotificationId;
+            IsInterim = isInterim;
+        }
+
+        public Guid ImportNotificationId { get; private set; }
+
+        public bool IsInterim { get; private set; }
+    }
+}
