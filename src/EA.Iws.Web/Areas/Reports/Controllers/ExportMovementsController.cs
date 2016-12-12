964b9ab5539532554a58fb96b2c8591426b62c54
David Raine <DavidR@sfwltd.co.uk>
2016-12-12 16:00:44 +0000

    Add UI to download report

---
+++ src/EA.Iws.Web/Areas/Reports/Controllers/ExportMovementsController.cs
@@ -0,0 +1,48 @@
+﻿namespace EA.Iws.Web.Areas.Reports.Controllers
+{
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Core.Admin.Reports;
+    using Infrastructure;
+    using Prsd.Core.Mediator;
+    using Requests.Admin.Reports;
+    using ViewModels.ExportMovements;
+
+    [Authorize(Roles = "internal")]
+    public class ExportMovementsController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public ExportMovementsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new IndexViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index(IndexViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var from = model.From.AsDateTime().Value;
+            var to = model.To.AsDateTime().Value;
+
+            var report = await mediator.SendAsync(new GetExportMovementsReport(from, to));
+
+            var filename = string.Format("export-movement-documents-input-{0}-{1}.xlsx",
+                from.ToShortDateString(),
+                to.ToShortDateString());
+
+            return new XlsxActionResult<ExportMovementsData>(new[] { report }, filename);
+        }
+    }
+}
