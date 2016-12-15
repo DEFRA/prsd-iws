006db9b25d53980096b67157586d761e1f7b1e74
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-12-15 13:33:37 +0000

    Add UI to confirm and delete export notification payment

--- src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/DeleteTransactionController.cs
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/DeleteTransactionController.cs
@@ -27,5 +27,24 @@
 
             return View(model);
         }
+
+        [HttpGet]
+        public async Task<ActionResult> Confirm(Guid id, Guid transactionId)
+        {
+            var transaction = await mediator.SendAsync(new GetTransactionById(transactionId));
+
+            var model = new ConfirmViewModel(id, transaction);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Confirm(ConfirmViewModel model)
+        {
+            await mediator.SendAsync(new DeleteTransaction(model.TransactionId));
+
+            return RedirectToAction("Index", "AccountManagement");
+        }
     }
 }
