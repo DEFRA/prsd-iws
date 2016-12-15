b54c2fb83c1f6fcdac117c4080bfd6f0e53d5b88
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-12-15 10:52:58 +0000

    Add request and handler to get export notification transactions

---
+++ src/EA.Iws.RequestHandlers/Mappings/NotificationAssessment/NotificationTransactionMap.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Iws.RequestHandlers.Mappings.NotificationAssessment
+{
+    using Core.NotificationAssessment;
+    using Core.Shared;
+    using Domain.NotificationAssessment;
+    using Prsd.Core.Mapper;
+
+    internal class NotificationTransactionMap : IMap<NotificationTransaction, TransactionRecordData>
+    {
+        public TransactionRecordData Map(NotificationTransaction source)
+        {
+            return new TransactionRecordData
+            {
+                Amount = source.Credit.HasValue ? source.Credit.Value : source.Debit.GetValueOrDefault(0),
+                Date = source.Date,
+                ReceiptNumber = source.ReceiptNumber,
+                Comments = source.Comments,
+                Type = source.PaymentMethod,
+                Transaction = source.Credit.HasValue ? TransactionType.Payment : TransactionType.Refund,
+                TransactionId = source.Id
+            };
+        }
+    }
+}
