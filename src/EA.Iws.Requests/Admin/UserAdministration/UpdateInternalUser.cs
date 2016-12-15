ca3aa51b4b71a8e1ea1cb85d2543f1edb03a23a1
David Raine <DavidR@sfwltd.co.uk>
2016-12-15 10:52:32 +0000

    Add request authorization

--- src/EA.Iws.Requests/Admin/UserAdministration/UpdateInternalUser.cs
+++ src/EA.Iws.Requests/Admin/UserAdministration/UpdateInternalUser.cs
@@ -1,10 +1,11 @@
 ﻿namespace EA.Iws.Requests.Admin.UserAdministration
 {
-    using System;
     using Core.Admin;
     using Core.Authorization;
+    using Core.Authorization.Permissions;
     using Prsd.Core.Mediator;
 
+    [RequestAuthorization(UserAdministrationPermissions.CanManageExistingInternalUser)]
     public class UpdateInternalUser : IRequest<Unit>
     {
         public UpdateInternalUser(string userId, InternalUserStatus status, UserRole role)
