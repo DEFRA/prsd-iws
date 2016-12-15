f94b6f78f7b6751d0c990c7898dc3c46baf1aa71
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-12-15 14:24:43 +0000

    Add requests to delete notifications so that permissions can be tested in the menu controller

---
+++ src/EA.Iws.Requests/ImportNotificationAssessment/DeleteImportNotification.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Iws.Requests.ImportNotificationAssessment
+{
+    using System;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
+    using Prsd.Core.Mediator;
+
+    [RequestAuthorization(UserAdministrationPermissions.CanDeleteNotification)]
+    public class DeleteImportNotification : IRequest<bool>
+    {
+        public Guid NotificationId { get; private set; }
+
+        public DeleteImportNotification(Guid notificationId)
+        {
+            NotificationId = notificationId;
+        }
+    }
+}
---
+++ src/EA.Iws.Requests/NotificationAssessment/DeleteExportNotification.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Iws.Requests.NotificationAssessment
+{
+    using System;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
+    using Prsd.Core.Mediator;
+
+    [RequestAuthorization(UserAdministrationPermissions.CanDeleteNotification)]
+    public class DeleteExportNotification : IRequest<bool>
+    {
+        public Guid NotificationId { get; private set; }
+
+        public DeleteExportNotification(Guid notificationId)
+        {
+            NotificationId = notificationId;
+        }
+    }
+}
