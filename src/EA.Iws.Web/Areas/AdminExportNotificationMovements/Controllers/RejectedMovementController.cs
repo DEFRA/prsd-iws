7c4a48aac175d079b9879e12fbff54c959d755d5
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-12-21 13:33:01 +0000

    Add link on export shipment options to view rejected shipments

---
+++ src/EA.Iws.Web/Areas/AdminExportNotificationMovements/Controllers/RejectedMovementController.cs
@@ -0,0 +1,13 @@
+﻿namespace EA.Iws.Web.Areas.AdminExportNotificationMovements.Controllers
+{
+    using System.Web.Mvc;
+
+    public class RejectedMovementController : Controller
+    {
+        [HttpGet]
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
