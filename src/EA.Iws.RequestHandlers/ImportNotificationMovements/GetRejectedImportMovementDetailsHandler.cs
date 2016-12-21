bfad6f6301c1723e3c3821f4d753f488c440d5c1
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-12-21 13:10:22 +0000

    Add request and handler to get rejected movement details for import notifications

---
+++ src/EA.Iws.RequestHandlers/ImportNotificationMovements/GetRejectedImportMovementDetailsHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace EA.Iws.RequestHandlers.ImportNotificationMovements
+{
+    using System.Threading.Tasks;
+    using Core.Movement;
+    using Domain.ImportMovement;
+    using Prsd.Core.Mediator;
+    using Requests.ImportNotificationMovements;
+
+    internal class GetRejectedImportMovementDetailsHandler : IRequestHandler<GetRejectedImportMovementDetails, RejectedMovementDetails>
+    {
+        private readonly IImportMovementRejectionRepository repository;
+
+        public GetRejectedImportMovementDetailsHandler(IImportMovementRejectionRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<RejectedMovementDetails> HandleAsync(GetRejectedImportMovementDetails message)
+        {
+            var details = await repository.GetByMovementIdOrDefault(message.MovementId);
+
+            return new RejectedMovementDetails
+            {
+                Number = message.Number,
+                Date = details.Date,
+                Reason = details.Reason,
+                FurtherDetails = details.Reason
+            };
+        }
+    }
+}
