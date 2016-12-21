2ed0e40af987f8df28ea4271061c3eac65d1ea2f
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-12-21 16:07:27 +0000

    Add screen to display all rejected movements for an import notification

--- src/EA.Iws.Web/Areas/AdminImportNotificationMovements/Controllers/RejectedMovementController.cs
+++ src/EA.Iws.Web/Areas/AdminImportNotificationMovements/Controllers/RejectedMovementController.cs
@@ -1,11 +1,35 @@
 ﻿namespace EA.Iws.Web.Areas.AdminImportNotificationMovements.Controllers
 {
+    using System;
+    using System.Threading.Tasks;
     using System.Web.Mvc;
+    using Prsd.Core.Mediator;
+    using Requests.ImportNotificationMovements;
+    using ViewModels.RejectedMovement;
 
+    [Authorize(Roles = "internal")]
     public class RejectedMovementController : Controller
     {
+        private readonly IMediator mediator;
+
+        public RejectedMovementController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
         [HttpGet]
-        public ActionResult List()
+        public async Task<ActionResult> List(Guid id)
+        {
+            var model = new ListViewModel
+            {
+                Movements = await mediator.SendAsync(new GetRejectedImportMovements(id))
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Details(Guid movementId)
         {
             return View();
         }
