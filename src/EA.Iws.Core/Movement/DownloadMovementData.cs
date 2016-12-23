d438f7a186d5bd902e0619785870c2ba50428a75
Charlie Lusty <CharlesL@sfwltd.co.uk>
2016-12-23 15:51:04 +0000

    Add controller action for an internal user to download movements for an export notificaiton

---
+++ src/EA.Iws.Core/Movement/DownloadMovementData.cs
@@ -0,0 +1,31 @@
+﻿namespace EA.Iws.Core.Movement
+{
+    using System;
+    using System.ComponentModel;
+    using Shared;
+
+    public class DownloadMovementData
+    {
+        [DisplayName("Shipment number")]
+        public int Number { get; set; }
+
+        public MovementStatus Status { get; set; }
+
+        [DisplayName("Prenotified")]
+        public DateTime? SubmittedDate { get; set; }
+
+        [DisplayName("Shipment due")]
+        public DateTime? ShipmentDate { get; set; }
+
+        [DisplayName("Received")]
+        public DateTime? ReceivedDate { get; set; }
+
+        public decimal? Quantity { get; set; }
+
+        [DisplayName("Units")]
+        public ShipmentQuantityUnits? QuantityUnits { get; set; }
+
+        [DisplayName("Recovered/Disposed")]
+        public DateTime? CompletedDate { get; set; }
+    }
+}
