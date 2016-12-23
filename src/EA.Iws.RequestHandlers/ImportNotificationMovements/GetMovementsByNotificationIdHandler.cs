69e76251381367c4df5f04b180706f896a2f2819
Charlie Lusty <charles.lusty@gmail.com>
2016-12-23 18:09:44 +0000

    Add request and handler to get movements for import notifications

---
+++ src/EA.Iws.RequestHandlers/ImportNotificationMovements/GetMovementsByNotificationIdHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace EA.Iws.RequestHandlers.ImportNotificationMovements
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Core.Movement;
+    using Domain.ImportMovement;
+    using Prsd.Core.Mapper;
+    using Prsd.Core.Mediator;
+    using Requests.ImportNotificationMovements;
+
+    internal class GetMovementsByNotificationIdHandler : IRequestHandler<GetMovementsByNotificationId, IList<MovementTableDataRow>>
+    {
+        private readonly IImportMovementRepository repository;
+        private readonly IMapper mapper;
+
+        public GetMovementsByNotificationIdHandler(IImportMovementRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public async Task<IList<MovementTableDataRow>> HandleAsync(GetMovementsByNotificationId message)
+        {
+            var movements = await repository.GetForNotification(message.Id);
+
+            return movements.Select(m => mapper.Map<MovementTableDataRow>(m)).ToList();
+        }
+    }
+}
---
+++ src/EA.Iws.Requests/ImportNotificationMovements/GetMovementsByNotificationId.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Iws.Requests.ImportNotificationMovements
+{
+    using System;
+    using System.Collections.Generic;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
+    using Core.Movement;
+    using Prsd.Core.Mediator;
+
+    [RequestAuthorization(ImportMovementPermissions.CanReadImportMovements)]
+    public class GetMovementsByNotificationId : IRequest<IList<MovementTableDataRow>>
+    {
+        public Guid Id { get; private set; }
+
+        public GetMovementsByNotificationId(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
