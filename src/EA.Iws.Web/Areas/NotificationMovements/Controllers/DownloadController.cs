39d5a157930fc5129b26a2ac28c1fb78a3f91e68
Charlie Lusty <CharlesL@sfwltd.co.uk>
2017-01-03 10:04:47 +0000

    Add link for external user to download shipment details for an export notification

---
+++ src/EA.Iws.Web/Areas/NotificationMovements/Controllers/DownloadController.cs
@@ -0,0 +1,37 @@
+﻿namespace EA.Iws.Web.Areas.NotificationMovements.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Core.Authorization.Permissions;
+    using Core.Movement;
+    using Infrastructure;
+    using Infrastructure.Authorization;
+    using Prsd.Core.Mediator;
+    using Requests.Notification;
+    using Requests.NotificationMovements;
+
+    [AuthorizeActivity(ExportMovementPermissions.CanReadExportMovements)]
+    public class DownloadController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public DownloadController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index(Guid notificationId)
+        {
+            var notification = await mediator.SendAsync(new GetNotificationBasicInfo(notificationId));
+            var movementData = await mediator.SendAsync(new GetMovementsByNotificationId(notificationId));
+            var data = movementData.Select(m => new DownloadExportMovementData(m)).ToList();
+
+            var filename = string.Format("movement-details-for-notification-number-{0}.csv", notification.NotificationNumber);
+
+            return new CsvActionResult<DownloadExportMovementData>(data, filename);
+        }
+    }
+}
