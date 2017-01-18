5f016c7eb7db8dca30a246465c5c33d6f4ec5fea
David Raine <DavidR@sfwltd.co.uk>
2017-01-18 16:13:16 +0000

    Add missing indexes

---
+++ src/EA.Iws.Database/scripts/Update/0003-Migration/Sprint1/20170118-160400-add-indexes.sql
@@ -0,0 +1,9 @@
+﻿CREATE INDEX [IX_WasteCodeInfo_NotificationId] ON [EA.IWS].[Notification].[WasteCodeInfo] ([NotificationId]) INCLUDE ([CodeType]);
+
+CREATE INDEX [IX_Carrier_CarrierCollectionId] ON [EA.IWS].[Notification].[Carrier] ([CarrierCollectionId]) INCLUDE ([Id]);
+
+CREATE INDEX [IX_Notification_CompetentAuthority] ON [EA.IWS].[Notification].[Notification] ([CompetentAuthority]) INCLUDE ([Id], [NotificationNumber]);
+
+CREATE INDEX [IX_WasteCodeInfo_NotificationId_CodeType] ON [EA.IWS].[Notification].[WasteCodeInfo] ([NotificationId], [CodeType]) INCLUDE ([WasteCodeId]);
+
+CREATE INDEX [IX_NotificationStatusChange_NotificationAssessmentId] ON [EA.IWS].[Notification].[NotificationStatusChange] ([NotificationAssessmentId]);
