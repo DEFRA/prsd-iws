3f860e30e11b20422da2ae3f7a73e17c19f39c55
David Raine <DavidR@sfwltd.co.uk>
2017-01-19 15:59:12 +0000

    Add indexes to import tables

--- src/EA.Iws.Database/scripts/Update/0003-Migration/Sprint1/20170119-114000-add-indexes.sql
+++ src/EA.Iws.Database/scripts/Update/0003-Migration/Sprint1/20170119-114000-add-indexes.sql
@@ -1,1 +1,7 @@
 ﻿CREATE INDEX [IX_Movement_NotificationId_2] ON [Notification].[Movement] ([NotificationId]) INCLUDE ([Id], [Number], [Date], [Status], [PrenotificationDate]);
+
+CREATE INDEX [IX_ImportMovement_NotificationId_2] ON [ImportNotification].[Movement] ([NotificationId]) INCLUDE ([Id], [Number], [ActualShipmentDate], [IsCancelled], [PrenotificationDate]);
+
+CREATE INDEX [IX_ImportNotificationStatusChange_NotificationAssessmentId] ON [ImportNotification].[NotificationStatusChange] ([NotificationAssessmentId]);
+
+CREATE INDEX [IX_ImportNotification_CompetentAuthority] ON [ImportNotification].[Notification] ([CompetentAuthority]) INCLUDE ([Id], [NotificationNumber]);
