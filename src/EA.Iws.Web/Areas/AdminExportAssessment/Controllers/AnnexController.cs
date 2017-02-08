08084ab175e868320c9de7d421d4e08a8c30cb2f
David Raine <DavidR@sfwltd.co.uk>
2017-02-08 11:37:42 +0000

    Add screen for internal users to download annex documents

---
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/AnnexController.cs
@@ -0,0 +1,40 @@
+﻿namespace EA.Iws.Web.Areas.AdminExportAssessment.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Core.Authorization.Permissions;
+    using Infrastructure;
+    using Infrastructure.Authorization;
+    using Prsd.Core.Mediator;
+    using Requests.Annexes;
+    using ViewModels.Annex;
+
+    [AuthorizeActivity(ExportNotificationPermissions.CanReadExportNotificationAssessment)]
+    public class AnnexController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public AnnexController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index(Guid id)
+        {
+            var result = await mediator.SendAsync(new GetAnnexes(id));
+
+            return View(new DownloadAnnexViewModel(result));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Download(Guid id, Guid fileId)
+        {
+            var result = await mediator.SendAsync(new GetAnnexFile(id, fileId));
+
+            return File(result.Content, MimeTypeHelper.GetMimeType(result.Type),
+                string.Format("{0}.{1}", result.Name, result.Type));
+        }
+    }
+}
---
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/ViewModels/Annex/DownloadAnnexViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace EA.Iws.Web.Areas.AdminExportAssessment.ViewModels.Annex
+{
+    using Core.Annexes;
+    using Core.Annexes.ExportNotification;
+
+    public class DownloadAnnexViewModel
+    {
+        public DownloadAnnexViewModel()
+        {
+        }
+
+        public DownloadAnnexViewModel(ProvidedAnnexesData data)
+        {
+            ProcessOfGeneration = data.ProcessOfGeneration;
+            TechnologyEmployed = data.TechnologyEmployed;
+            WasteComposition = data.WasteComposition;
+        }
+
+        public AnnexStatus ProcessOfGeneration { get; set; }
+
+        public AnnexStatus TechnologyEmployed { get; set; }
+
+        public AnnexStatus WasteComposition { get; set; }
+
+        public bool HasAnnexes
+        {
+            get { return TechnologyEmployed.IsRequired || ProcessOfGeneration.IsRequired || WasteComposition.IsRequired; }
+        }
+    }
+}
