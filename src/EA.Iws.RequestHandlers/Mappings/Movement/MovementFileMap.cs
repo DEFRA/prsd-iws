ce2098fb3c01c61572724f505f2e944638540d75
David Raine <DavidR@sfwltd.co.uk>
2017-02-08 16:12:16 +0000

    Add screen to download shipment documents

---
+++ src/EA.Iws.RequestHandlers/Mappings/Movement/MovementFileMap.cs
@@ -0,0 +1,30 @@
+﻿namespace EA.Iws.RequestHandlers.Mappings.Movement
+{
+    using Core.Movement;
+    using Domain.Movement;
+    using Prsd.Core.Mapper;
+
+    public class MovementFileMap : IMap<Movement, MovementFileData>
+    {
+        public MovementFileData Map(Movement source)
+        {
+            var result = new MovementFileData
+            {
+                ShipmentNumber = source.Number,
+                PrenotificationFileId = source.FileId
+            };
+
+            if (source.Receipt != null)
+            {
+                result.ReceiptFileId = source.Receipt.FileId;
+            }
+
+            if (source.CompletedReceipt != null)
+            {
+                result.OperationReceiptFileId = source.CompletedReceipt.FileId;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/EA.Iws.RequestHandlers/Movement/GetMovementFilesHandler.cs
@@ -0,0 +1,40 @@
+﻿namespace EA.Iws.RequestHandlers.Movement
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Core.Movement;
+    using Domain.Movement;
+    using Prsd.Core.Mapper;
+    using Prsd.Core.Mediator;
+    using Requests.Movement;
+
+    internal class GetMovementFilesHandler : IRequestHandler<GetMovementFiles, MovementFiles>
+    {
+        private const int PageSize = 30;
+        private readonly IMapper mapper;
+        private readonly IMovementRepository repository;
+
+        public GetMovementFilesHandler(IMovementRepository repository,
+            IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public async Task<MovementFiles> HandleAsync(GetMovementFiles message)
+        {
+            var movements = await repository.GetPagedMovements(message.NotificationId, message.PageNumber, PageSize);
+
+            var fileData = movements.Select(m => mapper.Map<MovementFileData>(m)).ToArray();
+            var numberOfShipments = await repository.GetTotalNumberOfMovements(message.NotificationId, null);
+
+            return new MovementFiles
+            {
+                FileData = fileData,
+                NumberOfShipments = numberOfShipments,
+                PageNumber = message.PageNumber,
+                PageSize = PageSize
+            };
+        }
+    }
+}
