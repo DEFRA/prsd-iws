e3c1427330d53093574e48c8842b25a9f5f9defd
David Raine <DavidR@sfwltd.co.uk>
2017-02-08 14:12:10 +0000

    Add link to download shipment documents

---
+++ src/EA.Iws.Web/Areas/NotificationMovements/Controllers/DocumentsController.cs
@@ -0,0 +1,17 @@
+﻿namespace EA.Iws.Web.Areas.NotificationMovements.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class DocumentsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
