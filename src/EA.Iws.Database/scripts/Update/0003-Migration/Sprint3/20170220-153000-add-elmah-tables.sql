4bdf11bab84f2c8c37be81d2086f9858ddf6f0ab
David Raine <DavidR@sfwltd.co.uk>
2017-02-20 16:30:34 +0000

    Add scripts for elmah database objects

---
+++ src/EA.Iws.Database/scripts/Update/0003-Migration/Sprint3/20170220-153000-add-elmah-tables.sql
@@ -0,0 +1,66 @@
+﻿/*
+  
+   ELMAH - Error Logging Modules and Handlers for ASP.NET
+   Copyright (c) 2004-9 Atif Aziz. All rights reserved.
+  
+    Author(s):
+  
+        Atif Aziz, http://www.raboof.com
+        Phil Haacked, http://haacked.com
+  
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+  
+      http://www.apache.org/licenses/LICENSE-2.0
+  
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+  
+*/
+
+-- ELMAH DDL script for Microsoft SQL Server 2000 or later.
+
+-- $Id: SQLServer.sql 776 2011-01-12 21:09:24Z azizatif $
+
+CREATE SCHEMA [Logging] 
+    AUTHORIZATION [dbo];
+GO
+
+CREATE TABLE [Logging].[ELMAH_Error]
+(
+    [ErrorId]     UNIQUEIDENTIFIER NOT NULL,
+    [Application] NVARCHAR(60)  COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [Host]        NVARCHAR(50)  COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [Type]        NVARCHAR(100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [Source]      NVARCHAR(60)  COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [Message]     NVARCHAR(500) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [User]        NVARCHAR(50)  COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [StatusCode]  INT NOT NULL,
+    [TimeUtc]     DATETIME NOT NULL,
+    [Sequence]    INT IDENTITY (1, 1) NOT NULL,
+    [AllXml]      NVARCHAR(MAX) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL 
+) 
+ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+ALTER TABLE [Logging].[ELMAH_Error] WITH NOCHECK ADD 
+    CONSTRAINT [PK_ELMAH_Error] PRIMARY KEY NONCLUSTERED ([ErrorId]) ON [PRIMARY] 
+GO
+
+ALTER TABLE [Logging].[ELMAH_Error] ADD 
+    CONSTRAINT [DF_ELMAH_Error_ErrorId] DEFAULT (NEWID()) FOR [ErrorId]
+GO
+
+CREATE NONCLUSTERED INDEX [IX_ELMAH_Error_App_Time_Seq] ON [Logging].[ELMAH_Error] 
+(
+    [Application]   ASC,
+    [TimeUtc]       DESC,
+    [Sequence]      DESC
+) 
+ON [PRIMARY]
+GO
