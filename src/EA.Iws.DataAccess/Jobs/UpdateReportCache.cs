ed1857c22b83c086ac688d68dc7e7c5ca0d958d7
David Raine <DavidR@sfwltd.co.uk>
2017-02-24 09:41:38 +0000

    Revert "Increase command timeout"
    This reverts commit d745001691567e297e15263ef6f3e53cb8f94591.

--- src/EA.Iws.DataAccess/Jobs/UpdateReportCache.cs
+++ src/EA.Iws.DataAccess/Jobs/UpdateReportCache.cs
@@ -23,7 +23,6 @@
                 {
                     command.CommandType = CommandType.StoredProcedure;
                     command.CommandText = "[Reports].[uspUpdateReportCache]";
-                    command.CommandTimeout = 300; // 5 minutes
 
                     await command.ExecuteNonQueryAsync();
                 }
