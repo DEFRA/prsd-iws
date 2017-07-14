a9359e09d6c253d08720e50ea101edb275b37342
David Raine <David.Raine@civica.co.uk>
2017-07-14 10:44:46 +0100

    Add authorization attribute

--- src/EA.Iws.Requests/ImportMovement/GetNotificationIdByMovementId.cs
+++ src/EA.Iws.Requests/ImportMovement/GetNotificationIdByMovementId.cs
@@ -1,8 +1,11 @@
 ﻿namespace EA.Iws.Requests.ImportMovement
 {
     using System;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
     using Prsd.Core.Mediator;
 
+    [RequestAuthorization(ImportMovementPermissions.CanReadImportMovements)]
     public class GetNotificationIdByMovementId : IRequest<Guid>
     {
         public GetNotificationIdByMovementId(Guid movementId)
