36981d64a0260be6ef4c77eb5ff21adef2ba378a
David Raine <David.Raine@civica.co.uk>
2017-07-20 09:40:18 +0100

    Update validation message

--- src/EA.Iws.Web/Areas/AdminImportAssessment/ViewModels/RefundDetails/RefundDetailsViewModelResources.Designer.cs
+++ src/EA.Iws.Web/Areas/AdminImportAssessment/ViewModels/RefundDetails/RefundDetailsViewModelResources.Designer.cs
@@ -161,7 +161,7 @@ namespace EA.Iws.Web.Areas.AdminImportAssessment.ViewModels.RefundDetails {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Please provide the date the refund was made.
+        ///   Looks up a localized string similar to Please provide the date the refund was requested.
         /// </summary>
         public static string DateRequiredError {
             get {
