7a447cfdcf2058f5266aebd26d6405a7dde68bf8
David Raine <David.Raine@civica.co.uk>
2017-08-07 13:36:15 +0100

    Add authorization attribute

--- src/EA.Iws.Requests/Movement/GetMovementsByIds.cs
+++ src/EA.Iws.Requests/Movement/GetMovementsByIds.cs
@@ -2,9 +2,12 @@
 {
     using System;
     using System.Collections.Generic;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
     using Core.Movement;
     using Prsd.Core.Mediator;
 
+    [RequestAuthorization(ExportMovementPermissions.CanReadExportMovements)]
     public class GetMovementsByIds : IRequest<MovementInfo[]>
     {
         public GetMovementsByIds(Guid notificationId, IEnumerable<Guid> movementIds)
