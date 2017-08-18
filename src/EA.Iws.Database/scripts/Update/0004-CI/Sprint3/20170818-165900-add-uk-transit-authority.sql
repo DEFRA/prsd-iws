defa341fd538674490497481c0a0f934d0704ec6
David Raine <David.Raine@civica.co.uk>
2017-08-18 17:05:11 +0100

    Add UK transit authority

---
+++ src/EA.Iws.Database/scripts/Update/0004-CI/Sprint3/20170818-165900-add-uk-transit-authority.sql
@@ -0,0 +1,25 @@
+﻿DECLARE @ukId UNIQUEIDENTIFIER;
+SELECT @ukId = [Id] FROM [Lookup].[Country] WHERE [Name] = 'United Kingdom';
+
+UPDATE [Lookup].[CompetentAuthority]
+SET [IsTransitAuthority] = 0
+WHERE [CountryId] = @ukId;
+
+INSERT INTO [Lookup].[CompetentAuthority]
+           ([Id]
+           ,[Name]
+           ,[Abbreviation]
+           ,[IsSystemUser]
+           ,[Code]
+           ,[CountryId]
+           ,[Region]
+           ,[IsTransitAuthority])
+     VALUES
+           ((SELECT Cast(Cast(Newid() AS BINARY(10)) + Cast(Getdate() AS BINARY(6)) AS UNIQUEIDENTIFIER))
+           ,'Environment Agency'
+           ,'EA'
+           ,0
+           ,'GB00'
+           ,@ukId
+           ,null
+           ,1);
