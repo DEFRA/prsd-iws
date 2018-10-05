cec239f72f89f1750ded7af7b9aae4a30cb36b7f
Charlie Lusty <Charlie.Lusty@civica.co.uk>
2018-10-05 07:53:11 +0100

    Update repository to get new data for created on dates

---
+++ src/EA.Iws.Core/Admin/Reports/UserActionData.cs
@@ -0,0 +1,16 @@
+﻿namespace EA.Iws.Core.Admin.Reports
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class UserActionData
+    {
+        public string NewValue { get; set; }
+
+        public Guid RecordId { get; set; } 
+    }
+}
--- src/EA.Iws.DataAccess/Repositories/Reports/ExportMovementsRepository.cs
+++ src/EA.Iws.DataAccess/Repositories/Reports/ExportMovementsRepository.cs
@@ -18,24 +18,66 @@
 
         public async Task<ExportMovementsData> Get(DateTime from, DateTime to, UKCompetentAuthority competentAuthority)
         {
-            return await context.Database.SqlQuery<ExportMovementsData>(
-                @"SELECT
-                      COUNT(CASE WHEN MovementInternalUserId IS NULL AND FileId IS NOT NULL THEN 1 ELSE NULL END) AS FilesUploadedExternally,
-                      COUNT(CASE WHEN MovementInternalUserId IS NOT NULL AND FileId IS NOT NULL THEN 1 ELSE NULL END) AS FilesUploadedInternally,
-                      COUNT(CASE WHEN MovementInternalUserId IS NULL THEN 1 ELSE NULL END) AS MovementsCreatedExternally,
-                      COUNT(MovementInternalUserId) AS MovementsCreatedInternally,
-                      COUNT(CASE WHEN MovementReceiptInternalUserId IS NULL AND ReceiptDate IS NOT NULL THEN 1 ELSE NULL END) AS MovementReceiptsCreatedExternally,
-                      COUNT(CASE WHEN MovementReceiptInternalUserId IS NOT NULL AND ReceiptDate IS NOT NULL THEN 1 ELSE NULL END) AS MovementReceiptsCreatedInternally,
-                      COUNT(CASE WHEN MovementOperationReceiptInternalUserId IS NULL AND OperationDate IS NOT NULL THEN 1 ELSE NULL END) AS MovementOperationReceiptsCreatedExternally,
-                      COUNT(CASE WHEN MovementOperationReceiptInternalUserId IS NOT NULL AND OperationDate IS NOT NULL THEN 1 ELSE NULL END) AS MovementOperationReceiptsCreatedInternally
-                  FROM
-                      [Reports].[ExportMovements]
-                  WHERE 
-                      [CompetentAuthority] = @ca
-                      AND ShipmentDate BETWEEN @from AND @to",
+            var movementData = await context.Database.SqlQuery<ExportMovementsData>(
+                @"WITH MovementAudit AS
+                   (SELECT 
+                        IU.Id AS UserId,
+						MR.Date AS MRReceiptDate,
+						MOR.Date AS MORReceiptDate,
+						A.TableName,
+						N.Id AS bob
+                    FROM Auditing.AuditLog AS A 
+                        LEFT JOIN [Notification].[Movement] AS M ON A.RecordId = M.Id
+						LEFT JOIN [Notification].[MovementReceipt] AS MR ON A.RecordId = MR.Id
+						LEFT JOIN [Notification].[MovementOperationReceipt] AS MOR ON A.RecordId = MOR.Id
+                        LEFT JOIN [Notification].[Notification] N ON M.NotificationId = N.Id
+                        LEFT JOIN [Identity].[AspNetUsers] U ON M.CreatedBy = U.Id OR MR.CreatedBy = U.Id OR MOR.CreatedBy = U.Id
+							LEFT JOIN [Person].[InternalUser] IU ON U.Id = IU.UserId
+                    WHERE A.EventType = 0 
+                        AND (A.TableName = '[Notification].[Movement]' OR A.TableName = '[Notification].[MovementReceipt]' OR A.TableName = '[Notification].[MovementOperationReceipt]')
+                        AND (N.CompetentAuthority = @ca OR (A.TableName != '[Notification].[Movement]' AND N.CompetentAuthority IS NULL))
+                        AND A.EventDate BETWEEN @from AND @to)
+
+                    SELECT 
+                        COUNT(CASE WHEN UserId IS NULL AND TableName = '[Notification].[Movement]' THEN 1 ELSE NULL END) AS MovementsCreatedExternally,
+                        COUNT(CASE WHEN UserId IS NOT NULL AND TableName = '[Notification].[Movement]' THEN 1 ELSE NULL END) AS MovementsCreatedInternally,
+						COUNT(CASE WHEN UserId IS NULL AND MRReceiptDate IS NOT NULL THEN 1 ELSE NULL END) AS MovementReceiptsCreatedExternally,
+                        COUNT(CASE WHEN UserId IS NOT NULL AND MRReceiptDate IS NOT NULL THEN 1 ELSE NULL END) AS MovementReceiptsCreatedInternally,
+						COUNT(CASE WHEN UserId IS NULL AND MORReceiptDate IS NOT NULL THEN 1 ELSE NULL END) AS MovementOperationReceiptsCreatedExternally,
+                        COUNT(CASE WHEN UserId IS NOT NULL AND MORReceiptDate IS NOT NULL THEN 1 ELSE NULL END) AS MovementOperationReceiptsCreatedInternally
+                    FROM MovementAudit",
+                    new SqlParameter("@ca", (int)competentAuthority),
+                    new SqlParameter("@from", from),
+                    new SqlParameter("@to", to)).SingleAsync();
+
+            var result = new ExportMovementsData();
+            result.MovementsCreatedExternally = movementData.MovementsCreatedExternally;
+            result.MovementsCreatedInternally = movementData.MovementsCreatedInternally;
+            result.MovementReceiptsCreatedExternally = movementData.MovementReceiptsCreatedExternally;
+            result.MovementReceiptsCreatedInternally = movementData.MovementReceiptsCreatedInternally;
+            result.MovementOperationReceiptsCreatedExternally = movementData.MovementOperationReceiptsCreatedExternally;
+            result.MovementOperationReceiptsCreatedInternally = movementData.MovementOperationReceiptsCreatedInternally;
+
+            var userActions = await context.Database.SqlQuery<UserActionData>(
+                @"SELECT     
+	                A.NewValue,
+	                A.RecordId
+                FROM [Auditing].[AuditLog] AS A
+                INNER JOIN [Notification].[Movement] AS M ON M.Id = A.RecordId
+                INNER JOIN [Notification].[Notification] N ON M.NotificationId = N.Id
+                INNER JOIN [Identity].[AspNetUsers] U ON A.UserId = U.Id
+                LEFT JOIN [Person].[InternalUser] IU ON U.Id = IU.UserId
+                WHERE TableName = '[Notification].[Movement]' 
+                    AND EventType != 0 
+                    AND IU.UserId IS NULL
+                    AND N.CompetentAuthority = @ca
+                    AND A.EventDate BETWEEN @from AND @to
+                ORDER BY RecordId, EventDate",
                 new SqlParameter("@ca", (int)competentAuthority),
                 new SqlParameter("@from", from),
-                new SqlParameter("@to", to)).SingleAsync();
+                new SqlParameter("@to", to)).ToListAsync();
+
+            return result;
         }
     }
 }
