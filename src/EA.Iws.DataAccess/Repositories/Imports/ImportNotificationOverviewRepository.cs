dd919d5f15f47b244ed0ba3288c78449429bbe2f
Gonzalo Girasol <gonzalog@sfwltd.co.uk>
2018-11-15 17:50:54 +0000

    Update retrieval data for import notification overview to handle instances where sections are incomplete.

--- src/EA.Iws.DataAccess/Repositories/Imports/ImportNotificationOverviewRepository.cs
+++ src/EA.Iws.DataAccess/Repositories/Imports/ImportNotificationOverviewRepository.cs
@@ -22,127 +22,35 @@
         {
             await authorization.EnsureAccessAsync(notificationId);
 
-            var data = await context.ImportNotifications
-                .Join(context.ImportNotificationAssessments,
-                    n => n.Id,
-                    a => a.NotificationApplicationId,
-                    (n, a) => new
-                    {
-                        Notification = n,
-                        Assessment = a
-                    })
-                .Join(context.Exporters,
-                    x => x.Notification.Id,
-                    e => e.ImportNotificationId,
-                    (x, e) => new
-                    {
-                        Exporter = e,
-                        x.Notification,
-                        x.Assessment
-                    })
-                .Join(context.Importers,
-                    x => x.Notification.Id,
-                    i => i.ImportNotificationId,
-                    (x, i) => new
-                    {
-                        Importer = i,
-                        x.Notification,
-                        x.Assessment,
-                        x.Exporter
-                    })
-                .Join(context.Producers,
-                    x => x.Notification.Id,
-                    p => p.ImportNotificationId,
-                    (x, p) => new
-                    {
-                        Producer = p,
-                        x.Notification,
-                        x.Assessment,
-                        x.Exporter,
-                        x.Importer
-                    })
-                .Join(context.Facilities,
-                    x => x.Notification.Id,
-                    f => f.ImportNotificationId,
-                    (x, f) => new
-                    {
-                        Facilities = f,
-                        x.Notification,
-                        x.Assessment,
-                        x.Exporter,
-                        x.Importer,
-                        x.Producer
-                    })
-                .Join(context.Shipments,
-                    x => x.Notification.Id,
-                    s => s.ImportNotificationId,
-                    (x, s) => new
-                    {
-                        Shipment = s,
-                        x.Notification,
-                        x.Assessment,
-                        x.Exporter,
-                        x.Importer,
-                        x.Producer,
-                        x.Facilities
-                    })
-                .Join(context.TransportRoutes,
-                    x => x.Notification.Id,
-                    t => t.ImportNotificationId,
-                    (x, t) => new
-                    {
-                        TransportRoute = t,
-                        x.Notification,
-                        x.Assessment,
-                        x.Exporter,
-                        x.Importer,
-                        x.Producer,
-                        x.Facilities,
-                        x.Shipment
-                    })
-                .Join(context.OperationCodes,
-                    x => x.Notification.Id,
-                    o => o.ImportNotificationId,
-                    (x, o) => new
-                    {
-                        WasteOperation = o,
-                        x.Notification,
-                        x.Assessment,
-                        x.Exporter,
-                        x.Importer,
-                        x.Producer,
-                        x.Facilities,
-                        x.TransportRoute,
-                        x.Shipment
-                    })
-                .Join(context.WasteTypes,
-                    x => x.Notification.Id,
-                    t => t.ImportNotificationId,
-                    (x, t) => new
-                    {
-                        WasteType = t,
-                        x.Notification,
-                        x.Assessment,
-                        x.Exporter,
-                        x.Importer,
-                        x.Producer,
-                        x.Facilities,
-                        x.Shipment,
-                        x.TransportRoute,
-                        x.WasteOperation
-                    })
-                .SingleAsync(x => x.Notification.Id == notificationId);
+            var notification =
+                await context.ImportNotifications.SingleOrDefaultAsync(x => x.Id == notificationId);
+            var assessments =
+                await
+                    context.ImportNotificationAssessments.SingleOrDefaultAsync(
+                        x => x.NotificationApplicationId == notificationId);
+            var exporter = await context.Exporters.SingleOrDefaultAsync(x => x.ImportNotificationId == notificationId);
+            var importer = await context.Importers.SingleOrDefaultAsync(x => x.ImportNotificationId == notificationId);
+            var producer = await context.Producers.SingleOrDefaultAsync(x => x.ImportNotificationId == notificationId);
+            var facilities =
+                await context.Facilities.SingleOrDefaultAsync(x => x.ImportNotificationId == notificationId);
+            var shipment = await context.Shipments.SingleOrDefaultAsync(x => x.ImportNotificationId == notificationId);
+            var transportRoute =
+                await context.TransportRoutes.SingleOrDefaultAsync(x => x.ImportNotificationId == notificationId);
+            var operationCode =
+                await context.OperationCodes.SingleOrDefaultAsync(x => x.ImportNotificationId == notificationId);
+            var wasteType =
+                await context.WasteTypes.SingleOrDefaultAsync(x => x.ImportNotificationId == notificationId);
 
-            return ImportNotificationOverview.Load(data.Notification,
-                data.Assessment,
-                data.Exporter,
-                data.Importer,
-                data.Producer,
-                data.Facilities,
-                data.Shipment,
-                data.TransportRoute,
-                data.WasteOperation,
-                data.WasteType);
+            return ImportNotificationOverview.Load(notification,
+                assessments,
+                exporter,
+                importer,
+                producer,
+                facilities,
+                shipment,
+                transportRoute,
+                operationCode,
+                wasteType);
         }
 
         public Task<ImportNotificationOverview> GetFromDraft(Guid notificationId)
