ee97cfff2e2ac013d6180b4a7e02b7b814ee00f7
willdot <will7989@hotmail.com>
2018-12-12 11:26:30 +0000

    Added repositories for shared user and shared user history

---
+++ src/EA.Iws.DataAccess/Repositories/SharedUserRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Iws.DataAccess.Repositories
+{
+    using Domain.NotificationApplication;
+
+    public class SharedUserRepository : ISharedUserRepository
+    {
+        private readonly IwsContext context;
+
+        public SharedUserRepository(IwsContext context)
+        {
+            this.context = context;
+        }
+
+        public void AddSharedUser(SharedUser sharedUser)
+        {
+            this.context.SharedUser.Add(sharedUser);
+        }
+
+        public void RemoveSharedUser(SharedUser sharedUser)
+        {
+            this.context.SharedUser.Remove(sharedUser);
+        }
+    }
+}
---
+++ src/EA.Iws.Domain/NotificationApplication/ISharedUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace EA.Iws.Domain.NotificationApplication
+{
+    public interface ISharedUserRepository
+    {
+        void AddSharedUser(SharedUser sharedUser);
+        void RemoveSharedUser(SharedUser sharedUser);
+    }
+}
