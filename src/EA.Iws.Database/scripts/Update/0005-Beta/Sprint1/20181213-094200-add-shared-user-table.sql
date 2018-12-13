13846ac78493f8df9014823a85839d5d2bc006f8
willdot <will7989@hotmail.com>
2018-12-13 09:55:19 +0000

    Added scripts for the creation of shared user access tables.

---
+++ src/EA.Iws.Database/scripts/Update/0005-Beta/Sprint1/20181213-094200-add-shared-user-table.sql
@@ -0,0 +1,37 @@
+USE [EA.Iws]
+GO
+
+/****** Object:  Table [Notification].[SharedUser]    Script Date: 13/12/2018 09:43:20 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [Notification].[SharedUser](
+	[Id] [uniqueidentifier] NOT NULL,
+	[NotificationId] [uniqueidentifier] NOT NULL,
+	[UserId] [nvarchar](128) NOT NULL,
+	[DateAdded] [datetimeoffset](0) NOT NULL,
+	[RowVersion] [timestamp] NOT NULL,
+ CONSTRAINT [PK_SharedUser] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [Notification].[SharedUser]  WITH CHECK ADD  CONSTRAINT [FK_SharedUser_Notification] FOREIGN KEY([NotificationId])
+REFERENCES [Notification].[Notification] ([Id])
+GO
+
+ALTER TABLE [Notification].[SharedUser] CHECK CONSTRAINT [FK_SharedUser_Notification]
+GO
+
+ALTER TABLE [Notification].[SharedUser]  WITH CHECK ADD  CONSTRAINT [FK_SharedUser_User] FOREIGN KEY([UserId])
+REFERENCES [Identity].[AspNetUsers] ([Id])
+GO
+
+ALTER TABLE [Notification].[SharedUser] CHECK CONSTRAINT [FK_SharedUser_User]
+GO
+
