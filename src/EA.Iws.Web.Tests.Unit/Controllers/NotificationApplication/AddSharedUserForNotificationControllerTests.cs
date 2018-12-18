d16efa9ec055c4674a58dfcf96b9b1aae6220e44
Gonzalo Girasol <gonzalog@sfwltd.co.uk>
2018-12-18 16:02:15 +0000

    Fix spacing.

--- src/EA.Iws.Web.Tests.Unit/Controllers/NotificationApplication/AddSharedUserForNotificationControllerTests.cs
+++ src/EA.Iws.Web.Tests.Unit/Controllers/NotificationApplication/AddSharedUserForNotificationControllerTests.cs
@@ -157,7 +157,7 @@
             {
                 returnList.Add(new NotificationSharedUser()
                 {
-                    Email = string.Format("{0}@fake.com",i),
+                    Email = string.Format("{0}@fake.com", i),
                     UserId = Guid.NewGuid().ToString()
                 });
             }
