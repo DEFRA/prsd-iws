787ea4fd9c446561f7a4acd6b8fcfdd624cedec1
Will Andrews <will7989@hotmail.com>
2019-01-07 14:07:50 +0000

    Added functionality to add watermark to word document, if Aspose license isn't available

--- src/EA.Iws.RequestHandlers/Notification/GenerateNotificationPreviewDocumentHandler.cs
+++ src/EA.Iws.RequestHandlers/Notification/GenerateNotificationPreviewDocumentHandler.cs
@@ -1,21 +1,29 @@
 ﻿namespace EA.Iws.RequestHandlers.Notification
 {
-    using System.Threading.Tasks;
     using Core.Documents;
     using Documents;
     using Domain;
     using Prsd.Core;
     using Prsd.Core.Mediator;
     using Requests.Notification;
+    using System.Threading.Tasks;
 
     internal class GenerateNotificationPreviewDocumentHandler : IRequestHandler<GenerateNotificationPreviewDocument, FileData>
     {
         private readonly INotificationDocumentGenerator notificationDocumentGenerator;
+        private readonly INotificationPreviewWatermarkGenerator notificationPreviewWatermarkGenerator;
         private readonly IPdfGenerator pdfGenerator;
 
-        public GenerateNotificationPreviewDocumentHandler(INotificationDocumentGenerator notificationDocumentGenerator, IPdfGenerator pdfGenerator)
+        public GenerateNotificationPreviewDocumentHandler(INotificationDocumentGenerator notificationDocumentGenerator, INotificationPreviewWatermarkGenerator notificationPreviewWatermarkGenerator)
         {
             this.notificationDocumentGenerator = notificationDocumentGenerator;
+            this.notificationPreviewWatermarkGenerator = notificationPreviewWatermarkGenerator;
+        }
+
+        public GenerateNotificationPreviewDocumentHandler(INotificationDocumentGenerator notificationDocumentGenerator, INotificationPreviewWatermarkGenerator notificationPreviewWatermarkGenerator, IPdfGenerator pdfGenerator)
+        {
+            this.notificationDocumentGenerator = notificationDocumentGenerator;
+            this.notificationPreviewWatermarkGenerator = notificationPreviewWatermarkGenerator;
             this.pdfGenerator = pdfGenerator;
         }
 
@@ -23,6 +31,12 @@
         {
             var docxBytes = await notificationDocumentGenerator.GenerateNotificationDocument(query.NotificationId);
             var fileName = string.Format("IWS-Notification-Preview-{0}-{1}", query.NotificationId, SystemTime.UtcNow);
+
+            if (pdfGenerator == null)
+            {
+                var wordBytes = notificationPreviewWatermarkGenerator.GenerateNotificationPreviewWatermark(docxBytes);
+                return new FileData(fileName, FileType.Docx, wordBytes);
+            }
 
             var pdfBytes = pdfGenerator.ConvertToPreviewPdf(docxBytes);
             return new FileData(fileName, FileType.Pdf, pdfBytes);
