1d2d22c1c7d25be65428759249f2ad50e81c384b
Michael Coull <Michael.Coull@ipl.com>
2019-01-09 16:08:56 +0000

    Added the page which will be shown when no changes for a notification are found

---
+++ src/EA.Iws.Web/Areas/NotificationApplication/Controllers/UpdateHistoryController.cs
@@ -0,0 +1,32 @@
+﻿namespace EA.Iws.Web.Areas.NotificationApplication.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+    using Prsd.Core.Mediator;
+    using ViewModels.Options;
+
+    // Does this need an Authorize tag (like HomeController) or AuthorizeActivity tag (like OptionsController)?
+    public class UpdateHistoryController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public UpdateHistoryController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult NoChanges(Guid id)
+        {
+            var model = new UnavailableViewModel { NotificationId = id };
+
+            return View(model);
+        }
+    }
+}
