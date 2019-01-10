705d5c5b1ea5920a8a05a278ef34338df34226a8
MichaelCoull1 <45666515+MichaelCoull1@users.noreply.github.com>
2019-01-10 14:12:02 +0000

    Feature/notification update history no changes (#624)
    * Added the page which will be shown when no changes for a notification are found
    
    * Extracted the update history title into a partial cshtml file
    
    * Added basic UI elements to UpdateHistory/Index.cshtml
    
    * Created a specific update history view model
    
    * Added the Authorize tag to the UpdateHistoryController class
    
    * Removed redundant reference to Areas\NotificationMovements\Views\Certificate\WhatNextResources.Designer.cs
    
    * Replaced use of line breaks with css

---
+++ src/EA.Iws.Web/Areas/NotificationApplication/Controllers/UpdateHistoryController.cs
@@ -0,0 +1,34 @@
+﻿namespace EA.Iws.Web.Areas.NotificationApplication.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+    using Prsd.Core.Mediator;
+    using ViewModels.UpdateHistory;
+
+    [Authorize]
+    public class UpdateHistoryController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public UpdateHistoryController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public ActionResult Index(Guid id)
+        {
+            var model = new UpdateHistoryViewModel { NotificationId = id };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult NoChanges(Guid id)
+        {
+            var model = new UpdateHistoryViewModel { NotificationId = id };
+
+            return View(model);
+        }
+    }
+}
---
+++ src/EA.Iws.Web/Areas/NotificationApplication/ViewModels/UpdateHistory/UpdateHistoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EA.Iws.Web.Areas.NotificationApplication.ViewModels.UpdateHistory
+{
+    using System;
+
+    public class UpdateHistoryViewModel
+    {
+        public Guid NotificationId { get; set; }
+    }
+}
