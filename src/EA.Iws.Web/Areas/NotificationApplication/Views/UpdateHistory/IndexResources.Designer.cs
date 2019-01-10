b5a0f16cc6cfa5cd17d91e0cf8683ec3dca4515f
Michael Coull <Michael.Coull@ipl.com>
2019-01-10 11:31:41 +0000

    Added basic UI elements to UpdateHistory/Index.cshtml

--- src/EA.Iws.Web/Areas/NotificationApplication/Views/UpdateHistory/IndexResources.Designer.cs
+++ src/EA.Iws.Web/Areas/NotificationApplication/Views/UpdateHistory/IndexResources.Designer.cs
@@ -22,7 +22,7 @@ namespace EA.Iws.Web.Areas.NotificationApplication.Views.UpdateHistory {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class IndexResources {
+    public class IndexResources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace EA.Iws.Web.Areas.NotificationApplication.Views.UpdateHistory {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EA.Iws.Web.Areas.NotificationApplication.Views.UpdateHistory.IndexResources", typeof(IndexResources).Assembly);
@@ -51,12 +51,39 @@ namespace EA.Iws.Web.Areas.NotificationApplication.Views.UpdateHistory {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Notification update history.
+        /// </summary>
+        public static string TabTitle {
+            get {
+                return ResourceManager.GetString("TabTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Notification update history.
+        /// </summary>
+        public static string Title {
+            get {
+                return ResourceManager.GetString("Title", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Information changes reflect the changes made by officers and administrators..
+        /// </summary>
+        public static string UpdateHistoryInfo {
+            get {
+                return ResourceManager.GetString("UpdateHistoryInfo", resourceCulture);
             }
         }
     }
