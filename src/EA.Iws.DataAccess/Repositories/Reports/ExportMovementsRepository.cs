bb8ba646dd3b482ecc8152bf59f1100347ea8b98
Charlie Lusty <Charlie.Lusty@civica.co.uk>
2019-01-11 12:40:02 +0000

    Refactor export movement repo

--- src/EA.Iws.DataAccess/Repositories/Reports/ExportMovementsRepository.cs
+++ src/EA.Iws.DataAccess/Repositories/Reports/ExportMovementsRepository.cs
@@ -1,6 +1,7 @@
 ﻿namespace EA.Iws.DataAccess.Repositories.Reports
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.SqlClient;
     using System.Linq;
     using System.Threading.Tasks;
@@ -21,57 +22,102 @@
         public async Task<ExportMovementsData> Get(DateTime from, DateTime to, UKCompetentAuthority competentAuthority)
         {
             var movementData = await context.Database.SqlQuery<ExportMovementsData>(
-                @"WITH MovementAudit AS
-                   (SELECT 
-                        IU.Id AS UserId,
-						MR.Date AS MRReceiptDate,
-						MOR.Date AS MORReceiptDate,
-						A.TableName
-                    FROM Auditing.AuditLog AS A 
-                        LEFT JOIN [Notification].[Movement] AS M ON A.RecordId = M.Id
-						LEFT JOIN [Notification].[MovementReceipt] AS MR ON A.RecordId = MR.Id
-						LEFT JOIN [Notification].[MovementOperationReceipt] AS MOR ON A.RecordId = MOR.Id
-                        LEFT JOIN [Notification].[Notification] N ON M.NotificationId = N.Id
-                        LEFT JOIN [Identity].[AspNetUsers] U ON M.CreatedBy = U.Id OR MR.CreatedBy = U.Id OR MOR.CreatedBy = U.Id
-							LEFT JOIN [Person].[InternalUser] IU ON U.Id = IU.UserId
-                    WHERE A.EventType = 0 
-                        AND (A.TableName = '[Notification].[Movement]' OR A.TableName = '[Notification].[MovementReceipt]' OR A.TableName = '[Notification].[MovementOperationReceipt]')
-                        AND (N.CompetentAuthority = @ca OR (A.TableName != '[Notification].[Movement]' AND N.CompetentAuthority IS NULL))
-                        AND A.EventDate BETWEEN @from AND @to)
-
+                @"WITH movementcreateddata AS (
                     SELECT 
-                        COUNT(CASE WHEN UserId IS NULL AND TableName = '[Notification].[Movement]' THEN 1 ELSE NULL END) AS MovementsCreatedExternally,
-                        COUNT(CASE WHEN UserId IS NOT NULL AND TableName = '[Notification].[Movement]' THEN 1 ELSE NULL END) AS MovementsCreatedInternally,
-						COUNT(CASE WHEN UserId IS NULL AND MRReceiptDate IS NOT NULL THEN 1 ELSE NULL END) AS MovementReceiptsCreatedExternally,
-                        COUNT(CASE WHEN UserId IS NOT NULL AND MRReceiptDate IS NOT NULL THEN 1 ELSE NULL END) AS MovementReceiptsCreatedInternally,
-						COUNT(CASE WHEN UserId IS NULL AND MORReceiptDate IS NOT NULL THEN 1 ELSE NULL END) AS MovementOperationReceiptsCreatedExternally,
-                        COUNT(CASE WHEN UserId IS NOT NULL AND MORReceiptDate IS NOT NULL THEN 1 ELSE NULL END) AS MovementOperationReceiptsCreatedInternally
-                    FROM MovementAudit",
+	                    M.NotificationId,
+	                    IU.Id AS InternalUserId
+                        FROM [Notification].[Movement] M
+                        LEFT JOIN [Notification].[Notification] N ON M.NotificationId = N.Id
+	                    INNER JOIN [Identity].[AspNetUsers] U ON M.CreatedBy = U.Id
+                            LEFT JOIN [Person].[InternalUser] IU ON M.Id = IU.UserId
+                        WHERE N.CompetentAuthority = @ca
+                            AND M.Date BETWEEN @from AND @to),
+
+                    receiptdata AS (
+                    SELECT MR.*, IU.Id AS InternalUserId
+                    FROM
+                        [Notification].[MovementReceipt] MR
+	                    INNER JOIN [Notification].[Movement] M ON M.Id = MR.MovementId
+	                    INNER JOIN [Notification].[Notification] N ON M.NotificationId = N.Id
+	                    INNER JOIN [Identity].[AspNetUsers] U ON MR.CreatedBy = U.Id
+                            LEFT JOIN [Person].[InternalUser] IU ON MR.Id = IU.UserId
+	                    WHERE MR.FileId IS NOT NULL 
+		                    AND N.CompetentAuthority = @ca
+		                    AND MR.Date BETWEEN @from AND @to),
+
+                    operationreceiptdata AS (
+                    SELECT MOR.*, IU.Id AS InternalUserId
+                    FROM
+                        [Notification].[MovementOperationReceipt] MOR
+	                    INNER JOIN [Notification].[Movement] M ON M.Id = MOR.MovementId
+	                    INNER JOIN [Notification].[Notification] N ON M.NotificationId = N.Id
+	                    INNER JOIN [Identity].[AspNetUsers] U ON MOR.CreatedBy = U.Id
+                            LEFT JOIN [Person].[InternalUser] IU ON MOR.Id = IU.UserId
+	                    WHERE MOR.FileId IS NOT NULL 
+		                    AND N.CompetentAuthority = @ca
+		                    AND MOR.Date BETWEEN @from AND @to),
+
+                    movementcreatedresult AS (
+                    SELECT
+                        COUNT(CASE WHEN InternalUserId IS NULL THEN 1 ELSE NULL END) AS MovementsCreatedExternally,
+                        COUNT(InternalUserId) AS MovementsCreatedInternally
+                    FROM movementcreateddata),
+
+                    receiptresult AS (
+                    SELECT 
+	                    COUNT(CASE WHEN InternalUserId IS NULL AND Date IS NOT NULL THEN 1 ELSE NULL END) AS MovementReceiptsCreatedExternally,
+                        COUNT(CASE WHEN InternalUserId IS NOT NULL AND Date IS NOT NULL THEN 1 ELSE NULL END) AS MovementReceiptsCreatedInternally
+                    FROM receiptdata),
+
+                    operationresult AS (
+                    SELECT 
+                        COUNT(CASE WHEN InternalUserId IS NULL AND Date IS NOT NULL THEN 1 ELSE NULL END) AS MovementOperationReceiptsCreatedExternally,
+                        COUNT(CASE WHEN InternalUserId IS NOT NULL AND Date IS NOT NULL THEN 1 ELSE NULL END) AS MovementOperationReceiptsCreatedInternally
+                    FROM operationreceiptdata)
+
+                    SELECT * FROM movementcreatedresult, receiptresult, operationresult",
                     new SqlParameter("@ca", (int)competentAuthority),
                     new SqlParameter("@from", from),
                     new SqlParameter("@to", to)).SingleAsync();
 
-            var result = new ExportMovementsData();
-            result.MovementsCreatedExternally = movementData.MovementsCreatedExternally;
-            result.MovementsCreatedInternally = movementData.MovementsCreatedInternally;
-            result.MovementReceiptsCreatedExternally = movementData.MovementReceiptsCreatedExternally;
-            result.MovementReceiptsCreatedInternally = movementData.MovementReceiptsCreatedInternally;
-            result.MovementOperationReceiptsCreatedExternally = movementData.MovementOperationReceiptsCreatedExternally;
-            result.MovementOperationReceiptsCreatedInternally = movementData.MovementOperationReceiptsCreatedInternally;
-
-            result.FilesUploadedExternally = await context.Database.SqlQuery<int>(
-                @"SELECT COUNT(FileId) 
-                    FROM Notification.Movement M
-                    LEFT JOIN [Notification].[Notification] N ON M.NotificationId = N.Id
-                    WHERE PrenotificationDate is not null 
-	                    AND FileId is not null
-	                    AND N.CompetentAuthority = @ca
-                        AND M.PrenotificationDate BETWEEN @from AND @to",
+            var userActions = await context.Database.SqlQuery<UserActionData>(
+                @"SELECT
+                    A.NewValue,
+                    A.RecordId
+                FROM[Auditing].[AuditLog] AS A
+                INNER JOIN[Notification].[Movement] AS M ON M.Id = A.RecordId
+                INNER JOIN[Notification].[Notification] AS N ON M.NotificationId = N.Id
+                INNER JOIN[Identity].[AspNetUsers] AS U ON A.UserId = U.Id
+                LEFT JOIN[Person].[InternalUser] AS IU ON U.Id = IU.UserId
+                WHERE TableName = '[Notification].[Movement]'
+                    AND EventType != 0
+                    AND IU.UserId IS NULL
+                    AND N.CompetentAuthority = @ca
+                    AND A.EventDate BETWEEN @from AND @to
+                ORDER BY RecordId, EventDate",
                 new SqlParameter("@ca", (int)competentAuthority),
                 new SqlParameter("@from", from),
-                new SqlParameter("@to", to)).SingleAsync();
+                new SqlParameter("@to", to)).ToListAsync();
 
-            return result;
+            var filesUploadedByExternalUser = new HashSet<string>();
+
+            foreach (var notificationGroup in userActions.GroupBy(a => a.RecordId))
+            {
+                for (var i = 0; i < notificationGroup.Count(); i++)
+                {
+                    UserActionJsonModel m = JsonConvert.DeserializeObject<UserActionJsonModel>(notificationGroup.ElementAt(i).NewValue);
+
+                    if (m.FileId != null)
+                    {
+                        filesUploadedByExternalUser.Add(m.FileId);
+                        i = notificationGroup.Count();
+                    }
+                }
+            }
+
+            movementData.FilesUploadedExternally = filesUploadedByExternalUser.Count;
+
+            return movementData;
         }
     }
 }
