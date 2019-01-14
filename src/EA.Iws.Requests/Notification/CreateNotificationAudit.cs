8a4a6a437b0046404fd09b9f1943208cb6e51f38
Will Andrews <will7989@hotmail.com>
2019-01-14 14:19:08 +0000

    Added auth attribute to fix unit test

--- src/EA.Iws.Requests/Notification/CreateNotificationAudit.cs
+++ src/EA.Iws.Requests/Notification/CreateNotificationAudit.cs
@@ -1,9 +1,12 @@
 ﻿namespace EA.Iws.Requests.Notification
 {
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
     using Core.Notification.Audit;
     using Prsd.Core.Mediator;
     using System;
 
+    [RequestAuthorization(ExportNotificationPermissions.CanEditExportNotification)]
     public class CreateNotificationAudit : IRequest<bool>
     {
         public Guid NotificationId { get; set; }
