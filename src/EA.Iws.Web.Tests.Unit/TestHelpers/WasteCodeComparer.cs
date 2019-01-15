389bf4946e09142814a61ea9371dea4b81dd79e7
Gonzalo Girasol <35956285+GonzaloGirasol@users.noreply.github.com>
2019-01-15 10:02:44 +0000

    IMPORT notification: Wastes in numerical order (#628)
    * Add waste code numerical order for import notification overview.
    
    * Add unit tests for the view model.
    
    * Refactor unit test.

---
+++ src/EA.Iws.Web.Tests.Unit/TestHelpers/WasteCodeComparer.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Iws.Web.Tests.Unit.TestHelpers
+{
+    using System.Collections.Generic;
+    using Core.ImportNotification.Summary;
+
+    public class WasteCodeComparer : IEqualityComparer<WasteCode>
+    {
+        public bool Equals(WasteCode x, WasteCode y)
+        {
+            return y != null && x != null && x.Name == y.Name;
+        }
+
+        public int GetHashCode(WasteCode obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+}
