0a47e65cb5b0d9e2484dbdae24a5849e1babc3ef
Will Andrews <will7989@hotmail.com>
2019-01-16 09:19:43 +0000

    Added in notification application auth check

--- src/EA.Iws.DataAccess/Repositories/NotificationAuditRepository.cs
+++ src/EA.Iws.DataAccess/Repositories/NotificationAuditRepository.cs
@@ -1,29 +1,36 @@
 ﻿namespace EA.Iws.DataAccess.Repositories
 {
-    using Domain.NotificationApplication;
     using System;
     using System.Collections.Generic;
     using System.Data.Entity;
     using System.Linq;
     using System.Threading.Tasks;
+    using Domain.NotificationApplication;
+    using Domain.Security;
 
     public class NotificationAuditRepository : INotificationAuditRepository
     {
         private readonly IwsContext context;
+        private readonly INotificationApplicationAuthorization notificationApplicationAuthorization;
 
-        public NotificationAuditRepository(IwsContext context)
+        public NotificationAuditRepository(IwsContext context, INotificationApplicationAuthorization notificationApplicationAuthorization)
         {
             this.context = context;
+            this.notificationApplicationAuthorization = notificationApplicationAuthorization;
         }
 
         public async Task AddNotificationAudit(Audit notificationAudit)
         {
+            await notificationApplicationAuthorization.EnsureAccessAsync(notificationAudit.NotificationId);
+
             this.context.NotificationAudit.Add(notificationAudit);
             await this.context.SaveChangesAsync();
         }
 
         public async Task<IEnumerable<Audit>> GetNotificationAuditsById(Guid notificationId)
         {
+            await notificationApplicationAuthorization.EnsureAccessAsync(notificationId);
+
             return await this.context.NotificationAudit.Where(p => p.NotificationId == notificationId).ToArrayAsync();
         }
     }
