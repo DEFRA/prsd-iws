ef8402ef162f47e6bc06b016875f0ad00cb162cf
Gonzalo Girasol <gonzalog@sfwltd.co.uk>
2019-01-16 09:29:54 +0000

    Redirect to NotFound if the user does not have access to a notification during a search.

--- src/EA.Iws.RequestHandlers/Notification/GetNotificationIdByNumberHandler.cs
+++ src/EA.Iws.RequestHandlers/Notification/GetNotificationIdByNumberHandler.cs
@@ -4,22 +4,35 @@
     using System.Text.RegularExpressions;
     using System.Threading.Tasks;
     using Domain.NotificationApplication;
+    using Domain.Security;
     using Prsd.Core.Mediator;
     using Requests.Notification;
 
     internal class GetNotificationIdByNumberHandler : IRequestHandler<GetNotificationIdByNumber, Guid?>
     {
         private readonly INotificationApplicationRepository notificationApplicationRepository;
+        private readonly INotificationApplicationAuthorization notificationApplicationAuthorization;
         private static readonly Regex NotificationNumberRegex = new Regex(@"(GB)(\d{4})(\d{6})", RegexOptions.Compiled);
 
-        public GetNotificationIdByNumberHandler(INotificationApplicationRepository notificationApplicationRepository)
+        public GetNotificationIdByNumberHandler(INotificationApplicationRepository notificationApplicationRepository, 
+            INotificationApplicationAuthorization notificationApplicationAuthorization)
         {
             this.notificationApplicationRepository = notificationApplicationRepository;
+            this.notificationApplicationAuthorization = notificationApplicationAuthorization;
         }
 
         public async Task<Guid?> HandleAsync(GetNotificationIdByNumber message)
         {
-            return await notificationApplicationRepository.GetIdOrDefault(FormatNotificationNumber(message.NotificationNumber));
+            var id =
+                await
+                    notificationApplicationRepository.GetIdOrDefault(FormatNotificationNumber(message.NotificationNumber));
+
+            if (id != null)
+            {
+                await notificationApplicationAuthorization.EnsureAccessAsync(id.Value);
+            }
+
+            return id;
         }
 
         private static string FormatNotificationNumber(string number)
