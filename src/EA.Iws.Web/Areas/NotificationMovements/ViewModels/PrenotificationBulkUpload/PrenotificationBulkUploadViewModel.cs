a6ac3f3c387793e3df3acaf9e1cda1a1fcfdbe75
Michael Coull <Michael.Coull@ipl.com>
2019-01-17 16:16:58 +0000

    Completed UI part of upload prenotifications page

--- src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/PrenotificationBulkUpload/PrenotificationBulkUploadViewModel.cs
+++ src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/PrenotificationBulkUpload/PrenotificationBulkUploadViewModel.cs
@@ -1,9 +1,14 @@
 ﻿namespace EA.Iws.Web.Areas.NotificationMovements.ViewModels.PrenotificationBulkUpload
 {
     using System;
+    using System.Web;
+    using Infrastructure.Validation;
 
     public class PrenotificationBulkUploadViewModel
     {
         public Guid NotificationId { get; set; }
+
+        [RestrictToAllowedUploadTypes]
+        public HttpPostedFileBase File { get; set; }
     }
 }
