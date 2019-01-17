97774de608de5adc981d55b5f71bd106099fecf5
Michael Coull <Michael.Coull@ipl.com>
2019-01-17 11:48:09 +0000

    Added the empty PrenotificationBulkUploadController

---
+++ src/EA.Iws.Web/Areas/NotificationMovements/Controllers/PrenotificationBulkUploadController.cs
@@ -0,0 +1,15 @@
+﻿namespace EA.Iws.Web.Areas.NotificationMovements.Controllers
+{
+    using System.Web.Mvc;
+    using Prsd.Core.Mediator;
+
+    public class PrenotificationBulkUploadController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public PrenotificationBulkUploadController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+    }
+}
