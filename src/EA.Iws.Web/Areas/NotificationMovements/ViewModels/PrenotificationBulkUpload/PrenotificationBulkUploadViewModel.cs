a5e924bea6e35efe53e71b869330ea3851b46fff
Michael Coull <Michael.Coull@ipl.com>
2019-01-17 14:16:51 +0000

    Created a generic view model for prenotification bulk upload

---
+++ src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/PrenotificationBulkUpload/PrenotificationBulkUploadViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EA.Iws.Web.Areas.NotificationMovements.ViewModels.PrenotificationBulkUpload
+{
+    using System;
+
+    public class PrenotificationBulkUploadViewModel
+    {
+        public Guid NotificationId { get; set; }
+    }
+}
