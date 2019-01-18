c54db1a5818a751028b861cfc6c6909cc1c82681
Michael Coull <Michael.Coull@ipl.com>
2019-01-18 16:50:59 +0000

    The upload prenotifications page now requires a file to be chosen, otherwise an error message is shown.

--- src/EA.Iws.Web/Areas/NotificationMovements/Controllers/PrenotificationBulkUploadController.cs
+++ src/EA.Iws.Web/Areas/NotificationMovements/Controllers/PrenotificationBulkUploadController.cs
@@ -1,5 +1,7 @@
 ﻿namespace EA.Iws.Web.Areas.NotificationMovements.Controllers
 {
+    using System;
+    using System.Threading.Tasks;
     using System.Web.Mvc;
     using Prsd.Core.Mediator;
     using ViewModels.PrenotificationBulkUpload;
@@ -14,9 +16,25 @@
         }
 
         [HttpGet]
-        public ActionResult UploadPrenotifications()
+        public ActionResult UploadPrenotifications(Guid notificationId)
         {
             var model = new PrenotificationBulkUploadViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> UploadPrenotifications(Guid notificationId, PrenotificationBulkUploadViewModel model2)
+        {
+            PrenotificationBulkUploadViewModel model = new PrenotificationBulkUploadViewModel();
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.NotificationId = notificationId;
+                model = new PrenotificationBulkUploadViewModel();
+                return View(model);
+            }
 
             return View(model);
         }
--- src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/PrenotificationBulkUpload/PrenotificationBulkUploadViewModel.cs
+++ src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/PrenotificationBulkUpload/PrenotificationBulkUploadViewModel.cs
@@ -1,14 +1,29 @@
 ﻿namespace EA.Iws.Web.Areas.NotificationMovements.ViewModels.PrenotificationBulkUpload
 {
     using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Web;
     using Infrastructure.Validation;
 
-    public class PrenotificationBulkUploadViewModel
+    public class PrenotificationBulkUploadViewModel : IValidatableObject
     {
+        public PrenotificationBulkUploadViewModel()
+        {
+        }
+
         public Guid NotificationId { get; set; }
 
+        [Display(Name = "Upload the signed copy of the prenotification document")]
         [RestrictToAllowedUploadTypes]
         public HttpPostedFileBase File { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (File == null || File.InputStream.Length == 0)
+            {
+                yield return new ValidationResult("Please upload the signed copy of the prenotification document", new[] { "File" });
+            }
+        }
     }
 }
