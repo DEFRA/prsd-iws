06f6d13d429e57fd4c64f7c27aad86d8187cfea5
Gonzalo Girasol <gonzalog@sfwltd.co.uk>
2019-01-21 16:30:21 +0000

    Add new screen for bulk upload template.

---
+++ src/EA.Iws.Web/Areas/NotificationMovements/Controllers/BulkUploadTemplateController.cs
@@ -0,0 +1,39 @@
+﻿namespace EA.Iws.Web.Areas.NotificationMovements.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Prsd.Core.Mediator;
+    using Requests.Notification;
+    using ViewModels.BulkUploadTemplate;
+
+    [Authorize]
+    public class BulkUploadTemplateController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public BulkUploadTemplateController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index(Guid notificationId)
+        {
+            var data = await mediator.SendAsync(new GetNotificationBasicInfo(notificationId));
+            var model = new BulkUploadTemplateViewModel()
+            {
+                NotificationId = data.NotificationId,
+                NotificationNumber = data.NotificationNumber
+            };
+        
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult PrenotificationTemplate()
+        {
+            return View("Index");
+        }
+    }
+}
