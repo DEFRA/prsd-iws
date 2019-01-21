1d23e97ecb3eae8060fd17533c9b80c77cf40f83
MichaelCoull1 <45666515+MichaelCoull1@users.noreply.github.com>
2019-01-21 10:50:19 +0000

    Feature/prenotification bulk upload upload prenotifications (#649)
    * Added UploadPrenotifications method to controller
    
    * Added the UploadPrenotifications page (WIP)
    
    * UploadPrenotifications page now points to correct resource file
    
    * Minor changes to UploadPrenotifications.cshtml
    
    * Made the initial upload prenotifications page work
    
    * Completed UI part of upload prenotifications page
    
    * The upload prenotifications page now requires a file to be chosen, otherwise an error message is shown.
    
    * Removed unnecessary variable from PrenotificationBulkUploadController
    
    * Fixed the link to the notification options page
    
    * Added Authorize attribute to PrenotificationBulkUploadController
    
    * Added empty constructor for PrenotificationBulkUploadViewModel
    
    * Removed async keyword and changed return type of the UploadPrenotifications method.

--- src/EA.Iws.Web/Areas/NotificationMovements/Controllers/PrenotificationBulkUploadController.cs
+++ src/EA.Iws.Web/Areas/NotificationMovements/Controllers/PrenotificationBulkUploadController.cs
@@ -1,8 +1,12 @@
 ﻿namespace EA.Iws.Web.Areas.NotificationMovements.Controllers
 {
+    using System;
+    using System.Threading.Tasks;
     using System.Web.Mvc;
     using Prsd.Core.Mediator;
+    using ViewModels.PrenotificationBulkUpload;
 
+    [Authorize]
     public class PrenotificationBulkUploadController : Controller
     {
         private readonly IMediator mediator;
@@ -10,6 +14,28 @@
         public PrenotificationBulkUploadController(IMediator mediator)
         {
             this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public ActionResult UploadPrenotifications(Guid notificationId)
+        {
+            var model = new PrenotificationBulkUploadViewModel(notificationId);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UploadPrenotifications(Guid notificationId, PrenotificationBulkUploadViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.NotificationId = notificationId;
+                model = new PrenotificationBulkUploadViewModel(notificationId);
+                return View(model);
+            }
+
+            return View(model);
         }
     }
 }
--- src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/PrenotificationBulkUpload/PrenotificationBulkUploadViewModel.cs
+++ src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/PrenotificationBulkUpload/PrenotificationBulkUploadViewModel.cs
@@ -1,9 +1,34 @@
 ﻿namespace EA.Iws.Web.Areas.NotificationMovements.ViewModels.PrenotificationBulkUpload
 {
     using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web;
+    using Infrastructure.Validation;
 
-    public class PrenotificationBulkUploadViewModel
+    public class PrenotificationBulkUploadViewModel : IValidatableObject
     {
+        public PrenotificationBulkUploadViewModel()
+        {
+        }
+
+        public PrenotificationBulkUploadViewModel(Guid notificationId)
+        {
+            this.NotificationId = notificationId;
+        }
+
         public Guid NotificationId { get; set; }
+
+        [Display(Name = "Upload the signed copy of the prenotification document")]
+        [RestrictToAllowedUploadTypes]
+        public HttpPostedFileBase File { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (File == null || File.InputStream.Length == 0)
+            {
+                yield return new ValidationResult("Please upload the signed copy of the prenotification document", new[] { "File" });
+            }
+        }
     }
 }
