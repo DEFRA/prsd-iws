ccfe35b4c474a22a862d389601a7c0badca1007d
Gonzalo Girasol <gonzalog@sfwltd.co.uk>
2019-01-23 15:10:05 +0000

    Update conditional expression to work with CI build.

--- src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/PrenotificationBulkUpload/PrenotificationBulkUploadViewModel.cs
+++ src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/PrenotificationBulkUpload/PrenotificationBulkUploadViewModel.cs
@@ -24,7 +24,7 @@
         {
             get
             {
-                return FailedFileRules?.Count ?? 0;
+                return FailedFileRules != null ? FailedFileRules.Count : 0;
             }
         }
 
