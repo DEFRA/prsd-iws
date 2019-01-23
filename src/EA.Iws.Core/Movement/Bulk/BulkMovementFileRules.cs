cb54719fe814f446430e209269c0877b8b836145
Gonzalo Girasol <gonzalog@sfwltd.co.uk>
2019-01-23 15:59:15 +0000

    Fix spacing.

--- src/EA.Iws.Core/Movement/Bulk/BulkMovementFileRules.cs
+++ src/EA.Iws.Core/Movement/Bulk/BulkMovementFileRules.cs
@@ -4,11 +4,11 @@
 
     public enum BulkMovementFileRules
     {
-        [Display(Name ="The file type must be either an EXCEL or .CSV format.")]
+        [Display(Name = "The file type must be either an EXCEL or .CSV format.")]
         FileType,
         [Display(Name = "The file size must not be larger than 2GB.")]
         FileSize,
-        [Display(Name = "We've detected a virus in the file you uploaded. ")]
+        [Display(Name = "We've detected a virus in the file you uploaded.")]
         Virus
     }
 }
