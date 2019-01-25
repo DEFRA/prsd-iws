85988e41cff894c5d231d23aaaa953cd448297d8
Will Andrews <will7989@hotmail.com>
2019-01-25 13:18:55 +0000

    Added notification ID to the request handler to use throughout validation rules (#677)

--- src/EA.Iws.Core/Movement/Bulk/IBulkMovementPrenotificationContentRule.cs
+++ src/EA.Iws.Core/Movement/Bulk/IBulkMovementPrenotificationContentRule.cs
@@ -1,11 +1,12 @@
 ﻿namespace EA.Iws.Core.Movement.Bulk
 {
+    using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Core.Movement.Bulk;
 
     public interface IBulkMovementPrenotificationContentRule
     {
-        Task<ContentRuleResult<BulkMovementContentRules>> GetResult(List<PrenotificationMovement> shipments);
+        Task<ContentRuleResult<BulkMovementContentRules>> GetResult(List<PrenotificationMovement> shipments, Guid notificationId);
     }
 }
