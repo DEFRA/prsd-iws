fbea6d7447a5f091db63dd7691a67e45e31717d3
Michael Coull <Michael.Coull@ipl.com>
2019-01-25 13:28:36 +0000

    Added the empty file for checking for missing shipment numbers

---
+++ src/EA.Iws.RequestHandlers/NotificationMovements/BulkUpload/PrenotificationContentMissingShipmentNumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EA.Iws.RequestHandlers.NotificationMovements.BulkUpload
+{
+    public class PrenotificationContentMissingShipmentNumber
+    {
+
+    }
+}
