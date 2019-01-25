7299897c908a06d204da257d8dfaa486a5b48a90
Michael Coull <Michael.Coull@ipl.com>
2019-01-25 14:39:53 +0000

    Added logic for PrenotificationContentMissingShipmentNumberRule

--- src/EA.Iws.Core/Movement/Bulk/BulkMovementContentRules.cs
+++ src/EA.Iws.Core/Movement/Bulk/BulkMovementContentRules.cs
@@ -5,6 +5,8 @@
     public enum BulkMovementContentRules
     {
         [Display(Name = "Shipment number/s {0}: there is missing data")]
-        MissingData
+        MissingData,
+        [Display(Name = "You can't create {0} shipment(s) as they are missing a value for shipment number ")]
+        MissingShipmentNumbers
     }
 }
---
+++ src/EA.Iws.RequestHandlers/NotificationMovements/BulkUpload/PrenotificationContentMissingShipmentNumberRule.cs
@@ -0,0 +1,32 @@
+﻿namespace EA.Iws.RequestHandlers.NotificationMovements.BulkUpload
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Core.Movement.Bulk;
+    using Core.Rules;
+
+    public class PrenotificationContentMissingShipmentNumberRule : IBulkMovementPrenotificationContentRule
+    {
+        public async Task<ContentRuleResult<BulkMovementContentRules>> GetResult(List<PrenotificationMovement> shipments)
+        {
+            return await Task.Run(() =>
+            {
+                var missingShipmentNumberResult = MessageLevel.Success;
+                int missingShipmentNumberCount = 0;
+
+                foreach (PrenotificationMovement shipment in shipments)
+                {
+                    if (shipment.ShipmentNumber.Equals(string.Empty))
+                    {
+                        missingShipmentNumberResult = MessageLevel.Error;
+                        missingShipmentNumberCount++;
+                    }
+                }
+
+                // COULLM: missingShipmentNumberCount needs to be used to populate error message in ContentRuleResult 
+
+                return new ContentRuleResult<BulkMovementContentRules>(BulkMovementContentRules.MissingShipmentNumbers, missingShipmentNumberResult, new List<string>());
+            });
+        }
+    }
+}
