ec81fa861fa16d890a3f1b329edfe4c1a5b2dc02
Michael Coull <Michael.Coull@ipl.com>
2019-01-25 14:08:14 +0000

    Merged common branch into this branch and fixed the shipment number rule.

--- src/EA.Iws.RequestHandlers/NotificationMovements/BulkUpload/PrenotificationContentDuplicateShipmentNumberRule.cs
+++ src/EA.Iws.RequestHandlers/NotificationMovements/BulkUpload/PrenotificationContentDuplicateShipmentNumberRule.cs
@@ -1,5 +1,6 @@
 ﻿namespace EA.Iws.RequestHandlers.NotificationMovements.BulkUpload
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
@@ -8,7 +9,7 @@
 
     public class PrenotificationContentDuplicateShipmentNumberRule : IBulkMovementPrenotificationContentRule
     {
-        public async Task<ContentRuleResult<BulkMovementContentRules>> GetResult(List<PrenotificationMovement> shipments)
+        public async Task<ContentRuleResult<BulkMovementContentRules>> GetResult(List<PrenotificationMovement> shipments, Guid notificationId)
         {
             return await Task.Run(() =>
             {
