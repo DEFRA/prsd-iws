226212f05411578ad419fe154232a6d3dcc26d56
MichaelCoull1 <45666515+MichaelCoull1@users.noreply.github.com>
2019-01-25 14:59:22 +0000

    Content rule added for dupliate shipping numbers (#676)
    * Content rule added for dupliate shipping numbers
    
    * Merged common branch into this branch and fixed the shipment number rule.

---
+++ src/EA.Iws.RequestHandlers/NotificationMovements/BulkUpload/PrenotificationContentDuplicateShipmentNumberRule.cs
@@ -0,0 +1,31 @@
+﻿namespace EA.Iws.RequestHandlers.NotificationMovements.BulkUpload
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Core.Movement.Bulk;
+    using Core.Rules;
+
+    public class PrenotificationContentDuplicateShipmentNumberRule : IBulkMovementPrenotificationContentRule
+    {
+        public async Task<ContentRuleResult<BulkMovementContentRules>> GetResult(List<PrenotificationMovement> shipments, Guid notificationId)
+        {
+            return await Task.Run(() =>
+            {
+                var duplicateShipmentNumberResult = MessageLevel.Success;
+                var duplicateShipmentNumbers = shipments.GroupBy(x => x.ShipmentNumber)
+                    .Where(g => g.Count() > 1)
+                    .Select(y => y.Key)
+                    .ToList();
+
+                if (duplicateShipmentNumbers.Count > 0)
+                {
+                    duplicateShipmentNumberResult = MessageLevel.Error;
+                }
+
+                return new ContentRuleResult<BulkMovementContentRules>(BulkMovementContentRules.DuplicateShipmentNumber, duplicateShipmentNumberResult, duplicateShipmentNumbers);
+            });
+        }
+    }
+}
