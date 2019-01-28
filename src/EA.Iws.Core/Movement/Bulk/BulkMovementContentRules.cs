b54a741359ab670d6238f896f0157c04bbcfc532
Gonzalo Girasol <35956285+GonzaloGirasol@users.noreply.github.com>
2019-01-28 15:24:01 +0000

    Prenotification bulk upload - 3 working days (#686)
    * Add content rule for 3 working days to the consent end date. Include unit test.
    
    * Add rule for shiment date within three working days from the current date.
    
    * Update rule to ensure the shipment date is in the future.
    
    * Add extra html markup for three or less working days content rule.

--- src/EA.Iws.Core/Movement/Bulk/BulkMovementContentRules.cs
+++ src/EA.Iws.Core/Movement/Bulk/BulkMovementContentRules.cs
@@ -18,7 +18,7 @@
         MissingData,
         [Display(Name = "You can't create {0} shipment(s) as they are missing a value for shipment number.")]
         MissingShipmentNumbers,
-        [Display(Name = "Shipment number/s {0}: data must only be for notification number {1}")]
+        [Display(Name = "Shipment number {0}: data must only be for notification number {1}")]
         WrongNotificationNumber,
         [Display(Name = "Shipment number {0}: this shipment number already exists.")]
         OnlyNewShipments,
@@ -33,6 +33,10 @@
         [Display(Name = "Shipment number {0}: the waste quantity should have no more than 4 decimal places.")]
         QuantityPrecision,
         [Display(Name = "Shipment number {0}: the quantity unit of measurement is not permitted on this notification.")]
-        QuantityUnit
+        QuantityUnit,
+        [Display(Name = "Shipment number {0}: the date of shipment is less than three working days from the notification expiry date. Remove the shipment data from the data file and use the existing ‘generate a prenotification’ journey to inform us of this shipment.")]
+        ThreeWorkingDaysToConsentDate,
+        [Display(Name = "Shipment {0}: the actual shipment date is less than 3 working days.")]
+        ThreeWorkingDaysToShipment
     }
 }
