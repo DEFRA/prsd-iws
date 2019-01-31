619d9fc96e132c16273c7a11d330ec1edb0c04d5
MichaelCoull1 <45666515+MichaelCoull1@users.noreply.github.com>
2019-01-31 09:21:27 +0000

    Feature/prenote confirmation screen (#693)
    * Added bulk of contents to success page (WIP)
    
    * Tweaked css to achieve desired design

--- src/EA.Iws.Web/Areas/NotificationMovements/Views/PrenotificationBulkUpload/PrenotificationBulkUploadResources.Designer.cs
+++ src/EA.Iws.Web/Areas/NotificationMovements/Views/PrenotificationBulkUpload/PrenotificationBulkUploadResources.Designer.cs
@@ -242,6 +242,33 @@ namespace EA.Iws.Web.Areas.NotificationMovements.Views.PrenotificationBulkUpload
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to You have uploaded:.
+        /// </summary>
+        public static string SuccessText {
+            get {
+                return ResourceManager.GetString("SuccessText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Your prenotifications of shipments {0} have now been recorded in the IWS service..
+        /// </summary>
+        public static string SuccessTextMultipleShipments {
+            get {
+                return ResourceManager.GetString("SuccessTextMultipleShipments", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Your prenotification of shipment {0} has now been recorded in the IWS service..
+        /// </summary>
+        public static string SuccessTextSingleShipment {
+            get {
+                return ResourceManager.GetString("SuccessTextSingleShipment", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Success.
         /// </summary>
         public static string SuccessTitle {
