5c0415e4527ec3073b5eb112b816fafba761b5aa
MichaelCoull1 <45666515+MichaelCoull1@users.noreply.github.com>
2019-02-01 14:28:23 +0000

    Added the initial, empty classes for receipt recovery bulk upload: co… (#699)
    * Added the initial, empty classes for receipt recovery bulk upload: controller and view model
    
    * Added the view resource file

---
+++ src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/ReceiptRecoveryBulkUpload/ReceiptRecoveryBulkUploadViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace EA.Iws.Web.Areas.NotificationMovements.ViewModels.PrenotificationBulkUpload
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ReceiptRecoveryBulkUploadViewModel : IValidatableObject
+    {
+        public ReceiptRecoveryBulkUploadViewModel()
+        {
+        }
+
+        public ReceiptRecoveryBulkUploadViewModel(Guid notificationId)
+        {
+            this.NotificationId = notificationId;
+        }
+
+        public Guid NotificationId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
