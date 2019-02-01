0a77b582289050135a6d6146535425dfa91ad14b
Michael Coull <Michael.Coull@ipl.com>
2019-02-01 10:40:11 +0000

    Added the initial, empty classes for receipt recovery bulk upload: controller and view model

---
+++ src/EA.Iws.Web/Areas/NotificationMovements/Controllers/ReceiptRecoveryBulkUploadController.cs
@@ -0,0 +1,33 @@
+﻿namespace EA.Iws.Web.Areas.NotificationMovements.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+    using Infrastructure;
+    using Infrastructure.BulkUpload;
+    using Prsd.Core.Mediator;
+    using ViewModels.PrenotificationBulkUpload;
+
+    [Authorize]
+    public class ReceiptRecoveryBulkUploadController : Controller
+    {
+        private readonly IMediator mediator;
+        private readonly IBulkMovementValidator validator;
+        private readonly IFileReader fileReader;
+
+        public ReceiptRecoveryBulkUploadController(IMediator mediator, IBulkMovementValidator validator, IFileReader fileReader)
+        {
+            this.mediator = mediator;
+            this.validator = validator;
+            this.fileReader = fileReader;
+        }
+
+        [HttpGet]
+        public ActionResult Index(Guid notificationId)
+        {
+            ViewBag.NotificationId = notificationId;
+
+            var model = new ReceiptRecoveryBulkUploadViewModel(notificationId);
+            return View("Index", model);
+        }
+    }
+}
