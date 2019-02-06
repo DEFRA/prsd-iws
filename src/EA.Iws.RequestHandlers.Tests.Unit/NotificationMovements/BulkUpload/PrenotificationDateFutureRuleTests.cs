12e3bbaabbed1817211b2d935419b12177e85d4e
Gonzalo Girasol <35956285+GonzaloGirasol@users.noreply.github.com>
2019-02-06 13:18:33 +0000

    Bulk prenote validations (#716)
    * Ensure enum value is defined when parsing.
    
    * New Content validation to check if date of shipment is more than 30 calender days in the future.
    
    * Fix issue with shipping document confirmation text.
    
    * Add Total Active Loads Reached error view.
    
    * Fix issue with Total Actives Reached resource file.

---
+++ src/EA.Iws.RequestHandlers.Tests.Unit/NotificationMovements/BulkUpload/PrenotificationDateFutureRuleTests.cs
@@ -0,0 +1,71 @@
+﻿namespace EA.Iws.RequestHandlers.Tests.Unit.NotificationMovements.BulkUpload
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Core.Movement.BulkPrenotification;
+    using Core.Rules;
+    using Prsd.Core;
+    using RequestHandlers.NotificationMovements.BulkPrenotification;
+    using Xunit;
+
+    public class PrenotificationDateFutureRuleTests
+    {
+        private readonly PrenotificationDateFutureRule rule;
+        private readonly Guid notificationId;
+        private const int MaxDays = 30;
+
+        public PrenotificationDateFutureRuleTests()
+        {
+            rule = new PrenotificationDateFutureRule();
+
+            notificationId = Guid.NewGuid();
+        }
+
+        [Fact]
+        public async Task LessThanMaxDays_Success()
+        {
+            var movements = new List<PrenotificationMovement>()
+            {
+                new PrenotificationMovement()
+                {
+                    ShipmentNumber = 1,
+                    ActualDateOfShipment = SystemTime.UtcNow.AddDays(MaxDays - 10)
+                },
+                new PrenotificationMovement()
+                {
+                    ShipmentNumber = 2,
+                    ActualDateOfShipment = SystemTime.UtcNow.AddDays(MaxDays - 20)
+                }
+            };
+
+            var result = await rule.GetResult(movements, notificationId);
+
+            Assert.Equal(PrenotificationContentRules.FutureDate, result.Rule);
+            Assert.Equal(MessageLevel.Success, result.MessageLevel);
+        }
+
+        [Fact]
+        public async Task MoreThanMaxDays_Error()
+        {
+            var movements = new List<PrenotificationMovement>()
+            {
+                new PrenotificationMovement()
+                {
+                    ShipmentNumber = 1,
+                    ActualDateOfShipment = SystemTime.UtcNow.AddDays(MaxDays + 1)
+                },
+                new PrenotificationMovement()
+                {
+                    ShipmentNumber = 2,
+                    ActualDateOfShipment = SystemTime.UtcNow.AddDays(MaxDays + 10)
+                }
+            };
+
+            var result = await rule.GetResult(movements, notificationId);
+
+            Assert.Equal(PrenotificationContentRules.FutureDate, result.Rule);
+            Assert.Equal(MessageLevel.Error, result.MessageLevel);
+        }
+    }
+}
---
+++ src/EA.Iws.RequestHandlers/NotificationMovements/BulkPrenotification/PrenotificationDateFutureRule.cs
@@ -0,0 +1,37 @@
+﻿namespace EA.Iws.RequestHandlers.NotificationMovements.BulkPrenotification
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Core.Movement.BulkPrenotification;
+    using Core.Rules;
+    using Prsd.Core;
+
+    public class PrenotificationDateFutureRule : IPrenotificationContentRule
+    {
+        private const int MaxDays = 30;
+
+        public async Task<PrenotificationContentRuleResult<PrenotificationContentRules>> GetResult(List<PrenotificationMovement> movements, Guid notificationId)
+        {
+            return await Task.Run(() =>
+            {
+                var shipments =
+                    movements.Where(
+                            m =>
+                                m.ShipmentNumber.HasValue && m.ActualDateOfShipment.HasValue &&
+                                m.ActualDateOfShipment.Value.Date > SystemTime.UtcNow.Date &&
+                                (m.ActualDateOfShipment.Value.Date - SystemTime.UtcNow.Date).TotalDays > MaxDays)
+                        .Select(m => m.ShipmentNumber.Value)
+                        .ToList();
+
+                var result = shipments.Any() ? MessageLevel.Error : MessageLevel.Success;
+
+                var shipmentNumbers = string.Join(", ", shipments);
+                var errorMessage = string.Format(Prsd.Core.Helpers.EnumHelper.GetDisplayName(PrenotificationContentRules.FutureDate), shipmentNumbers, MaxDays);
+
+                return new PrenotificationContentRuleResult<PrenotificationContentRules>(PrenotificationContentRules.FutureDate, result, errorMessage);
+            });
+        }
+    }
+}
