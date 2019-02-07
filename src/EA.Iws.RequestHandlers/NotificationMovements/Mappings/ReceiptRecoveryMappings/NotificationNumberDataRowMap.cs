4793f49406d8e90e6ee47a5763e30127f5f86ecd
Michael Coull <Michael.Coull@ipl.com>
2019-02-07 15:03:36 +0000

    Corrected the data mapping for receipt recovery. Added the validation for the receipt date format and the relevant unit tests.

---
+++ src/EA.Iws.RequestHandlers/NotificationMovements/Mappings/ReceiptRecoveryMappings/NotificationNumberDataRowMap.cs
@@ -0,0 +1,44 @@
+﻿namespace EA.Iws.RequestHandlers.NotificationMovements.Mappings.ReceiptRecoveryMappings
+{
+    using System.Text.RegularExpressions;
+    using Core.Movement.BulkReceiptRecovery;
+    using Prsd.Core.Mapper;
+
+    public class NotificationNumberDataRowMap : IMap<ReceiptRecoveryDataRow, string>
+    {
+        private static readonly Regex NotificationNumberRegex = new Regex(@"(GB)(\d{4})(\d{6})", RegexOptions.Compiled);
+
+        public string Map(ReceiptRecoveryDataRow source)
+        {
+            string result = null;
+
+            try
+            {
+                result = source.DataRow.ItemArray[(int)ReceiptRecoveryColumnIndex.NotificationNumber].ToString();
+            }
+            catch
+            {
+                //ignored
+            }
+
+            return FormatNotificationNumber(result);
+        }
+
+        private static string FormatNotificationNumber(string number)
+        {
+            if (string.IsNullOrWhiteSpace(number))
+            {
+                return string.Empty;
+            }
+
+            number = number.ToUpper().Replace(" ", string.Empty);
+
+            if (NotificationNumberRegex.IsMatch(number))
+            {
+                number = NotificationNumberRegex.Replace(number, "$1 $2 $3");
+            }
+
+            return number;
+        }
+    }
+}
