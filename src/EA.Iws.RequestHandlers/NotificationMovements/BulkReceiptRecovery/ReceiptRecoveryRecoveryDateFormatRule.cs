099a28fa8231533b533947e827fa19b61fdfa1aa
Will Andrews <will7989@hotmail.com>
2019-02-08 16:11:40 +0000

    Feature/receipt recovery content validation will (#729)
    * IReceiptRecoveryContentRule interface is now registered as an assembly type
    
    * Started on rules
    
    * Addded final content rules
    
    * Fixed PR queries

---
+++ src/EA.Iws.RequestHandlers/NotificationMovements/BulkReceiptRecovery/ReceiptRecoveryRecoveryDateFormatRule.cs
@@ -0,0 +1,38 @@
+﻿namespace EA.Iws.RequestHandlers.NotificationMovements.BulkReceiptRecovery
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Core.Movement.BulkReceiptRecovery;
+    using Core.Rules;
+
+    public class ReceiptRecoveryRecoveryDateFormatRule : IReceiptRecoveryContentRule
+    {
+        public ReceiptRecoveryRecoveryDateFormatRule()
+        {
+        }
+
+        public async Task<ReceiptRecoveryContentRuleResult<ReceiptRecoveryContentRules>> GetResult(List<ReceiptRecoveryMovement> movements, Guid notificationId)
+        {
+            return await Task.Run(() =>
+            {
+                List<int> shipments = new List<int>();
+                MessageLevel result = MessageLevel.Success;
+
+                foreach (var movement in movements)
+                {
+                    if (movement.RecoveredDisposedDate == null)
+                    {
+                        result = MessageLevel.Error;
+                        shipments.Add(movement.ShipmentNumber.GetValueOrDefault());
+                    }
+                }
+
+                var shipmentNumbers = string.Join(", ", shipments);
+                var errorMessage = string.Format(Prsd.Core.Helpers.EnumHelper.GetDisplayName(ReceiptRecoveryContentRules.RecoveryDateFormat), shipmentNumbers);
+
+                return new ReceiptRecoveryContentRuleResult<ReceiptRecoveryContentRules>(ReceiptRecoveryContentRules.RecoveryDateFormat, result, errorMessage);
+            });
+        }
+    }
+}
---
+++ src/EA.Iws.RequestHandlers/NotificationMovements/BulkReceiptRecovery/ReceiptRecoveryRecoveryQuantityUnitRule.cs
@@ -0,0 +1,47 @@
+﻿namespace EA.Iws.RequestHandlers.NotificationMovements.BulkReceiptRecovery
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Core.Movement;
+    using Core.Movement.BulkReceiptRecovery;
+    using Core.Rules;
+    using Core.Shared;
+    using Domain.Movement;
+    using Domain.NotificationApplication.Shipment;
+
+    public class ReceiptRecoveryRecoveryQuantityUnitRule : IReceiptRecoveryContentRule
+    {
+        private readonly IShipmentInfoRepository repo;
+
+        public ReceiptRecoveryRecoveryQuantityUnitRule(IShipmentInfoRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<ReceiptRecoveryContentRuleResult<ReceiptRecoveryContentRules>> GetResult(List<ReceiptRecoveryMovement> movements, Guid notificationId)
+        {
+            var shippingInfo = await repo.GetByNotificationId(notificationId);
+            var units = shippingInfo == null ? default(ShipmentQuantityUnits) : shippingInfo.Units;
+            var availableUnits = ShipmentQuantityUnitsMetadata.GetUnitsOfThisType(units).ToList();
+
+            List<int> shipments = new List<int>();
+            MessageLevel result = MessageLevel.Success;
+
+            foreach (var movement in movements)
+            {
+                if (availableUnits.Count(p => p == movement.Unit) == 0)
+                {
+                    result = MessageLevel.Error;
+                    shipments.Add(movement.ShipmentNumber.GetValueOrDefault());
+                }
+            }
+
+            var shipmentNumbers = string.Join(", ", shipments);
+            var errorMessage = string.Format(Prsd.Core.Helpers.EnumHelper.GetDisplayName(ReceiptRecoveryContentRules.QuantityUnit), shipmentNumbers);
+
+            return new ReceiptRecoveryContentRuleResult<ReceiptRecoveryContentRules>(ReceiptRecoveryContentRules.QuantityUnit, result, errorMessage);
+        }
+    }
+}
