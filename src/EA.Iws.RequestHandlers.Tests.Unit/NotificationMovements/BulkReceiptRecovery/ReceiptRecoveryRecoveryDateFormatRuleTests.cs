0f46ddaf161c2779fbaae07a3e9f47308faf5fe2
Will Andrews <will7989@hotmail.com>
2019-02-08 10:29:29 +0000

    Addded final content rules

---
+++ src/EA.Iws.RequestHandlers.Tests.Unit/NotificationMovements/BulkReceiptRecovery/ReceiptRecoveryRecoveryDateFormatRuleTests.cs
@@ -0,0 +1,64 @@
+﻿namespace EA.Iws.RequestHandlers.Tests.Unit.NotificationMovements.BulkReceiptRecovery
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Core.Movement.BulkReceiptRecovery;
+    using Core.Rules;
+    using RequestHandlers.NotificationMovements.BulkReceiptRecovery;
+    using Xunit;
+
+    public class ReceiptRecoveryRecoveryDateFormatRuleTests
+    {
+        private readonly ReceiptRecoveryRecoveryDateFormatRule rule;
+        private readonly Guid notificationId;
+
+        public ReceiptRecoveryRecoveryDateFormatRuleTests()
+        {
+            rule = new ReceiptRecoveryRecoveryDateFormatRule();
+            notificationId = Guid.NewGuid();
+        }
+
+        [Fact]
+        public async Task CorrectDate_Success()
+        {
+            var result = await rule.GetResult(GetTestData(false), notificationId);
+
+            Assert.Equal(ReceiptRecoveryContentRules.RecoveryDateFormat, result.Rule);
+            Assert.Equal(MessageLevel.Success, result.MessageLevel);
+        }
+
+        [Fact]
+        public async Task IncorrectDate_Failure()
+        {
+            var result = await rule.GetResult(GetTestData(true), notificationId);
+
+            Assert.Equal(ReceiptRecoveryContentRules.RecoveryDateFormat, result.Rule);
+            Assert.Equal(MessageLevel.Error, result.MessageLevel);
+        }
+
+        private List<ReceiptRecoveryMovement> GetTestData(bool invalidDate)
+        {
+            DateTime? date = null;
+
+            if (!invalidDate)
+            {
+                date = DateTime.Parse("20/02/2019");
+            }
+
+            return new List<ReceiptRecoveryMovement>()
+            {
+                new ReceiptRecoveryMovement()
+                {
+                    ShipmentNumber = 1,
+                    RecoveredDisposedDate = date
+                },
+                new ReceiptRecoveryMovement()
+                {
+                    ShipmentNumber = 2,
+                    RecoveredDisposedDate = DateTime.Parse("20/02/2019")
+                }
+            };
+        }
+    }
+}
---
+++ src/EA.Iws.RequestHandlers/NotificationMovements/BulkReceiptRecovery/ReceiptRecoveryRecoveryQuantityUnitRule.cs
@@ -0,0 +1,44 @@
+﻿namespace EA.Iws.RequestHandlers.NotificationMovements.BulkReceiptRecovery
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Core.Movement;
+    using Core.Movement.BulkReceiptRecovery;
+    using Core.Rules;
+    using Domain.Movement;
+    using Domain.NotificationApplication.Shipment;
+
+    public class ReceiptRecoveryRecoveryQuantityUnitRule : IReceiptRecoveryContentRule
+    {
+        private readonly IShipmentInfoRepository repo;
+
+        public ReceiptRecoveryRecoveryQuantityUnitRule(IShipmentInfoRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<ReceiptRecoveryContentRuleResult<ReceiptRecoveryContentRules>> GetResult(List<ReceiptRecoveryMovement> movements, Guid notificationId)
+        {
+            var shippingInfo = await repo.GetByNotificationId(notificationId);
+
+            List<int> shipments = new List<int>();
+            MessageLevel result = MessageLevel.Success;
+
+            foreach (var movement in movements)
+            {
+                if (shippingInfo.Units != movement.Unit)
+                {
+                    result = MessageLevel.Error;
+                    shipments.Add(movement.ShipmentNumber.GetValueOrDefault());
+                }
+            }
+
+            var shipmentNumbers = string.Join(", ", shipments);
+            var errorMessage = string.Format(Prsd.Core.Helpers.EnumHelper.GetDisplayName(ReceiptRecoveryContentRules.QuantityUnit), shipmentNumbers);
+
+            return new ReceiptRecoveryContentRuleResult<ReceiptRecoveryContentRules>(ReceiptRecoveryContentRules.QuantityUnit, result, errorMessage);
+        }
+    }
+}
