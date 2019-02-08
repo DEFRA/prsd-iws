7bec14cb32a34cf5a7d7a38e3583caaf90f2bd5b
MichaelCoull1 <45666515+MichaelCoull1@users.noreply.github.com>
2019-02-08 10:01:06 +0000

    Feature/receipt recovery content validation mike (#728)
    * IReceiptRecoveryContentRule interface is now registered as an assembly type
    
    * Added receipt/recovery bulk upload rule for when the notification number is not what is expected.
    
    * Corrected the data mapping for receipt recovery. Added the validation for the receipt date format and the relevant unit tests.
    
    * Updated the link text as per task 67050
    
    * Added the validation rule for receipt recovery date.
    
    * Added the unit tests for receipt recovery receipt date validation rule

---
+++ src/EA.Iws.RequestHandlers/NotificationMovements/Mappings/ReceiptRecoveryMappings/DateDataRowMap.cs
@@ -0,0 +1,42 @@
+﻿namespace EA.Iws.RequestHandlers.NotificationMovements.Mappings.ReceiptRecoveryMappings
+{
+    using System;
+    using System.Data;
+    using System.Globalization;
+    using Core.Movement.BulkReceiptRecovery;
+    using Prsd.Core.Mapper;
+
+    public class ActualDateOfShipmentDataRowMap : IMap<ReceiptRecoveryDataRow, DateTime?>
+    {
+        private readonly string[] formats = { "dd/MM/yyyy", "d/MM/yyyy", "d/M/yyyy", "dd/M/yyyy" };
+
+        public DateTime? Map(ReceiptRecoveryDataRow source)
+        {
+            DateTime? result = null;
+
+            try
+            {
+                int index = source.IsReceivedDate ? (int)ReceiptRecoveryColumnIndex.Received : (int)ReceiptRecoveryColumnIndex.RecoveredDisposed;
+                var data = source.DataRow.ItemArray[index].ToString();
+                data = data.Trim().Split(' ')[0];
+                DateTime parsed;
+
+                foreach (var format in formats)
+                {
+                    if (DateTime.TryParseExact(data, format, CultureInfo.InvariantCulture, DateTimeStyles.None,
+                        out parsed))
+                    {
+                        result = parsed;
+                        break;
+                    }
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return result;
+        }
+    }
+}
