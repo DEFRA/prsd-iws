af4b934a2fa92654ddf1df9793264b0d71cd2653
Will Andrews <will7989@hotmail.com>
2019-02-08 16:11:40 +0000

    Feature/receipt recovery content validation will (#729)
    * IReceiptRecoveryContentRule interface is now registered as an assembly type
    
    * Started on rules
    
    * Addded final content rules
    
    * Fixed PR queries

---
+++ src/EA.Iws.RequestHandlers.Tests.Unit/NotificationMovements/BulkReceiptRecovery/ReceiptRecoveryRecoverOnlyRuleTests.cs
@@ -0,0 +1,94 @@
+﻿namespace EA.Iws.RequestHandlers.Tests.Unit.NotificationMovements.BulkReceiptRecovery
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Core.Movement.BulkReceiptRecovery;
+    using Core.Rules;
+    using Domain;
+    using Domain.Movement;
+    using FakeItEasy;
+    using RequestHandlers.NotificationMovements.BulkReceiptRecovery;
+    using TestHelpers.DomainFakes;
+    using Xunit;
+
+    public class ReceiptRecoveryRecoverOnlyRuleTests
+    {
+        private readonly ReceiptRecoveryRecoveryOnlyRule rule;
+        private readonly Guid notificationId;
+        private readonly IMovementRepository repo;
+
+        public ReceiptRecoveryRecoverOnlyRuleTests()
+        {
+            repo = A.Fake<IMovementRepository>();
+            rule = new ReceiptRecoveryRecoveryOnlyRule(repo);
+            notificationId = Guid.NewGuid();
+        }
+
+        [Fact]
+        public async Task RecoverOnly_Received_Success()
+        {
+            A.CallTo(() => repo.GetAllMovements(notificationId)).Returns(GetRepoMovements(true));
+            var result = await rule.GetResult(GetTestData(false), notificationId);
+
+            Assert.Equal(ReceiptRecoveryContentRules.RecoveredValidation, result.Rule);
+            Assert.Equal(MessageLevel.Success, result.MessageLevel);
+        }
+
+        [Fact]
+        public async Task RecoverOnly_NotReceived_Failure()
+        {
+            A.CallTo(() => repo.GetAllMovements(notificationId)).Returns(GetRepoMovements(false));
+            var result = await rule.GetResult(GetTestData(true), notificationId);
+
+            Assert.Equal(ReceiptRecoveryContentRules.RecoveredValidation, result.Rule);
+            Assert.Equal(MessageLevel.Error, result.MessageLevel);
+        }
+
+        private List<ReceiptRecoveryMovement> GetTestData(bool received)
+        {
+            DateTime? date = null;
+
+            if (received)
+            {
+                date = DateTime.Now;
+            }
+
+            return new List<ReceiptRecoveryMovement>()
+            {
+                new ReceiptRecoveryMovement()
+                {
+                    ShipmentNumber = 1,
+                    MissingReceivedDate = received,
+                    RecoveredDisposedDate = date
+                },
+                new ReceiptRecoveryMovement()
+                {
+                    ShipmentNumber = 2,
+                    MissingReceivedDate = received,
+                    RecoveredDisposedDate = date
+                }
+            };
+        }
+
+        private List<Movement> GetRepoMovements(bool received)
+        {
+            return new List<Movement>()
+            {
+             new TestableMovement()
+                {
+                    NotificationId = notificationId,
+                    Status = received ? Core.Movement.MovementStatus.Received : Core.Movement.MovementStatus.Submitted,
+                    Number = 1
+                },
+
+            new TestableMovement()
+                {
+                    NotificationId = notificationId,
+                    Status = Core.Movement.MovementStatus.Submitted,
+                    Number = 2
+                }
+            };
+        }
+    }
+}
