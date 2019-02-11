70d7ec1b97bfebd2e2086847a53f95bbb2ca10ec
MichaelCoull1 <45666515+MichaelCoull1@users.noreply.github.com>
2019-02-11 12:02:18 +0000

    Removed the MissingShipmentNumbers and MissingNotificationNumber rule… (#737)
    * Removed the MissingShipmentNumbers and MissingNotificationNumber rule, replacing it with the InvalidNotificationOrShipmentNumbers rule. Invalid notification number format is now picked up.
    
    * Updated the tests.

--- src/EA.Iws.Core/Movement/BulkReceiptRecovery/ReceiptRecoveryContentRules.cs
+++ src/EA.Iws.Core/Movement/BulkReceiptRecovery/ReceiptRecoveryContentRules.cs
@@ -6,10 +6,8 @@
     {
         [Display(Name = "The maximum number of shipments that can be uploaded is {0}")]
         MaximumShipments,
-        [Display(Name = "You can't create {0} shipment(s) as they have their shipment numbers missing.")]
-        MissingShipmentNumbers,
-        [Display(Name = "Shipment number {0}: the notification number is missing.")]
-        MissingNotificationNumber,
+        [Display(Name = "All rows of data must contain valid notification and/or shipment numbers.")]
+        InvalidNotificationOrShipmentNumbers,
         [Display(Name = "Shipment number {0}: all three parts of the receipt data need to present.")]
         MissingReceiptData,
         [Display(Name = "Shipment number {0}: is duplicated within the data file.")]
