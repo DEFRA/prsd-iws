652356f8a60b7e3eb9a1e4245b2a71facf1f8165
MichaelCoull1 <45666515+MichaelCoull1@users.noreply.github.com>
2019-02-12 16:50:42 +0000

    Extended the quantity precision rule to cater for displaying decimal place specific errors. (#756)

---
+++ src/EA.Iws.RequestHandlers/NotificationMovements/BulkPrenotification/PrenotificationQuantityPrecisionForCubicMetresAndTonnesRule.cs
@@ -0,0 +1,47 @@
+﻿namespace EA.Iws.RequestHandlers.NotificationMovements.BulkPrenotification
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Core.Movement.BulkPrenotification;
+    using Core.Rules;
+    using Core.Shared;
+
+    public class PrenotificationQuantityPrecisionForCubicMetresAndTonnesRule : IPrenotificationContentRule
+    {
+        public async Task<PrenotificationContentRuleResult<PrenotificationContentRules>> GetResult(
+            List<PrenotificationMovement> movements, Guid notificationId)
+        {
+            return await Task.Run(() =>
+            {
+                // This rule only works while the precision for CubicMetres and Tonnes is the same
+                var precision = ShipmentQuantityUnitsMetadata.Precision[ShipmentQuantityUnits.CubicMetres];
+
+                var shipments =
+                    movements.Where(
+                            m =>
+                                m.ShipmentNumber.HasValue &&
+                                m.Quantity.HasValue && m.Unit.HasValue &&
+                                (m.Unit.Value == ShipmentQuantityUnits.CubicMetres ||
+                                m.Unit.Value == ShipmentQuantityUnits.Tonnes) &&
+                                decimal.Round(m.Quantity.Value, ShipmentQuantityUnitsMetadata.Precision[m.Unit.Value]) !=
+                                m.Quantity.Value)
+                        .GroupBy(x => x.ShipmentNumber)
+                        .Select(x => x.Key)
+                        .ToList();
+
+                var result = shipments.Any() ? MessageLevel.Error : MessageLevel.Success;
+
+                var shipmentNumbers = string.Join(", ", shipments);
+                var errorMessage =
+                    string.Format(
+                        Prsd.Core.Helpers.EnumHelper.GetDisplayName(PrenotificationContentRules.QuantityPrecision),
+                        shipmentNumbers, precision);
+
+                return new PrenotificationContentRuleResult<PrenotificationContentRules>(PrenotificationContentRules.QuantityPrecision,
+                    result, errorMessage);
+            });
+        }
+    }
+}
