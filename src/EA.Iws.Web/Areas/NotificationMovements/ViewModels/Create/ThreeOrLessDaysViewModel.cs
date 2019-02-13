1b3911446623beaed268197f8ad474a329af9cbb
SumeN <SumeN@users.noreply.github.com>
2019-02-13 15:20:48 +0000

    Feature/consent expiry screens (#764)
    * Add consent expiry screen redirection
    
    * Add Antiforgerytoken

---
+++ src/EA.Iws.Web/Areas/NotificationMovements/ViewModels/Create/ThreeOrLessDaysViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace EA.Iws.Web.Areas.NotificationMovements.ViewModels.Create
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    public class ThreeOrLessDaysViewModel : IValidatableObject
+    {
+        public ThreeOrLessWorkingDaysSelection Selection { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!(Selection == ThreeOrLessWorkingDaysSelection.ContinueAnyway || Selection == ThreeOrLessWorkingDaysSelection.AbandonCreation))
+            {
+                yield return new ValidationResult("Select one of the options", new[] { "Selection" });
+            }
+        }
+    }
+}
