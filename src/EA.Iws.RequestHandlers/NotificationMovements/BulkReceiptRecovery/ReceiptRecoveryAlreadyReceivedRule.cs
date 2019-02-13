44b2b293f405840a4e27db7d5f699aa85a42d93a
Will Andrews <will7989@hotmail.com>
2019-02-13 09:31:01 +0000

    Hotfix/receipt bulk upload (#758)
    * Fixed bug 67177 where additional text wasnt being displayed
    
    * Undid commented out code
    
    * Fixed bug 67154 where if only trying to add recover of disposed date, then the quanity and units arent needed
    
    * Fixed bug where receipt and recovered/disposed dates were throwing up format error if not entered by user
    
    * Changed wording per bug 67143
    
    * Fixed bug
    
    * removed double check
    
    * fixed typo

---
+++ src/EA.Iws.RequestHandlers/NotificationMovements/BulkReceiptRecovery/ReceiptRecoveryAlreadyReceivedRule.cs
@@ -0,0 +1,48 @@
+﻿namespace EA.Iws.RequestHandlers.NotificationMovements.BulkReceiptRecovery
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Core.Movement;
+    using Core.Movement.BulkReceiptRecovery;
+    using Core.Rules;
+    using Core.Shared;
+    using Domain.Movement;
+    using Domain.NotificationApplication;
+
+    public class ReceiptRecoveryAlreadyReceivedRule : IReceiptRecoveryContentRule
+    {
+        private readonly IMovementRepository movementRepo;
+
+        public ReceiptRecoveryAlreadyReceivedRule(IMovementRepository movementRepo)
+        {
+            this.movementRepo = movementRepo;
+        }
+
+        public async Task<ReceiptRecoveryContentRuleResult<ReceiptRecoveryContentRules>> GetResult(List<ReceiptRecoveryMovement> movements, Guid notificationId)
+        {
+            var actualMovements = await movementRepo.GetAllMovements(notificationId);
+
+            List<int> shipments = new List<int>();
+            MessageLevel result = MessageLevel.Success;
+
+            foreach (var movement in movements.Where(p => p.ReceivedDate.HasValue))
+            {
+                var actualMovement = actualMovements.FirstOrDefault(p => p.Number == movement.ShipmentNumber);
+
+                if (actualMovement != null && (actualMovement.Status == MovementStatus.Received || actualMovement.Status == MovementStatus.Completed))
+                {
+                    result = MessageLevel.Error;
+                    shipments.Add(movement.ShipmentNumber.GetValueOrDefault());
+                }
+            }
+
+            var shipmentNumbers = string.Join(", ", shipments);
+
+            var errorMessage = string.Format(Prsd.Core.Helpers.EnumHelper.GetDisplayName(ReceiptRecoveryContentRules.AlreadyRecievedRecoveredDisposed), shipmentNumbers, "received");
+
+            return new ReceiptRecoveryContentRuleResult<ReceiptRecoveryContentRules>(ReceiptRecoveryContentRules.AlreadyRecievedRecoveredDisposed, result, errorMessage);
+        }
+    }
+}
