e2cb708c2dfb60ebd7a9021a813616b545529199
Will Andrews <will7989@hotmail.com>
2019-02-13 09:31:01 +0000

    Hotfix/receipt bulk upload (#758)
    * Fixed bug 67177 where additional text wasnt being displayed
    
    * Undid commented out code
    
    * Fixed bug 67154 where if only trying to add recover of disposed date, then the quanity and units arent needed
    
    * Fixed bug where receipt and recovered/disposed dates were throwing up format error if not entered by user
    
    * Changed wording per bug 67143
    
    * Fixed bug
    
    * removed double check
    
    * fixed typo

---
+++ src/EA.Iws.RequestHandlers.Tests.Unit/NotificationMovements/BulkReceiptRecovery/ReceiptRecoveryAlreadyRecievedRuleTests.cs
@@ -0,0 +1,88 @@
+﻿namespace EA.Iws.RequestHandlers.Tests.Unit.NotificationMovements.BulkReceiptRecovery
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Core.Movement.BulkReceiptRecovery;
+    using Core.Rules;
+    using Domain.Movement;
+    using FakeItEasy;
+    using RequestHandlers.NotificationMovements.BulkReceiptRecovery;
+    using TestHelpers.DomainFakes;
+    using Xunit;
+
+    public class ReceiptRecoveryAlreadyRecievedRuleTests
+    {
+        private readonly ReceiptRecoveryAlreadyReceivedRule rule;
+        private readonly Guid notificationId;
+
+        private readonly IMovementRepository movementRepo;
+
+        public ReceiptRecoveryAlreadyRecievedRuleTests()
+        {
+            this.movementRepo = A.Fake<IMovementRepository>();
+
+            rule = new ReceiptRecoveryAlreadyReceivedRule(this.movementRepo);
+            notificationId = Guid.NewGuid();
+        }
+
+        [Fact]
+        public async Task NotAlreadyRecieved_Success()
+        {
+            A.CallTo(() => movementRepo.GetAllMovements(notificationId)).Returns(GetRepoMovements(false));
+            var result = await rule.GetResult(GetTestData(), notificationId);
+
+            Assert.Equal(ReceiptRecoveryContentRules.AlreadyRecievedRecoveredDisposed, result.Rule);
+            Assert.Equal(MessageLevel.Success, result.MessageLevel);
+        }
+
+        [Fact]
+        public async Task AlreadyRecieved_Fail()
+        {
+            A.CallTo(() => movementRepo.GetAllMovements(notificationId)).Returns(GetRepoMovements(true));
+            var result = await rule.GetResult(GetTestData(), notificationId);
+
+            Assert.Equal(ReceiptRecoveryContentRules.AlreadyRecievedRecoveredDisposed, result.Rule);
+            Assert.Equal(MessageLevel.Error, result.MessageLevel);
+        }
+
+        private List<ReceiptRecoveryMovement> GetTestData()
+        {
+            return new List<ReceiptRecoveryMovement>()
+            {
+                new ReceiptRecoveryMovement()
+                {
+                    ShipmentNumber = 1,
+                    ReceivedDate = DateTime.Now
+                },
+                new ReceiptRecoveryMovement()
+                {
+                    ShipmentNumber = 2,
+                    ReceivedDate = DateTime.Now
+                }
+            };
+        }
+
+        private List<Movement> GetRepoMovements(bool received)
+        {
+            return new List<Movement>()
+            {
+             new TestableMovement()
+                {
+                    NotificationId = notificationId,
+                    Status = received ? Core.Movement.MovementStatus.Received : Core.Movement.MovementStatus.Submitted,
+                    Number = 1,
+                    Date = DateTime.Now
+                },
+
+            new TestableMovement()
+                {
+                    NotificationId = notificationId,
+                    Status = Core.Movement.MovementStatus.Submitted,
+                    Number = 2,
+                    Date = DateTime.Now
+                }
+            };
+        }
+    }
+}
