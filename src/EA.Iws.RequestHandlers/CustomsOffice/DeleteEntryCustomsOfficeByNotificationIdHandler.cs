db7c5e5aeef4af8f97d04cb1786dac41f23855d2
Michael Coull <Michael.Coull@ipl.com>
2019-02-14 16:56:42 +0000

    Added the request and handler for calling the deletion of the entry customs office

---
+++ src/EA.Iws.RequestHandlers/CustomsOffice/DeleteEntryCustomsOfficeByNotificationIdHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace EA.Iws.RequestHandlers.CustomsOffice
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Core.CustomsOffice;
+    using DataAccess;
+    using Domain.ImportNotification;
+    using Prsd.Core.Mapper;
+    using Prsd.Core.Mediator;
+    using Requests.CustomsOffice;
+
+    internal class DeleteEntryCustomsOfficeByNotificationIdHandler :
+        IRequestHandler<DeleteEntryCustomsOfficeByNotificationId, bool>
+    {
+        private readonly ITransportRouteRepository repository;
+
+        public DeleteEntryCustomsOfficeByNotificationIdHandler(ITransportRouteRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<bool> HandleAsync(DeleteEntryCustomsOfficeByNotificationId message)
+        {
+            await repository.DeleteEntryCustomsOfficeByNotificationId(message.NotificationId);
+
+            return true;
+        }
+    }
+}
