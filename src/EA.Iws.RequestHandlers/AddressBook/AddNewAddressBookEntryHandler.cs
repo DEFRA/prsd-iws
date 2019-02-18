882149db7bdd7f50f9180266c5043da983ff80e0
SumeN <SumeN@users.noreply.github.com>
2019-02-18 17:22:53 +0000

    Feature/address book (#795)
    * Added in search functionality to address book
    
    * Change the text for required links
    
    * Added pagination to address book
    
    * Changed home link on address book page
    
    * Added the extra data to the text against the drop down list
    
    * Feature/edit address book (#794)
    
    * Add Edit functionality
    
    * Add address functionlaity

---
+++ src/EA.Iws.RequestHandlers/AddressBook/AddNewAddressBookEntryHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace EA.Iws.RequestHandlers.AddressBook
+{
+    using System.Data.Entity;
+    using System.Threading.Tasks;
+    using DataAccess;
+    using Domain.AddressBook;
+    using Mappings;
+    using Prsd.Core.Domain;
+    using Prsd.Core.Mediator;
+    using Requests.AddressBook;
+    internal class AddNewAddressBookEntryHandler : IRequestHandler<AddNewAddressBookEntry, bool>
+    {
+        private readonly IwsContext context;
+        private readonly IUserContext userContext;
+        private readonly IAddressBookRepository addressBookRepository;
+
+        public AddNewAddressBookEntryHandler(IwsContext context, IUserContext userContext, IAddressBookRepository addressBookRepository)
+        {
+            this.context = context;
+            this.userContext = userContext;
+            this.addressBookRepository = addressBookRepository;
+        }
+
+        public async Task<bool> HandleAsync(AddNewAddressBookEntry message)
+        {
+            var addressBook = await addressBookRepository.GetAddressBookForUser(userContext.UserId, message.Type);
+            var country = await context.Countries.SingleAsync(c => c.Id == message.Address.CountryId);
+            var address = ValueObjectInitializer.CreateAddress(message.Address, country.Name); 
+            var business = ValueObjectInitializer.CreateBusiness(message.Business);
+            var contact = ValueObjectInitializer.CreateContact(message.Contact);
+
+            addressBook.Add(new AddressBookRecord(address, business, contact));
+
+            await addressBookRepository.Update(addressBook);
+
+            return true;
+        }
+    }
+}
---
+++ src/EA.Iws.Web/Areas/AddressBook/ViewModels/EditAddressViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace EA.Iws.Web.Areas.AddressBook.ViewModels
+{
+    using System;
+    using Core.AddressBook;
+    using Core.Shared;
+    using Requests.AddressBook;
+
+    public class EditAddressViewModel : AddAddressViewModel
+    {
+        public Guid AddressBookRecordId { get; set; }
+
+        public EditAddressViewModel()
+        {
+            Address = new AddressData();
+
+            Contact = new ContactData();
+
+            Business = new AddressBusinessTypeViewModel();
+        }
+
+        public EditAddressViewModel(AddressBookRecordData addressbook, AddressRecordType type)
+        {
+            AddressBookRecordId = addressbook.Id;
+            Address = addressbook.AddressData;
+            Contact = addressbook.ContactData;
+            Business = new AddressBusinessTypeViewModel(addressbook.BusinessData);
+            Type = type;
+        }     
+    }
+}
