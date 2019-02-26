04e419fb594938719514177e2d5026a3b860680e
Will Andrews <will7989@hotmail.com>
2019-02-26 14:20:28 +0000

    Feature/internal shipments notification comments content (#838)
    * Added link
    
    * Added comments screen
    
    * Added auth attributes
    
    * Added auth attributes
    
    * Added a new auth for internal comments

---
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/CommentsController.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Iws.Web.Areas.AdminExportAssessment.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+    using Core.Authorization.Permissions;
+    using Infrastructure.Authorization;
+    using ViewModels.Comments;
+
+    [AuthorizeActivity(ExportNotificationPermissions.CanEditInternalComments)]
+    public class CommentsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(Guid id)
+        {
+            CommentsViewModel model = new CommentsViewModel();
+            model.NotificationId = id;
+            return View(model);
+        }
+    }
+}
