8bb2caec0b134ac666ec60857659cc3f8d9140cb
Gonzalo Girasol <35956285+GonzaloGirasol@users.noreply.github.com>
2019-03-04 14:15:59 +0000

    Producer report generation (#876)

---
+++ src/EA.Iws.Requests/Admin/Reports/GetProducerReport.cs
@@ -0,0 +1,41 @@
+﻿namespace EA.Iws.Requests.Admin.Reports
+{
+    using System;
+    using Core.Admin.Reports;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
+    using Core.Reports;
+    using Core.Reports.Producer;
+    using Prsd.Core.Mediator;
+
+    [RequestAuthorization(ReportingPermissions.CanViewProducerReport)]
+    public class GetProducerReport : IRequest<ProducerData[]>
+    {
+        public ProducerReportDates DateType { get; private set; }
+
+        public DateTime From { get; private set; }
+
+        public DateTime To { get; private set; }
+
+        public ProducerReportTextFields? TextFieldType { get; private set; }
+
+        public TextFieldOperator? OperatorType { get; private set; }
+
+        public string TextSearch { get; private set; }
+
+        public GetProducerReport(ProducerReportDates dateType,
+            DateTime from,
+            DateTime to,
+            ProducerReportTextFields? textFieldType,
+            TextFieldOperator? operatorType,
+            string textSearch)
+        {
+            DateType = dateType;
+            From = from;
+            To = to;
+            TextFieldType = textFieldType;
+            OperatorType = operatorType;
+            TextSearch = textSearch;
+        }
+    }
+}
--- src/EA.Iws.Web/Areas/Reports/ViewModels/Producer/IndexViewModel.cs
+++ src/EA.Iws.Web/Areas/Reports/ViewModels/Producer/IndexViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace EA.Iws.Web.Areas.Reports.ViewModels.Producer
 {
+    using System;
     using Core.Reports;
     using Core.Reports.Producer;
     using Web.ViewModels.Shared;
@@ -15,12 +16,27 @@
 
         public ProducerReportTextFields? TextFieldType
         {
-            get { return InputParameters.TryParse<ProducerReportTextFields>(InputParameters.SelectedDate); }
+            get { return InputParameters.TryParse<ProducerReportTextFields>(InputParameters.SelectedTextField); }
         }
 
         public TextFieldOperator? OperatorType
         {
-            get { return InputParameters.TryParse<TextFieldOperator>(InputParameters.SelectedDate); }
+            get { return InputParameters.TryParse<TextFieldOperator>(InputParameters.SelectedOperator); }
+        }
+
+        public DateTime From
+        {
+            get { return InputParameters.FromDate.AsDateTime().Value; }
+        }
+
+        public DateTime To
+        {
+            get { return InputParameters.ToDate.AsDateTime().Value; }
+        }
+
+        public string TextSearch
+        {
+            get { return InputParameters.TextSearch; }
         }
 
         public IndexViewModel()
