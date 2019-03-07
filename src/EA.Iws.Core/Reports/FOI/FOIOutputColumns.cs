99c8c54352b8b04ad50023c656d60be1a5990464
SumeN <sumen@sfwltd.co.uk>
2019-03-07 15:53:04 +0000

    Revert the column name

--- src/EA.Iws.Core/Reports/FOI/FOIOutputColumns.cs
+++ src/EA.Iws.Core/Reports/FOI/FOIOutputColumns.cs
@@ -124,7 +124,7 @@
         [Display(Name = "Decision date")]
         DecisionRequiredByDate,
 
-        [Display(Name = "Financial guarantee approved (Y/N)")]
+        [Display(Name = "Financial guarantee approved")]
         IsFinancialGuaranteeApproved,
 
         [Display(Name = "File closure date")]
