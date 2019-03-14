49bfb383829024afeae7a0211e6f87e499fff4c7
Will Andrews <will7989@hotmail.com>
2019-03-14 11:27:02 +0000

    Added in extra inputs to the report (#947)
    * Added in extra inputs to the report
    
    * Changed the way the select list is populated
    
    * moved using to within the namespace
    
    * Changed blank option to view all

--- src/EA.Iws.Web/Areas/Reports/ViewModels/ExportMovements/IndexViewModel.cs
+++ src/EA.Iws.Web/Areas/Reports/ViewModels/ExportMovements/IndexViewModel.cs
@@ -2,6 +2,10 @@
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web.Mvc;
+    using EA.Iws.Core.Reports;
+    using EA.Prsd.Core.Helpers;
     using Infrastructure.Validation;
     using Web.ViewModels.Shared;
 
@@ -15,10 +19,34 @@
         [RequiredDateInput(ErrorMessageResourceName = "ToRequired", ErrorMessageResourceType = typeof(IndexViewModelResources))]
         public OptionalDateInputViewModel To { get; set; }
 
+        public SelectList OrganisationTypesSelectList { get; set; }
+
+        public string SelectedOrganistationFilter { get; set; }
+
+        private string organisationName;
+        public string OrganisationName
+        {
+            get
+            {
+                if (this.SelectedOrganistationFilter == null || this.SelectedOrganistationFilter == string.Empty)
+                {
+                    return string.Empty;
+                }
+
+                return organisationName;
+            }
+            set { organisationName = value; }
+        }
+
         public IndexViewModel()
         {
             From = new OptionalDateInputViewModel(true);
             To = new OptionalDateInputViewModel(true);
+
+            var organisationOptions = EnumHelper.GetValues(typeof(OrganisationFilterOptions));
+            organisationOptions.Add(-1, "View all");
+            var orderedOrganisationOptions = organisationOptions.OrderBy(p => p.Key);
+            OrganisationTypesSelectList = new SelectList(orderedOrganisationOptions, "Key", "Value", null);
         }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
