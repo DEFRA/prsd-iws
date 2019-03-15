1673fd5e58add0e90dddd6441c099a6eb64069cb
Will Andrews <will7989@hotmail.com>
2019-03-15 13:57:33 +0000

    Started creating the screen for editing operation codes

--- src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/OperationCodesController.cs
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/OperationCodesController.cs
@@ -5,11 +5,15 @@
     using System.Linq;
     using System.Web;
     using System.Web.Mvc;
+    using EA.Iws.Web.Areas.AdminExportAssessment.ViewModels.OperationCodes;
+
     public class OperationCodesController : Controller
     {
         public ActionResult Edit()
         {
-            return View();
+            OperationCodesViewModel model = new OperationCodesViewModel();
+
+            return View(model);
         }
     }
 }
---
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/ViewModels/OperationCodes/OperationCodesViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace EA.Iws.Web.Areas.AdminExportAssessment.ViewModels.OperationCodes
+{
+    using EA.Iws.Core.Shared;
+    using Web.Areas.AdminExportAssessment.Views.OperationCodes;
+
+    public class OperationCodesViewModel
+    {
+        public string Title
+        {
+            get
+            {
+                return EditResources.Title.Replace("{type}", this.NotificationType.ToString());
+            }
+        }
+        public string Header
+        {
+            get
+            {
+                return EditResources.Header.Replace("{type}", this.NotificationType.ToString());
+            }
+        }
+
+        public NotificationType NotificationType { get; set; }
+
+        public OperationCodesViewModel()
+        {
+            this.NotificationType = NotificationType.Recovery;
+        }
+    }
+}
