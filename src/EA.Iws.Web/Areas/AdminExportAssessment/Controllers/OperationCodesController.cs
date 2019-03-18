46d1cab2ba8f2b7abd188cbab54ad9054243bb7e
Will Andrews <will7989@hotmail.com>
2019-03-18 09:58:18 +0000

    Existing codes presnted to the user when the navigate to the edit page

--- src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/OperationCodesController.cs
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/OperationCodesController.cs
@@ -3,17 +3,58 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Threading.Tasks;
     using System.Web;
     using System.Web.Mvc;
+    using EA.Iws.Requests.Notification;
+    using EA.Iws.Requests.OperationCodes;
     using EA.Iws.Web.Areas.AdminExportAssessment.ViewModels.OperationCodes;
+    using EA.Iws.Web.Infrastructure;
+    using EA.Prsd.Core.Mediator;
 
     public class OperationCodesController : Controller
     {
-        public ActionResult Edit()
+        private readonly IMediator mediator;
+        private readonly IAuditService auditService;
+
+        public OperationCodesController(IMediator mediator, IAuditService auditService)
         {
-            OperationCodesViewModel model = new OperationCodesViewModel();
+            this.mediator = mediator;
+            this.auditService = auditService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(Guid id)
+        {
+            var basicInfo =
+                await mediator.SendAsync(new GetNotificationBasicInfo(id));
+
+            var selectedCodes =
+                    await mediator.SendAsync(new GetOperationCodesByNotificationId(id));
+
+            OperationCodesViewModel model = new OperationCodesViewModel(basicInfo.NotificationType, selectedCodes);
 
             return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Guid id, OperationCodesViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return await Edit(id);
+            }
+
+            //await mediator.SendAsync(new SetEntryPoint(id, model.SelectedEntryPoint.Value));
+
+            /*await this.auditService.AddAuditEntry(this.mediator,
+                    id,
+                    User.GetUserId(),
+                    NotificationAuditType.Updated,
+                    NotificationAuditScreenType.ImportRoute);*/
+
+            return RedirectToAction("Index", "Overview");
         }
     }
 }
--- src/EA.Iws.Web/Areas/AdminExportAssessment/ViewModels/OperationCodes/OperationCodesViewModel.cs
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/ViewModels/OperationCodes/OperationCodesViewModel.cs
@@ -1,6 +1,12 @@
 ﻿namespace EA.Iws.Web.Areas.AdminExportAssessment.ViewModels.OperationCodes
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using EA.Iws.Core.Notification.Overview;
+    using EA.Iws.Core.OperationCodes;
     using EA.Iws.Core.Shared;
+    using EA.Iws.Web.ViewModels.Shared;
+    using EA.Prsd.Core.Helpers;
     using Web.Areas.AdminExportAssessment.Views.OperationCodes;
 
     public class OperationCodesViewModel
@@ -22,9 +28,40 @@
 
         public NotificationType NotificationType { get; set; }
 
+        public IList<KeyValuePairViewModel<OperationCode, bool>> PossibleCodes { get; set; }
+
+        public IList<string> DisplayValues
+        {
+            get
+            {
+                return PossibleCodes.Select(c => string.Format("{0} - {1}",
+                        EnumHelper.GetDisplayName(c.Key), EnumHelper.GetDescription(c.Key))).ToList();
+            }
+        }
+
+        public IList<OperationCode> SelectedValues
+        {
+            get
+            {
+                if (PossibleCodes == null)
+                {
+                    return new List<OperationCode>(0);
+                }
+
+                return PossibleCodes.Where(c => c.Value).Select(c => c.Key).ToList();
+            }
+        }
+
         public OperationCodesViewModel()
         {
-            this.NotificationType = NotificationType.Recovery;
+        }
+
+        public OperationCodesViewModel(NotificationType type, IEnumerable<OperationCode> selectedCodes)
+        {
+            this.NotificationType = type;
+            PossibleCodes = OperationCodeMetadata.GetCodesForOperation(type)
+                .Select(c => new KeyValuePairViewModel<OperationCode, bool>(c, selectedCodes.Contains(c)))
+                .ToList();
         }
     }
 }
