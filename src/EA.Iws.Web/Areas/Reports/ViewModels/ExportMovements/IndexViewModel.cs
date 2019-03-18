7a1a954b2ad14a0e91e3f0e02786ff42125298e3
Will Andrews <will7989@hotmail.com>
2019-03-18 08:35:21 +0000

    Fixed issue where model validation not happening for org type unless date was entered correctly

--- src/EA.Iws.Web/Areas/Reports/ViewModels/ExportMovements/IndexViewModel.cs
+++ src/EA.Iws.Web/Areas/Reports/ViewModels/ExportMovements/IndexViewModel.cs
@@ -1,10 +1,12 @@
 ﻿namespace EA.Iws.Web.Areas.Reports.ViewModels.ExportMovements
 {
+    using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Web.Mvc;
     using EA.Iws.Core.Reports;
+    using EA.Prsd.Core;
     using EA.Prsd.Core.Helpers;
     using Infrastructure.Validation;
     using Web.Areas.Reports.Views.ExportMovements;
@@ -13,11 +15,11 @@
     public class IndexViewModel : IValidatableObject
     {
         [Display(Name = "From", ResourceType = typeof(IndexViewModelResources))]
-        [RequiredDateInput(ErrorMessageResourceName = "FromRequired", ErrorMessageResourceType = typeof(IndexViewModelResources))]
+        //[RequiredDateInput(ErrorMessageResourceName = "FromRequired", ErrorMessageResourceType = typeof(IndexViewModelResources))]
         public OptionalDateInputViewModel From { get; set; }
 
         [Display(Name = "To", ResourceType = typeof(IndexViewModelResources))]
-        [RequiredDateInput(ErrorMessageResourceName = "ToRequired", ErrorMessageResourceType = typeof(IndexViewModelResources))]
+        //[RequiredDateInput(ErrorMessageResourceName = "ToRequired", ErrorMessageResourceType = typeof(IndexViewModelResources))]
         public OptionalDateInputViewModel To { get; set; }
 
         public SelectList OrganisationTypesSelectList { get; set; }
@@ -52,14 +54,34 @@
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            if (From.AsDateTime() > To.AsDateTime())
-            {
-                yield return new ValidationResult(IndexViewModelResources.FromDateBeforeToDate, new[] { "FromDate" });
-            }
-
             if ((SelectedOrganistationFilter != null && SelectedOrganistationFilter != "-1") && (OrganisationName == null || OrganisationName == string.Empty))
             {
                 yield return new ValidationResult(IndexResources.OrganisationNameRequiredError, new[] { "OrganisationName" });
+            }
+
+            DateTime startDate;
+            bool isValidstartDate = SystemTime.TryParse(this.From.Year.GetValueOrDefault(), this.From.Month.GetValueOrDefault(), this.From.Day.GetValueOrDefault(), out startDate);
+            if (!isValidstartDate)
+            {
+                yield return new ValidationResult("Please enter a valid from date", new[] { "From" });
+            }
+
+            DateTime endDate;
+            bool isValidEndDate = SystemTime.TryParse(this.To.Year.GetValueOrDefault(), this.To.Month.GetValueOrDefault(), this.To.Day.GetValueOrDefault(), out endDate);
+            if (!isValidEndDate)
+            {
+                yield return new ValidationResult("Please enter a valid to date", new[] { "To" });
+            }
+
+            if (!isValidEndDate || !isValidstartDate)
+            {
+                // Stop further validation if either date is not a valid date
+                yield break;
+            }
+
+            if (From.AsDateTime() > To.AsDateTime())
+            {
+                yield return new ValidationResult(IndexViewModelResources.FromDateBeforeToDate, new[] { "From" });
             }
         }
     }
