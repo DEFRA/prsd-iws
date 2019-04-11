5d7b9f5366bcb9b144ebb1b52be918008f9c94e3
Gonzalo Girasol <gonzalog@sfwltd.co.uk>
2019-04-11 16:16:21 +0100

    Add scripts for creating base tables.

---
+++ src/EA.Iws.Database/scripts/Update/0006-CI-2019/Sprint1/20190411-154500-add-shipment-audit-table.sql
@@ -0,0 +1,35 @@
+CREATE TABLE [Notification].[MovementAudit](
+	[Id] [uniqueidentifier] NOT NULL,
+	[NotificationId] [uniqueidentifier] NOT NULL,
+	[ShipmentNumber] [int] NOT NULL,
+	[UserId] [nvarchar](128) NOT NULL,
+	[Type] [int] NOT NULL,
+	[DateAdded] [datetimeoffset](0) NOT NULL,
+	[RowVersion] [timestamp] NOT NULL,
+ CONSTRAINT [PK_MovementAudit] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [Notification].[MovementAudit]  WITH CHECK ADD  CONSTRAINT [FK_MovementAudit_Notification] FOREIGN KEY([NotificationId])
+REFERENCES [Notification].[Notification] ([Id])
+GO
+
+ALTER TABLE [Notification].[MovementAudit] CHECK CONSTRAINT [FK_MovementAudit_Notification]
+GO
+
+ALTER TABLE [Notification].[MovementAudit]  WITH CHECK ADD  CONSTRAINT [FK_MovementAudit_User] FOREIGN KEY([UserId])
+REFERENCES [Identity].[AspNetUsers] ([Id])
+GO
+
+ALTER TABLE [Notification].[MovementAudit] CHECK CONSTRAINT [FK_MovementAudit_User]
+GO
+
+ALTER TABLE [Notification].[MovementAudit]  WITH CHECK ADD  CONSTRAINT [FK_MovementAudit_Type] FOREIGN KEY([Type])
+REFERENCES [Lookup].[MovementAuditType] ([Id])
+GO
+
+ALTER TABLE [Notification].[MovementAudit] CHECK CONSTRAINT [FK_MovementAudit_Type]
+GO
