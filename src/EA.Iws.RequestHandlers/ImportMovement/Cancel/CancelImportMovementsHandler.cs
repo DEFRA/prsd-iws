4ec8f3bc734ef83979cb32614ff051e8188648c0
SumeN <sumen@sfwltd.co.uk>
2019-04-16 11:31:28 +0100

    Add internal audit for edit, cancel and delete

--- src/EA.Iws.RequestHandlers/ImportMovement/Cancel/CancelImportMovementsHandler.cs
+++ src/EA.Iws.RequestHandlers/ImportMovement/Cancel/CancelImportMovementsHandler.cs
@@ -2,7 +2,11 @@
 {
     using System.Linq;
     using System.Threading.Tasks;
+    using Core.Movement;
     using DataAccess;
+    using Domain.ImportMovement;
+    using Prsd.Core;
+    using Prsd.Core.Domain;
     using Prsd.Core.Mediator;
     using Requests.ImportMovement.Cancel;
 
@@ -10,11 +14,15 @@
     {
         private readonly Domain.ImportMovement.CancelImportMovement cancelMovement;
         private readonly ImportNotificationContext context;
+        private readonly IImportMovementAuditRepository repository;
+        private readonly IUserContext userContext;
 
-        public CancelImportMovementsHandler(Domain.ImportMovement.CancelImportMovement cancelMovement, ImportNotificationContext context)
+        public CancelImportMovementsHandler(Domain.ImportMovement.CancelImportMovement cancelMovement, ImportNotificationContext context, IImportMovementAuditRepository repository, IUserContext userContext)
         {
             this.cancelMovement = cancelMovement;
             this.context = context;
+            this.repository = repository;
+            this.userContext = userContext;
         }
 
         public async Task<bool> HandleAsync(CancelImportMovements message)
@@ -27,6 +35,11 @@
             }
 
             await context.SaveChangesAsync();
+
+            foreach (var movement in message.CancelledMovements)
+            {
+                await repository.Add(new ImportMovementAudit(message.NotificationId, movement.Number, userContext.UserId.ToString().ToUpper(), (int)MovementAuditType.Cancelled, SystemTime.UtcNow));
+            }
 
             return true;
         }
