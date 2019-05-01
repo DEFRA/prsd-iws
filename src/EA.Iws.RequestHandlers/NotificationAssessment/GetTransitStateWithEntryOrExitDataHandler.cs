3102e8d8950fcbb0701fcee240a81b2ffdbec420
Gonzalo Girasol <35956285+GonzaloGirasol@users.noreply.github.com>
2019-05-01 17:21:11 +0100

    Change transit states on export notifications (#1005)
    * Add new screen for changing transit state entry point.
    
    * Add screen for changing transit state exit point.
    
    * Add new request/handler for updating entry or exit point for transit states.
    
    * Update resource text.
    
    * Permissions for change link of transit state entry or exit points.

---
+++ src/EA.Iws.RequestHandlers/NotificationAssessment/GetTransitStateWithEntryOrExitDataHandler.cs
@@ -0,0 +1,57 @@
+﻿namespace EA.Iws.RequestHandlers.NotificationAssessment
+{
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Core.TransitState;
+    using Core.TransportRoute;
+    using DataAccess;
+    using Domain.TransportRoute;
+    using Prsd.Core.Mapper;
+    using Prsd.Core.Mediator;
+    using Requests.NotificationAssessment;
+
+    internal class GetTransitStateWithEntryOrExitDataHandler : IRequestHandler<GetTransitStateWithEntryOrExitData, TransitStateWithEntryOrExitPointsData>
+    {
+        private readonly IwsContext context;
+        private readonly IMapper mapper;
+        private readonly ITransportRouteRepository transportRouteRepository;
+
+        public GetTransitStateWithEntryOrExitDataHandler(IwsContext context,
+            IMapper mapper,
+            ITransportRouteRepository transportRouteRepository)
+        {
+            this.context = context;
+            this.mapper = mapper;
+            this.transportRouteRepository = transportRouteRepository;
+        }
+
+        public async Task<TransitStateWithEntryOrExitPointsData> HandleAsync(GetTransitStateWithEntryOrExitData message)
+        {
+            var transportRoute = await transportRouteRepository.GetByNotificationId(message.Id);
+            var result = new TransitStateWithEntryOrExitPointsData()
+            {
+                TransitState = new TransitStateData(),
+                EntryOrExitPoints = new List<EntryOrExitPointData>()
+            };
+
+            if (transportRoute == null)
+            {
+                return result;
+            }
+
+            var thisTransitState = transportRoute.TransitStates.SingleOrDefault(ts => ts.Id == message.TransitStateId);
+            if (thisTransitState != null)
+            {
+                result.TransitState = mapper.Map<TransitStateData>(thisTransitState);
+                result.EntryOrExitPoints =
+                    (await
+                            context.EntryOrExitPoints.Where(ep => ep.Country.Id == thisTransitState.Country.Id).ToArrayAsync())
+                        .Select(e => mapper.Map<EntryOrExitPointData>(e));
+            }
+
+            return result;
+        }
+    }
+}
