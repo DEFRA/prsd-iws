8d91e0f2cf0ddd014203211d8230b2427eb1d10c
Gonzalo Girasol <gonzalog@sfwltd.co.uk>
2019-05-14 16:59:34 +0100

    Remove newline character in the comments within the report payments view for import notifications.

--- src/EA.Iws.Database/scripts/Everytime/02-Views/0017-Reports-Payments.sql
+++ src/EA.Iws.Database/scripts/Everytime/02-Views/0017-Reports-Payments.sql
@@ -29,11 +29,11 @@ AS
         SUM(T.Debit) AS TotalRefunded,
         (SELECT TOP 1 [Date] FROM [ImportNotification].[Transaction] WHERE NotificationId = N.Id AND Credit IS NOT NULL ORDER BY [Date] DESC) AS [LatestPaymentDate],
         (SELECT TOP 1 [Date] FROM [ImportNotification].[Transaction] WHERE NotificationId = N.Id AND Debit IS NOT NULL ORDER BY [Date] DESC) AS [LatestRefundDate],
-        (SELECT CASE WHEN RIGHT(LTRIM(RTRIM(T2.Comments)),1) = '.' THEN LTRIM(RTRIM(T2.Comments)) + ' ' ELSE LTRIM(RTRIM(T2.Comments)) + '. ' END AS Comments
+        REPLACE(REPLACE((SELECT CASE WHEN RIGHT(LTRIM(RTRIM(T2.Comments)),1) = '.' THEN LTRIM(RTRIM(T2.Comments)) + ' ' ELSE LTRIM(RTRIM(T2.Comments)) + '. ' END AS Comments
           FROM [ImportNotification].[Transaction] T2
           WHERE T2.NotificationId = N.Id
 		  ORDER BY [Date] DESC
-          FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)') AS Comments
+          FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), CHAR(13), ''), CHAR(10), '') AS Comments
     FROM [ImportNotification].[Transaction] T
     INNER JOIN [ImportNotification].[Notification] N ON N.Id = T.NotificationId
     GROUP BY N.Id, N.NotificationNumber
