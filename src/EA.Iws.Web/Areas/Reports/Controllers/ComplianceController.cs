ea8fc99536311bed3780c17dc38fe10bc271b9fb
Gonzalo Girasol <gonzalog@sfwltd.co.uk>
2019-05-15 17:40:37 +0100

    Move guidance text to a resource file.

--- src/EA.Iws.Web/Areas/Reports/Controllers/ComplianceController.cs
+++ src/EA.Iws.Web/Areas/Reports/Controllers/ComplianceController.cs
@@ -12,6 +12,7 @@
     using Prsd.Core.Mediator;
     using Requests.Admin.Reports;
     using ViewModels.Compliance;
+    using Resources = ComplianceGuidanceResources;
 
     [AuthorizeActivity(typeof(GetComplianceReport))]
     public class ComplianceController : Controller
@@ -69,28 +70,28 @@
 
             var guidance = new ComplianceDataGuidance
             {
-                NotificationNumber = "This shows the relevant notification number",
-                NoPrenotificationCount = "This shows the total number of shipments recorded in the service that haven't met prenotifcation rules",
-                PreNotificationColour = "The colour indicates the level of compliance based on data shown in the previous cell. 0 = GREEN, 1 to 10 = AMBER and 11 or more = RED",
-                MissingShipments = "This shows whether there are gaps in the sequential list of shipments for the notification",
-                MissingShipmentsColour = "The colour indicates the level of compliance based on data shown in the previous cell. 0 = GREEN, 1 to 10 = AMBER and 11 or more = RED",
-                OverLimitShipments = "For export notifications: This shows the number of shipments that are 'active' which exceed the number of permitted active loads approved against the financial provision. For import notifications: not monitiored by the service N/A",
-                OverActiveLoads = "The colour indicates the level of compliance based on data shown in the previous cell. 0 = GREEN, 1 to 5 = AMBER and 6 or more = RED",
-                OverTonnage = "This shows whether the total intended quantity consented on the notification has been exceeded (Y/N)",
-                OverTonnageColour = "The colour indicates compliance based on data shown in the previous cell. N = GREEN, and Y = RED",
-                OverShipments = "This shows whether the total intended number of shipments consented on the notification has been exceeded (Y/N)",
-                OverShipmentsColour = "The colour indicates compliance based on data shown in the previous cell. N = GREEN, and Y = RED",
-                Notifier = "This shows the name of the notifier/exporter for the notification",
-                Consignee = "This shows the name of the consignee/importer for the notification",
-                FileExpired = "This shows whether the notification has expired by having gone passed the 'Consent valid to' date when this report was run"
+                NotificationNumber = Resources.NotificationNumber,
+                NoPrenotificationCount = Resources.NoPrenotificationCount,
+                PreNotificationColour = Resources.PreNotificationColour,
+                MissingShipments = Resources.MissingShipments,
+                MissingShipmentsColour = Resources.MissingShipmentsColour,
+                OverLimitShipments = Resources.OverLimitShipments,
+                OverActiveLoads = Resources.OverActiveLoads,
+                OverTonnage = Resources.OverTonnage,
+                OverTonnageColour = Resources.OverTonnageColour,
+                OverShipments = Resources.OverShipments,
+                OverShipmentsColour = Resources.OverShipmentsColour,
+                Notifier = Resources.Notifier,
+                Consignee = Resources.Consignee,
+                FileExpired = Resources.FileExpired
             };
 
             var colourGuidance = new ComplianceDataColourGuidance
             {
-                HeaderText = "Colour/Symbol key",
-                GreenText = "GREEN with a black tick overlay indicates compliance",
-                AmberText = "AMBER with a black exclamation indicates non-compliance to a lesser degree",
-                RedText = "RED with a white cross indicates non-compliance to a greater degree warranting action"
+                HeaderText = Resources.ColourHeaderText,
+                GreenText = Resources.GreenText,
+                AmberText = Resources.AmberText,
+                RedText = Resources.RedText
             };
 
             return new ComplianceXlsxActionResult(report, new List<ComplianceDataGuidance> { guidance }, colourGuidance,
