85d67c3347c01a2bbe86e03f0c47b4b50ed7a5a4
Gonzalo Girasol <gonzalog@sfwltd.co.uk>
2019-05-17 10:13:22 +0100

    Add abandon link on the new shipments screen.

--- src/EA.Iws.Web/Areas/AdminExportNotificationMovements/Controllers/CancelController.cs
+++ src/EA.Iws.Web/Areas/AdminExportNotificationMovements/Controllers/CancelController.cs
@@ -217,6 +217,14 @@
         }
 
         [HttpGet]
+        public ActionResult AddAbandon(Guid id)
+        {
+            TempData[AddedCancellableMovementsListKey] = null;
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
         public ActionResult Abandon(Guid id)
         {
             TempData[SubmittedMovementListKey] = null;
