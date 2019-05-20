05c5753640293d8c66c06a18606094d78a572ef2
SumeN <sumen@sfwltd.co.uk>
2019-05-20 15:57:57 +0100

    Show image if notifciation has comments

---
+++ src/EA.Iws.RequestHandlers/ImportNotification/CheckImportNotificationHasCommentsHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.Iws.RequestHandlers.ImportNotification
+{
+    using System.Threading.Tasks;
+    using Domain.ImportNotificationAssessment;
+    using Prsd.Core.Mediator;
+    using Requests.ImportNotification;
+
+    internal class CheckImportNotificationHasCommentsHandler : IRequestHandler<CheckImportNotificationHasComments, bool>
+    {
+        private readonly IImportNotificationCommentRepository repository;
+
+        public CheckImportNotificationHasCommentsHandler(IImportNotificationCommentRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<bool> HandleAsync(CheckImportNotificationHasComments message)
+        {
+            var commentsCount = await repository.GetCommentsCountForImportNotification(message.ImportNotificationId);
+
+            return commentsCount > 0 ? true : false;
+        }
+    }
+}
