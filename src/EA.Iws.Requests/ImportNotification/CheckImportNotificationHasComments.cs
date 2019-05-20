27f54cce3cd1ace30f9abff7e1ce067e6d9ad5c4
SumeN <SumeN@users.noreply.github.com>
2019-05-20 16:49:10 +0100

    Show image if notification has comments (#1052)
    * Show image if notifciation has comments
    
    * Code Simplification

---
+++ src/EA.Iws.Requests/ImportNotification/CheckImportNotificationHasComments.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Iws.Requests.ImportNotification
+{
+    using System;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
+    using Prsd.Core.Mediator;
+
+    [RequestAuthorization(ImportNotificationPermissions.CanEditComments)]
+    public class CheckImportNotificationHasComments : IRequest<bool>
+    {
+        public Guid ImportNotificationId { get; private set; }
+
+        public CheckImportNotificationHasComments(Guid importNotificationId)
+        {
+            ImportNotificationId = importNotificationId;
+        }
+    }
+}
