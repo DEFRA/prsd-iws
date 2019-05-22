570dde5c9bd3482f15357460e9d9c9445935bc96
Gonzalo Girasol <35956285+GonzaloGirasol@users.noreply.github.com>
2019-05-22 16:16:20 +0100

    Financial guarantee changes (#1056)
    * Add Decision made and Current status on Financial guarantee assessment screen for exports.
    
    * Hide button for recording new financial guarantee in certain notification statuses.
    
    * Update financial guarantee link so that it's not be visible when current FG has been released

--- src/EA.Iws.RequestHandlers/Admin/FinancialGuarantee/GetCurrentFinancialGuaranteeDetailsHandler.cs
+++ src/EA.Iws.RequestHandlers/Admin/FinancialGuarantee/GetCurrentFinancialGuaranteeDetailsHandler.cs
@@ -1,10 +1,13 @@
 ﻿namespace EA.Iws.RequestHandlers.Admin.FinancialGuarantee
 {
+    using System.Linq;
     using System.Threading.Tasks;
     using Core.FinancialGuarantee;
     using Core.Notification;
+    using Core.NotificationAssessment;
     using Domain.FinancialGuarantee;
     using Domain.NotificationApplication;
+    using Domain.NotificationAssessment;
     using Prsd.Core.Mapper;
     using Prsd.Core.Mediator;
     using Requests.Admin.FinancialGuarantee;
@@ -15,14 +18,18 @@
         private readonly IMapWithParameter<FinancialGuarantee, UKCompetentAuthority, FinancialGuaranteeData> financialGuaranteeMap;
         private readonly IFinancialGuaranteeRepository financialGuaranteeRepository;
         private readonly INotificationApplicationRepository notificationApplicationRepository;
+        private readonly INotificationAssessmentRepository assessmentRepository;
 
-        public GetCurrentFinancialGuaranteeDetailsHandler(IMapWithParameter<FinancialGuarantee, UKCompetentAuthority, FinancialGuaranteeData> financialGuaranteeMap,
+        public GetCurrentFinancialGuaranteeDetailsHandler(
+            IMapWithParameter<FinancialGuarantee, UKCompetentAuthority, FinancialGuaranteeData> financialGuaranteeMap,
             IFinancialGuaranteeRepository financialGuaranteeRepository,
-            INotificationApplicationRepository notificationApplicationRepository)
+            INotificationApplicationRepository notificationApplicationRepository,
+            INotificationAssessmentRepository assessmentRepository)
         {
             this.financialGuaranteeMap = financialGuaranteeMap;
             this.financialGuaranteeRepository = financialGuaranteeRepository;
             this.notificationApplicationRepository = notificationApplicationRepository;
+            this.assessmentRepository = assessmentRepository;
         }
 
         public async Task<FinancialGuaranteeData> HandleAsync(GetCurrentFinancialGuaranteeDetails message)
@@ -30,8 +37,26 @@
             var financialGuaranteeCollection = await financialGuaranteeRepository.GetByNotificationId(message.NotificationId);
             var financialGuarantee = financialGuaranteeCollection.GetLatestFinancialGuarantee();
             var authority = (await notificationApplicationRepository.GetById(message.NotificationId)).CompetentAuthority;
+            var notificationStatus = await assessmentRepository.GetStatusByNotificationId(message.NotificationId);
 
-            return financialGuaranteeMap.Map(financialGuarantee, authority);
+            var result = financialGuaranteeMap.Map(financialGuarantee, authority);
+
+            result.IsNotificationStatusRecordable = IsRecordableStatus(notificationStatus);
+
+            return result;
+        }
+
+        private static bool IsRecordableStatus(NotificationStatus status)
+        {
+            var invalidStatuses = new[]
+            {
+                NotificationStatus.Withdrawn,
+                NotificationStatus.ConsentWithdrawn,
+                NotificationStatus.Objected,
+                NotificationStatus.FileClosed
+            };
+
+            return !invalidStatuses.Any(x => x == status);
         }
     }
 }
