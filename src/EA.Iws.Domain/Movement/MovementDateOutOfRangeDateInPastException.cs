3c7cde26a490403dba6ba2f050b8ec24e8f74b4d
Gonzalo Girasol <35956285+GonzaloGirasol@users.noreply.github.com>
2019-05-22 14:33:52 +0100

    External edit shipment date (#1055)
    * Use shared date input view model for editing shipment date as an external user.
    
    * Add validation rules for active load restrictions on Edit shipment date screen
    
    * Handle dates in the past.

---
+++ src/EA.Iws.Domain/Movement/MovementDateOutOfRangeDateInPastException.cs
@@ -0,0 +1,27 @@
+﻿namespace EA.Iws.Domain.Movement
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class MovementDateOutOfRangeDateInPastException : MovementDateException
+    {
+        public MovementDateOutOfRangeDateInPastException()
+        {
+        }
+
+        public MovementDateOutOfRangeDateInPastException(string message) : base(message)
+        {
+        }
+
+        public MovementDateOutOfRangeDateInPastException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected MovementDateOutOfRangeDateInPastException(
+          SerializationInfo info,
+          StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
