bb6f7b2b032c68a48c4e735ada56a08b379c7fe7
Gonzalo Girasol <35956285+GonzaloGirasol@users.noreply.github.com>
2019-05-23 09:50:17 +0100

    Hide/reveal producers and carriers on overview screen (#1057)
    * Hide/reveal for more than one Producer on Notification overview screen for export notifications.
    
    * Hide/reveal for more than one Intended Carrier on Notification overview screen for export notifications.

--- src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/OverviewController.cs
+++ src/EA.Iws.Web/Areas/AdminExportAssessment/Controllers/OverviewController.cs
@@ -53,11 +53,13 @@
                                                                          NotificationStatus.Consented;
             model.OrganisationsInvolvedViewModel.CanAddProducer = canAddProducer && result.SubmitSummaryData.Status != NotificationStatus.FileClosed;
             model.OrganisationsInvolvedViewModel.CanEditContactDetails = canEditContactDetails && result.SubmitSummaryData.Status != NotificationStatus.FileClosed;
+            model.OrganisationsInvolvedViewModel.DisplayAllProducers = IsEditableStatus(result.SubmitSummaryData.Status);
             model.WasteCodeOverviewViewModel.CanEditEWCCodes = canEditEwcCodes && result.SubmitSummaryData.Status == NotificationStatus.Consented;
             model.WasteCodeOverviewViewModel.CanEditYCodes = canEditYCodes && result.SubmitSummaryData.Status == NotificationStatus.Consented;
             model.RecoveryOperationViewModel.CanEditCodes = canEditOperationCodes && result.SubmitSummaryData.Status == NotificationStatus.Consented;
             model.WasteCodeOverviewViewModel.CanEditHCodes = canEditHCodes && result.SubmitSummaryData.Status == NotificationStatus.Consented;
             model.TransportationViewModel.CanAddCarrier = canAddCarrier && result.SubmitSummaryData.Status == NotificationStatus.Consented;
+            model.TransportationViewModel.DisplayAllCarriers = IsEditableStatus(result.SubmitSummaryData.Status);
 
             return View(model);
         }
