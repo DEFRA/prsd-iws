400372c91f939b0a12737f34251fe3388e7003d5
John Padgett <john.padgett@civica.co.uk>
2020-02-18 15:11:31 +0000

    Add unit tests for api authorisation

---
+++ src/EA.Iws.Api.Tests.Unit/Controllers/RegistrationControllerTests.cs
@@ -0,0 +1,23 @@
+﻿namespace EA.Iws.Api.Tests.Unit.Controllers
+{
+    using System.Web.Http;
+    using Api.Controllers;
+    using FluentAssertions;
+    using TestHelpers.Helpers;
+    using Xunit;
+
+    public class RegistrationControllerTests
+    {
+        [Fact]
+        public void Controller_ShouldHaveAuthorizeAttribute()
+        {
+            typeof(RegistrationController).Should().BeDecoratedWith<AuthorizeAttribute>();
+        }
+
+        [Fact]
+        public void Controller_ShouldNotHaveAnonymousActions()
+        {
+            AttributeHelper.ShouldNotHaveAnonymousMethods(typeof(RegistrationController));
+        }
+    }
+}
