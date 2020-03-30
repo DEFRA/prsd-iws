47a3d54c27b63c9769ee7161f016d0f0319203d2
John Padgett <john.padgett@civica.co.uk>
2020-03-30 09:32:40 +0100

    Update to admin view models so that the generic password error is displayed always

--- src/EA.Iws.Web/Areas/Admin/ViewModels/Registration/AdminRegistrationViewModel.cs
+++ src/EA.Iws.Web/Areas/Admin/ViewModels/Registration/AdminRegistrationViewModel.cs
@@ -42,7 +42,6 @@
         public override string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long", MinimumLength = 8)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public override string Password { get; set; }
