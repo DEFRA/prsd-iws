751a2f54aff8ded8c940c220046c4fc406a10c74
Alisdair Fyfe <alisdair.fyfe@civica.co.uk>
2020-10-23 21:57:54 +0100

    removed transaction and try catch statements from ascension island adding script

--- src/EA.Iws.Database/scripts/Update/0007-Support/20200918-328081-CR0044-add_ascension_island.sql
+++ src/EA.Iws.Database/scripts/Update/0007-Support/20200918-328081-CR0044-add_ascension_island.sql
@@ -1,6 +1,4 @@
-﻿BEGIN
-    SET NOCOUNT ON;
-    DECLARE @CountryId          UNIQUEIDENTIFIER    = (SELECT NEWID()),
+﻿    DECLARE @CountryId          UNIQUEIDENTIFIER    = (SELECT NEWID()),
             @CountryName        NVARCHAR(2048)      = 'Ascension Island',
             @IsoAlpha2Code      NCHAR(2)            = 'AC',
             @IsEuropeanMember   bit                 = 0,
@@ -10,24 +8,9 @@
             @CAcode             NVARCHAR(25);
 
     SET @CAcode = @IsoAlpha2Code; -- Currently set to be same as country code
-    BEGIN TRY
-        SET XACT_ABORT ON;
-        BEGIN TRAN
 
-            INSERT INTO [Lookup].[Country] ([Id],[Name],[IsoAlpha2Code],[IsEuropeanUnionMember])
-            VALUES (@CountryId, @CountryName, @IsoAlpha2Code, @IsEuropeanMember);
+	INSERT INTO [Lookup].[Country] ([Id],[Name],[IsoAlpha2Code],[IsEuropeanUnionMember])
+	VALUES (@CountryId, @CountryName, @IsoAlpha2Code, @IsEuropeanMember);
 
-            INSERT INTO [Lookup].[CompetentAuthority] ([Id],[Name],[Abbreviation],[IsSystemUser],[Code],[CountryId],[Region],[IsTransitAuthority])
-            VALUES (NEWID(), @CAName, NULL, @IsSystemUser, @CAcode, @CountryId, NULL, @IsTransitAuthority);
-
-        --COMMIT TRAN
-        ROLLBACK TRAN
-
-    END TRY
-    BEGIN CATCH
-
-        IF @@TRANCOUNT > 0
-            ROLLBACK;
-        THROW
-    END CATCH
-END
+	INSERT INTO [Lookup].[CompetentAuthority] ([Id],[Name],[Abbreviation],[IsSystemUser],[Code],[CountryId],[Region],[IsTransitAuthority])
+	VALUES (NEWID(), @CAName, NULL, @IsSystemUser, @CAcode, @CountryId, NULL, @IsTransitAuthority);
