7100c3cc7577491b7bd27ad6a45d004620eb0d33
Hamza-Khaleel <hamza.khaleel@civica.co.uk>
2020-10-23 10:27:51 +0100

    328081 | Update spelling and white spaces

--- src/EA.Iws.Database/scripts/Update/0007-Support/20200918-328081-CR0044-add_ascension_island.sql
+++ src/EA.Iws.Database/scripts/Update/0007-Support/20200918-328081-CR0044-add_ascension_island.sql
@@ -1,34 +1,33 @@
 ﻿BEGIN 
-	SET NOCOUNT ON;
-	DECLARE @CountryId		    UNIQUEIDENTIFIER    = (SELECT NEWID()),
+    SET NOCOUNT ON;
+    DECLARE @CountryId          UNIQUEIDENTIFIER    = (SELECT NEWID()),
             @CountryName        NVARCHAR(2048)      = 'Ascension Island',
             @IsoAlpha2Code      NCHAR(2)            = 'AC',
-            @IsEuropeanMemeber  bit                 = 0,
-            @CAName             NVARCHAR(1023)      = 'Other', 
-            @IsSystemUser       bit                 = 0,       
+            @IsEuropeanMember   bit                 = 0,
+            @CAName             NVARCHAR(1023)      = 'Other',
+            @IsSystemUser       bit                 = 0,
             @IsTransitAuthority bit                 = NULL,
             @CAcode             NVARCHAR(25);
+    
+    SET @CAcode = @IsoAlpha2Code; -- Currently set to be same as country code
+    BEGIN TRY
+        SET XACT_ABORT ON; 
+        BEGIN TRAN
 
-	BEGIN TRY
-		SET XACT_ABORT ON; 
-		BEGIN TRAN
-			
-			SET @CAcode = @IsoAlpha2Code; -- Currently set to be same as country code
-
-			INSERT INTO [Lookup].[Country] ([Id],[Name],[IsoAlpha2Code],[IsEuropeanUnionMember]) 
+            INSERT INTO [Lookup].[Country] ([Id],[Name],[IsoAlpha2Code],[IsEuropeanUnionMember])
             VALUES (@CountryId, @CountryName, @IsoAlpha2Code, @IsEuropeanMemeber);
-            
+
             INSERT INTO [Lookup].[CompetentAuthority] ([Id],[Name],[Abbreviation],[IsSystemUser],[Code],[CountryId],[Region],[IsTransitAuthority])
             VALUES (NEWID(), @CAName, NULL, @IsSystemUser, @CAcode, @CountryId, NULL, @IsTransitAuthority);
-			
-		--COMMIT TRAN
-		ROLLBACK TRAN
-													
-	END TRY
-	BEGIN CATCH
-				
-		IF @@TRANCOUNT > 0
-			ROLLBACK;
-		THROW
-	END CATCH
+
+        --COMMIT TRAN
+        ROLLBACK TRAN
+
+    END TRY
+    BEGIN CATCH
+
+        IF @@TRANCOUNT > 0
+            ROLLBACK;
+        THROW
+    END CATCH
 END
