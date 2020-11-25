c69a3939647dbdce694a221b3217510b551b685c
Jonathan Stratford <jonathan.stratford@civica.co.uk>
2020-11-25 14:59:18 +0000

    [post-brexit-part2-GB-NI-imports-exports] Add domain class for new table

---
+++ src/EA.Iws.Domain/IntraCountryExportAllowed.cs
@@ -0,0 +1,14 @@
+﻿namespace EA.Iws.Domain
+{
+    using System;
+
+    public class IntraCountryExportAllowed
+    {
+        public Guid ExportCompetentAuthorityId { get; private set; }
+        public Guid ImportCompetentAuthorityId { get; private set; }
+
+        protected IntraCountryExportAllowed()
+        {
+        }
+    }
+}
