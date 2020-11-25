709c1e38641b53f871b7a012596093c84644bd42
Jonathan Stratford <jonathan.stratford@civica.co.uk>
2020-11-25 15:11:22 +0000

    [post-brexit-part2-GB-NI-imports-exports] Add Data class for IntraCountryExportAllowed

---
+++ src/EA.Iws.Core/Shared/IntraCountryExportAllowedData.cs
@@ -0,0 +1,10 @@
+﻿namespace EA.Iws.Core.Shared
+{
+    using System;
+
+    public class IntraCountryExportAllowedData
+    {
+        public Guid ExportCompetentAuthorityId { get; set; }
+        public Guid ImportCompetentAuthorityId { get; set; }
+    }
+}
