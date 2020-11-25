63de1c1b2b97810a592571a6a1d3efebf2c16119
Jonathan Stratford <jonathan.stratford@civica.co.uk>
2020-11-25 15:34:33 +0000

    [post-brexit-part2-GB-NI-imports-exports] Add Mapping from table to domain object for intra-country exports table

---
+++ src/EA.Iws.DataAccess/Mappings/Common/IntraCountryExportAllowedMapping.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Iws.DataAccess.Mappings.Exports
+{
+    using System.Data.Entity.ModelConfiguration;
+    using EA.Iws.Domain;
+
+    public class IntraCountryExportAllowedMapping : EntityTypeConfiguration<IntraCountryExportAllowed>
+    {
+        public IntraCountryExportAllowedMapping()
+        {
+            ToTable("IntraCountryExportAllowed", "Lookup")
+                .HasKey(x => new
+                {
+                    x.ExportCompetentAuthorityId,
+                    x.ImportCompetentAuthorityId
+                });
+            Property(x => x.ExportCompetentAuthorityId).IsRequired();
+            Property(x => x.ImportCompetentAuthorityId).IsRequired();
+        }
+    }
+}
