099e3870998d1b6131955be3971de41b54f651aa
Jonathan Stratford <jonathan.stratford@civica.co.uk>
2020-11-25 15:39:18 +0000

    [post-brexit-part2-GB-NI-imports-exports] Add Map class for IntraCountryExportAllowed to IntraCountryExportAllowedData

---
+++ src/EA.Iws.RequestHandlers/Mappings/IntraCountryExportAllowedMap.cs
@@ -0,0 +1,18 @@
+﻿namespace EA.Iws.RequestHandlers.Mappings
+{
+    using Core.Shared;
+    using Domain;
+    using Prsd.Core.Mapper;
+
+    internal class IntraCountryExportAllowedMap : IMap<IntraCountryExportAllowed, IntraCountryExportAllowedData>
+    {
+        public IntraCountryExportAllowedData Map(IntraCountryExportAllowed source)
+        {
+            return new IntraCountryExportAllowedData
+            {
+                ExportCompetentAuthorityId = source.ExportCompetentAuthorityId,
+                ImportCompetentAuthorityId = source.ImportCompetentAuthorityId
+            };
+        }
+    }
+}
