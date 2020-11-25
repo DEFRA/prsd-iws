cdccd0fe071cb0fbab6120c4e40533f1746b9db3
Jonathan Stratford <jonathan.stratford@civica.co.uk>
2020-11-25 15:05:40 +0000

    [post-brexit-part2-GB-NI-imports-exports] Add repository for intra-country exports table

---
+++ src/EA.Iws.DataAccess/Repositories/IntraCountryExportAllowedRepository.cs
@@ -0,0 +1,33 @@
+﻿namespace EA.Iws.DataAccess.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Domain;
+
+    internal class IntraCountryExportAllowedRepository : IIntraCountryExportAllowedRepository
+    {
+        private readonly IwsContext context;
+
+        public IntraCountryExportAllowedRepository(IwsContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<IntraCountryExportAllowed>> GetAll()
+        {
+            return await
+                context.IntraCountryExportAllowed.ToArrayAsync();
+        }
+
+        public async Task<IEnumerable<IntraCountryExportAllowed>> GetImportCompetentAuthorities(Guid exportCompetentAuthority)
+        {
+            return await
+                context.IntraCountryExportAllowed.Where(
+                    c => (c.ExportCompetentAuthorityId == exportCompetentAuthority))
+                    .ToArrayAsync();
+        }
+    }
+}
