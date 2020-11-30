32fa32e9196848db31bcead3bd66eff1d81abe37
Jonathan Stratford <jonathan.stratford@civica.co.uk>
2020-11-30 11:57:54 +0000

    [post-brexit-part2-GB-NI-imports-exports] Add new class for getting entry points given a country ID and optional exit point CA ID
    Entry and Exit point retrievers need splitting out for Notifications

---
+++ src/EA.Iws.Requests/TransportRoute/GetCompetentAuthoritiesAndEntryPointsByCountryId.cs
@@ -0,0 +1,20 @@
+﻿namespace EA.Iws.Requests.TransportRoute
+{
+    using System;
+    using Core.Authorization;
+    using Core.Authorization.Permissions;
+    using Prsd.Core.Mediator;
+
+    [RequestAuthorization(GeneralPermissions.CanReadCountryData)]
+    public class GetCompetentAuthoritiesAndEntryPointsByCountryId : IRequest<CompetentAuthorityAndEntryOrExitPointData>
+    {
+        public Guid Id { get; private set; }
+        public Guid? ExitPointCompetentAuthorityId { get; private set; }
+
+        public GetCompetentAuthoritiesAndEntryPointsByCountryId(Guid id, Guid? exitPointCompetentAuthorityId)
+        {
+            Id = id;
+            ExitPointCompetentAuthorityId = exitPointCompetentAuthorityId;
+        }
+    }
+}
