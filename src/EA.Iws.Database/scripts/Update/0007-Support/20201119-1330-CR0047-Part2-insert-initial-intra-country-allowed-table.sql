b07121ba15c460cd3e9e85f95035acc47c7440d9
Alisdair Fyfe <alisdair.fyfe@civica.co.uk>
2020-12-03 11:54:44 +0000

    updated the scripts to create and seed then new table for allowed intra-country exports

--- src/EA.Iws.Database/scripts/Update/0007-Support/20201119-1330-CR0047-Part2-insert-initial-intra-country-allowed-table.sql
+++ src/EA.Iws.Database/scripts/Update/0007-Support/20201119-1330-CR0047-Part2-insert-initial-intra-country-allowed-table.sql
@@ -1,11 +1,16 @@
+
 DECLARE @EAID UNIQUEIDENTIFIER = (SELECT ID FROM [Lookup].[CompetentAuthority] WHERE Abbreviation = 'EA' AND IsTransitAuthority = 0)
 DECLARE @SEPAID UNIQUEIDENTIFIER = (SELECT ID FROM [Lookup].[CompetentAuthority] WHERE Abbreviation = 'SEPA' AND IsTransitAuthority = 0)
 DECLARE @NRWID UNIQUEIDENTIFIER = (SELECT ID FROM [Lookup].[CompetentAuthority] WHERE Abbreviation = 'NRW' AND IsTransitAuthority = 0)
 DECLARE @NIEAID UNIQUEIDENTIFIER = (SELECT ID FROM [Lookup].[CompetentAuthority] WHERE Abbreviation = 'NIEA' AND IsTransitAuthority = 0)
 
+DECLARE @EA_UKCA_ID INT = (SELECT ID FROM [Lookup].[UnitedKingdomCompetentAuthority] WHERE CompetentAuthorityId = @EAID)
+DECLARE @SEPA_UKCA_ID INT = (SELECT ID FROM [Lookup].[UnitedKingdomCompetentAuthority] WHERE CompetentAuthorityId = @SEPAID)
+DECLARE @NRW_UKCA_ID INT = (SELECT ID FROM [Lookup].[UnitedKingdomCompetentAuthority] WHERE CompetentAuthorityId = @NRWID)
+
 INSERT INTO [Lookup].[IntraCountryExportAllowed]
-(ExportCompetentAuthorityID, ImportCompetentAuthorityID)
+([ExportCompetentAuthority], [ImportCompetentAuthorityID])
 VALUES
-(@SEPAID,@NIEAID),
-(@NRWID,@NIEAID),
-(@EAID,@NIEAID)
+(@EA_UKCA_ID,@NIEAID),
+(@NRW_UKCA_ID,@NIEAID),
+(@SEPA_UKCA_ID,@NIEAID)
