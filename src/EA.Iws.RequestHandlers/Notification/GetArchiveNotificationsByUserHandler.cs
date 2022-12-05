d6650d85a3d7a8befd023b17dfc4a478da41396a
CIVICA\Jonathan.Rush <jonathan.rush@civica.co.uk>
2022-12-05 18:04:07 +0000

    Adding pagination to the initial archive page.

--- src/EA.Iws.RequestHandlers/Notification/GetArchiveNotificationsByUserHandler.cs
+++ src/EA.Iws.RequestHandlers/Notification/GetArchiveNotificationsByUserHandler.cs
@@ -11,7 +11,7 @@
     {
         private readonly IwsContext context;
         private readonly IUserContext userContext;
-        private readonly int pageSize = 20;
+        private readonly int pageSize = 25;
 
         public GetArchiveNotificationsByUserHandler(IwsContext context, IUserContext userContext)
         {
@@ -23,32 +23,16 @@
         //and is greater than 3 years from the current date.
         //User should only see notifications from companies that relate to their appropriate authority
         //Order the list showing the oldest at the top to the newest, based on generated date.
-        //OrganisationName
         public async Task<UserArchiveNotifications> HandleAsync(GetArchiveNotificationsByUser message)
         {
-            //Modify to Stored Procedure
-            //Add to C:\Workspace\prsd-iws\src\EA.Iws.Database\scripts\Everytime\03-StoredProcedures\
-
-            //Needs to be export and import notifications
-
-            //[Notification],[notification] union with [ImportNotification].[Notification]
-            //TODO JR use E.Name AS Exporter, for company name
-            //TODO JR filter by user competent authority
-            //TODO JR filter by dateCreated > 3 years
-
-            var notifications = await context.Database.SqlQuery<NotificationArchiveSummaryData>("[Notification].[uspGetArchiveNotificationsByUser] @UserId, @Skip, @Take",
+            var notifications = await context.Database.SqlQuery<NotificationArchiveSummaryData>(
+                "[Notification].[uspGetArchiveNotificationsByUser] @UserId, @Skip, @Take",
                 new SqlParameter("@UserId", userContext.UserId),
                 new SqlParameter("@Skip", (message.PageNumber - 1) * pageSize),
                 new SqlParameter("@Take", pageSize)).ToListAsync();
 
-            //TODO JR Fix this to show the number of notifications overall.
-            var numberOfNotifications = await context.Database.SqlQuery<int>(
-                @"SELECT COUNT(N.[Id])
-                  FROM [Notification].[Notification] N
-                  INNER JOIN [Notification].[NotificationAssessment] NA ON N.Id = NA.NotificationApplicationId
-                  LEFT JOIN [Notification].[SharedUser] SU ON SU.NotificationId = N.Id AND SU.UserId =  @Id
-                  WHERE (N.UserId = @Id OR SU.UserId = @Id) ",
-                new SqlParameter("@Id", userContext.UserId)).SingleAsync();
+            var numberOfNotifications = await context.Database.SqlQuery<int>("[Notification].[uspGetArchiveNotificationsCountByUser] @UserId",
+                new SqlParameter("@UserId", userContext.UserId)).SingleAsync();
 
             return new UserArchiveNotifications(notifications, numberOfNotifications, message.PageNumber, pageSize);
         }
