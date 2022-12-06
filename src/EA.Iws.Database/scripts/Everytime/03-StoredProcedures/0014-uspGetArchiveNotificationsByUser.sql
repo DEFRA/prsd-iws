a6cac0c634b18b69cdd39e66157090d197d396dd
CIVICA\Jonathan.Rush <jonathan.rush@civica.co.uk>
2022-12-06 15:24:32 +0000

    Make Status a string with an inner join on the lookup

--- src/EA.Iws.Database/scripts/Everytime/03-StoredProcedures/0014-uspGetArchiveNotificationsByUser.sql
+++ src/EA.Iws.Database/scripts/Everytime/03-StoredProcedures/0014-uspGetArchiveNotificationsByUser.sql
@@ -1,25 +1,24 @@
 ﻿IF OBJECT_ID('[Notification].[uspGetArchiveNotificationsByUser]') IS NULL
     EXEC('CREATE PROCEDURE [Notification].[uspGetArchiveNotificationsByUser] AS SET NOCOUNT ON;')
 GO
-
     ALTER PROCEDURE [Notification].[uspGetArchiveNotificationsByUser]
         @UserId UNIQUEIDENTIFIER,
         @Skip int,
         @Take int
-
 AS
 BEGIN
     SET NOCOUNT ON;
     SELECT
         N.Id,
         N.NotificationNumber,
-        NA.[Status],
+        LNS.[Description] AS [Status],
         N.CreatedDate,
         E.[Name] AS CompanyName,
 	    N.CompetentAuthority
     FROM
         [Notification].[Notification] N
         INNER JOIN [Notification].[NotificationAssessment] NA ON N.Id = NA.NotificationApplicationId
+        INNER JOIN [Lookup].NotificationStatus LNS on LNS.Id = NA.[Status]
         LEFT JOIN [Notification].[Exporter] E ON N.Id = E.NotificationId
     WHERE n.CreatedDate < dateadd(year, -3, getdate())
 	    AND NA.[Status] IN (14,8,9,11)
@@ -28,13 +27,14 @@ BEGIN
     SELECT 
 	    INN.Id,
 	    INN.NotificationNumber,
-	    INNA.[Status],
+	    LINS.[Description] as [Status],
 	    INND.NotificationReceivedDate,
 	    INE.[Name] as CompanyName,
 	    INN.CompetentAuthority
     FROM [ImportNotification].[Notification] INN
         INNER JOIN [ImportNotification].[NotificationAssessment] INNA ON INN.Id = INNA .NotificationApplicationId
 	    INNER JOIN [ImportNotification].[NotificationDates] INND ON INND.NotificationAssessmentId = INNA.Id
+		INNER JOIN [Lookup].ImportNotificationStatus LINS ON LINS.Id = INNA.[Status]
 	    LEFT JOIN [ImportNotification].[Exporter] INE on INN.Id = INE.ImportNotificationId
     WHERE INND.NotificationReceivedDate < dateadd(year, -3, getdate())
 	    AND INNA.[Status] IN (13,12,11,10)
