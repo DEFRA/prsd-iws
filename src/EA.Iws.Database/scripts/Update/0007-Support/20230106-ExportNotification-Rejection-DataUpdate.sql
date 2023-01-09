d72cb913d7ec2d37d33e63312d007fc3c0e4ed07
Sreedhar Bangarugari <Sreedhar.Bangarugari@civica.co.uk>
2023-01-09 09:39:04 +0000

    Modified Shipment report cache table column name.

--- src/EA.Iws.Database/scripts/Update/0007-Support/20230106-ExportNotification-Rejection-DataUpdate.sql
+++ src/EA.Iws.Database/scripts/Update/0007-Support/20230106-ExportNotification-Rejection-DataUpdate.sql
@@ -1,1 +1,3 @@
-﻿UPDATE [Notification].[MovementRejection] SET RejectedQuantity=0, RejectedUnit=1 WHERE RejectedQuantity IS NULL
+﻿EXEC sp_RENAME '[Reports].[ShipmentsCache].ShipmentRejectedDate' , 'RejectedShipmentDate', 'COLUMN'
+
+UPDATE [Notification].[MovementRejection] SET RejectedQuantity=0, RejectedUnit=1 WHERE RejectedQuantity IS NULL
