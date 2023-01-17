627655dc0d4177ca6fb7f0eb99918f73b9351748
CIVICA\Jonathan.Rush <jonathan.rush@civica.co.uk>
2023-01-17 15:10:45 +0000

    Correct archive stored procedure to remove file id from movement tables

--- src/EA.Iws.Database/scripts/Everytime/03-StoredProcedures/0016-uspArchiveNotification.sql
+++ src/EA.Iws.Database/scripts/Everytime/03-StoredProcedures/0016-uspArchiveNotification.sql
@@ -60,6 +60,8 @@ BEGIN
 		
 		DELETE FROM [FileStore].[File] WHERE Id IN (SELECT FileId FROM [ImportNotification].[MovementPartialRejection]
 			WHERE MovementId IN (SELECT Id FROM [Notification].[Movement] WHERE NotificationId = @NotificationId))
+		UPDATE [ImportNotification].[MovementPartialRejection] SET FileId = null
+			WHERE MovementId IN (SELECT Id FROM [Notification].[Movement] WHERE NotificationId = @NotificationId)
 
 		UPDATE [ImportNotification].[Notification] SET IsArchived = 1, ArchivedByUserId = @CurrentUserId, ArchivedDate = SYSDATETIMEOFFSET() WHERE Id = @NotificationId
 		
@@ -127,15 +129,22 @@ BEGIN
 		DELETE FROM [FileStore].[File] where Id IN (SELECT FileId FROM [Notification].[Movement] WHERE NotificationId = @NotificationId)
 		DELETE FROM [FileStore].[File] where Id IN (SELECT FileId FROM [Notification].[MovementOperationReceipt] 
 			WHERE MovementId IN (select Id FROM [Notification].[Movement] WHERE NotificationId = @NotificationId))
-
 		DELETE FROM [FileStore].[File] WHERE Id IN (SELECT FileId FROM [Notification].[MovementPartialRejection]
 			WHERE MovementId IN (SELECT Id FROM [Notification].[Movement] WHERE NotificationId = @NotificationId))
-
 		DELETE FROM [FileStore].[File] WHERE Id IN (SELECT FileId FROM [Notification].[MovementReceipt] 
 			WHERE MovementId IN (SELECT Id FROM [Notification].[Movement] WHERE NotificationId = @NotificationId))
-
 		DELETE FROM [FileStore].[File] WHERE Id IN (SELECT FileId FROM [Notification].[MovementRejection]
 			WHERE MovementId IN (SELECT Id FROM [Notification].[Movement] WHERE NotificationId = @NotificationId))
+
+		UPDATE [Notification].[Movement] SET FileId = null WHERE NotificationId = @NotificationId
+		UPDATE [Notification].[MovementOperationReceipt] SET FileId = null 
+			WHERE MovementId IN (select Id FROM [Notification].[Movement] WHERE NotificationId = @NotificationId)
+		UPDATE [Notification].[MovementPartialRejection]  SET FileId = null 
+			WHERE MovementId IN (SELECT Id FROM [Notification].[Movement] WHERE NotificationId = @NotificationId)
+		UPDATE [Notification].[MovementReceipt]  SET FileId = null 
+			WHERE MovementId IN (SELECT Id FROM [Notification].[Movement] WHERE NotificationId = @NotificationId)
+		UPDATE [Notification].[MovementRejection] SET FileId = null 
+			WHERE MovementId IN (SELECT Id FROM [Notification].[Movement] WHERE NotificationId = @NotificationId)
 
 		UPDATE [Notification].[Notification] SET IsArchived = 1, ArchivedByUserId = @CurrentUserId, ArchivedDate = SYSDATETIMEOFFSET() WHERE Id = @NotificationId
 
