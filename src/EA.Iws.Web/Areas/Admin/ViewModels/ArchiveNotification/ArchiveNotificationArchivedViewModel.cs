c5c0bf1889ab6ce5129e7be5002cb395e1672a14
Sreedhar Bangarugari <Sreedhar.Bangarugari@civica.co.uk>
2023-01-18 12:54:17 +0000

    Archived screen bugs fixed.

--- src/EA.Iws.Web/Areas/Admin/ViewModels/ArchiveNotification/ArchiveNotificationArchivedViewModel.cs
+++ src/EA.Iws.Web/Areas/Admin/ViewModels/ArchiveNotification/ArchiveNotificationArchivedViewModel.cs
@@ -1,18 +1,41 @@
 ﻿namespace EA.Iws.Web.Areas.Admin.ViewModels.ArchiveNotification
 {
     using EA.Iws.Requests.Notification;
+    using EA.Iws.Web.Areas.Admin.Views.ArchiveNotification;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
-    public class ArchiveNotificationArchivedViewModel
+    public class ArchiveNotificationArchivedViewModel : IValidatableObject
     {
         public ArchiveNotificationArchivedViewModel()
         {
-        }
+        }        
 
         public IList<NotificationArchiveSummaryData> ArchivedNotifications { get; set; }
 
         public int SuccessCount { get; set; }
 
-        public int FailuredCount { get; set; }
+        public int FailureCount { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (HasAnyNotificationFailures == true)
+            {
+                yield return new ValidationResult(ArchiveNotificationResources.FailureMsg, new[] { "HasAnyNotificationFailures" });
+            }
+        }
+
+        private bool hasAnyNotificationFailures = true;
+        public bool HasAnyNotificationFailures
+        {
+            get
+            {
+                return hasAnyNotificationFailures;
+            }
+            set
+            {
+                hasAnyNotificationFailures = value;
+            }
+        }
     }
 }
