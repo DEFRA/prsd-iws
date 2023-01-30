32501ea1480e4c9a52427ea61deea0bd3e3f5d73
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2023-01-30 16:31:15 +0000

    ElmahErrorXmlFormat removed from the ArchiveNotificationRepository added into Core

---
+++ src/EA.Iws.Core/Admin/ElmahErrorXmlFormat.cs
@@ -0,0 +1,13 @@
+﻿namespace EA.Iws.Core.Admin
+{
+    public class ElmahErrorXmlFormat
+    {
+        public string Message { get; set; }
+
+        public string ExceptionMessage { get; set; }
+
+        public string ExceptionType { get; set; }
+
+        public string StackTrace { get; set; }
+    }
+}
