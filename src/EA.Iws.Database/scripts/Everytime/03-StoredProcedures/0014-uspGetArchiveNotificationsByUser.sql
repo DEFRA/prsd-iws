8af506cfb42dca7fc025fe85c74b620ec7ea7053
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2023-01-31 10:00:11 +0000

    Added 'OrderByDate' to the sort Archive notifications.

--- src/EA.Iws.Database/scripts/Everytime/03-StoredProcedures/0014-uspGetArchiveNotificationsByUser.sql
+++ src/EA.Iws.Database/scripts/Everytime/03-StoredProcedures/0014-uspGetArchiveNotificationsByUser.sql
@@ -3,8 +3,8 @@
 GO
     ALTER PROCEDURE [Notification].[uspGetArchiveNotificationsByUser]
         @UserId UNIQUEIDENTIFIER,
-        @Skip int,
-        @Take int
+        @Skip INT,
+        @Take INT
 AS
 BEGIN
     SET NOCOUNT ON;
@@ -12,35 +12,37 @@ BEGIN
         N.Id,
         N.NotificationNumber,
         LNS.[Description] AS [Status],
-        CONVERT(varchar, N.CreatedDate, 103) AS [CreatedDate],
+        CONVERT(VARCHAR, N.CreatedDate, 103) AS [CreatedDate],
+        N.CreatedDate AS OrderByDate,
         E.[Name] AS CompanyName,
 	    N.CompetentAuthority
     FROM
         [Notification].[Notification] N
         INNER JOIN [Notification].[NotificationAssessment] NA ON N.Id = NA.NotificationApplicationId
-        INNER JOIN [Lookup].NotificationStatus LNS on LNS.Id = NA.[Status]
+        INNER JOIN [Lookup].NotificationStatus LNS ON LNS.Id = NA.[Status]
         LEFT JOIN [Notification].[Exporter] E ON N.Id = E.NotificationId
     WHERE N.IsArchived = 0 AND N.CreatedDate < dateadd(year, -3, getdate())
 	    AND NA.[Status] IN (14,8,9,11)
-	    AND N.CompetentAuthority IN (select CompetentAuthority from [Person].[InternalUser] where UserId = @UserID)
+	    AND N.CompetentAuthority IN (SELECT CompetentAuthority FROM [Person].[InternalUser] WHERE UserId = @UserID)
     UNION 
     SELECT 
 	    INN.Id,
 	    INN.NotificationNumber,
-	    LINS.[Description] as [Status],
-        CONVERT(varchar, INND.NotificationReceivedDate, 103) AS [CreatedDate],
-	    INE.[Name] as CompanyName,
+	    LINS.[Description] AS [Status],
+        CONVERT(VARCHAR, INND.NotificationReceivedDate, 103) AS [CreatedDate],
+        INND.NotificationReceivedDate AS OrderByDate,
+	    INE.[Name] AS CompanyName,
 	    INN.CompetentAuthority
     FROM [ImportNotification].[Notification] INN
         INNER JOIN [ImportNotification].[NotificationAssessment] INNA ON INN.Id = INNA .NotificationApplicationId
 	    INNER JOIN [ImportNotification].[NotificationDates] INND ON INND.NotificationAssessmentId = INNA.Id
 		INNER JOIN [Lookup].ImportNotificationStatus LINS ON LINS.Id = INNA.[Status]
-	    LEFT JOIN [ImportNotification].[Exporter] INE on INN.Id = INE.ImportNotificationId
+	    LEFT JOIN [ImportNotification].[Exporter] INE ON INN.Id = INE.ImportNotificationId
     WHERE INN.IsArchived = 0 AND INND.NotificationReceivedDate < dateadd(year, -3, getdate())
 	    AND INNA.[Status] IN (13,12,11,10)
-	    AND INN.CompetentAuthority IN (select CompetentAuthority from [Person].[InternalUser] where UserId = @UserID)
+	    AND INN.CompetentAuthority IN (SELECT CompetentAuthority FROM [Person].[InternalUser] WHERE UserId = @UserID)
 
-    ORDER by CreatedDate Asc
+    ORDER by [OrderByDate] ASC
     OFFSET (@Skip) ROWS FETCH NEXT (@Take) ROWS ONLY
 
 END
