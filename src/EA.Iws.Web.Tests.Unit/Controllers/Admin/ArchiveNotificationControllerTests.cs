d0496f3c552835e5607363c068158a128a34a845
CIVICA\Jonathan.Rush <jonathan.rush@civica.co.uk>
2023-02-08 15:56:46 +0000

    Fixing failing unit tests

--- src/EA.Iws.Web.Tests.Unit/Controllers/Admin/ArchiveNotificationControllerTests.cs
+++ src/EA.Iws.Web.Tests.Unit/Controllers/Admin/ArchiveNotificationControllerTests.cs
@@ -106,9 +106,9 @@
                 PageNumber = 1,
                 PageSize = 1,
                 SelectedNotifications = sampleNotifications
-            }) as ViewResult;
+            }) as RedirectToRouteResult;
 
-            Assert.True(res.ViewName == "Review");
+            Assert.True(res.RouteValues.ContainsValue("Review"));
         }
 
         [Fact]
@@ -134,10 +134,9 @@
         public async void Post_Archive()
         {
             controller.SelectAllNotifications(sampleNotifications, true);
-            var result = await controller.Archive() as ViewResult;
-            var model = result.Model as ArchiveNotificationArchivedViewModel;
+            var result = await controller.Archive() as RedirectToRouteResult;
 
-            Assert.True(result.ViewName == "Archived");
+            Assert.True(result.RouteValues.ContainsValue("Archived"));
         }
 
         private int HowManyNotificationsSelectedInHttpSession()
