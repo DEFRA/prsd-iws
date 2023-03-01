267e989c291d8640f2defef82c318743332bce99
CIVICA\Jonathan.Rush <jonathan.rush@civica.co.uk>
2023-03-01 17:06:46 +0000

    Corrected the dates

--- src/EA.Iws.Database/scripts/Everytime/03-StoredProcedures/0014-uspGetArchiveNotificationsByUser.sql
+++ src/EA.Iws.Database/scripts/Everytime/03-StoredProcedures/0014-uspGetArchiveNotificationsByUser.sql
@@ -18,7 +18,6 @@ BEGIN
 			WHEN Na.[Status] = 9 THEN CONVERT(VARCHAR, ND.ObjectedDate, 103)
 			WHEN Na.[Status] = 11 THEN CONVERT(VARCHAR, ND.ConsentWithdrawnDate, 103)
 			WHEN Na.[Status] = 14 THEN CONVERT(VARCHAR, ND.FileClosedDate, 103)
-			ELSE CONVERT(VARCHAR, N.CreatedDate, 103) 
 		END AS [DateActioned],
         E.[Name] AS CompanyName,
 	    N.CompetentAuthority
@@ -34,7 +33,6 @@ BEGIN
 			WHEN Na.[Status] = 9 THEN ND.ObjectedDate
 			WHEN Na.[Status] = 11 THEN ND.ConsentWithdrawnDate
 			WHEN Na.[Status] = 14 THEN ND.FileClosedDate
-			ELSE N.CreatedDate
 		END < dateadd(year, -3, getdate())
 	    AND NA.[Status] IN (8,9,11,14)
 	    AND N.CompetentAuthority IN (SELECT CompetentAuthority FROM [Person].[InternalUser] WHERE UserId = @UserID)
@@ -45,11 +43,9 @@ BEGIN
 	    LINS.[Description] AS [Status],
 		CASE 
 			WHEN INNA.[Status] = 10 THEN CONVERT(VARCHAR, INND.ConsentWithdrawnDate, 103)
-			WHEN INNA.[Status] = 12 THEN CONVERT(VARCHAR, INND.WithdrawnDate, 103)
+			WHEN INNA.[Status] = 11 THEN CONVERT(VARCHAR, W.[Date], 103)
+			WHEN INNA.[Status] = 12 THEN CONVERT(VARCHAR, O.[Date], 103)
 			WHEN INNA.[Status] = 13 THEN CONVERT(VARCHAR, INND.FileClosedDate, 103)
-			--We don't have a column in the NotificationDates that matches the Status 11, 
-			--and this doesn't seem to be a valid state for importnotifications, so ignoring
-			ELSE CONVERT(VARCHAR, INND.NotificationReceivedDate, 103) 
 		END AS [DateActioned],
 	    INE.[Name] AS CompanyName,
 	    INN.CompetentAuthority
@@ -58,12 +54,14 @@ BEGIN
 	    INNER JOIN [ImportNotification].[NotificationDates] INND ON INND.NotificationAssessmentId = INNA.Id
 		INNER JOIN [Lookup].ImportNotificationStatus LINS ON LINS.Id = INNA.[Status]
 	    LEFT JOIN [ImportNotification].[Exporter] INE ON INN.Id = INE.ImportNotificationId
+		LEFT JOIN [ImportNotification].[Withdrawn] W ON W.NotificationId = INN.Id
+		LEFT JOIN [ImportNotification].[Objection] O ON O.NotificationId = INN.Id
     WHERE INN.IsArchived = 0 
 		AND CASE 
 			WHEN INNA.[Status] = 10 THEN INND.ConsentWithdrawnDate
-			WHEN INNA.[Status] = 12 THEN INND.WithdrawnDate
+			WHEN INNA.[Status] = 11 THEN W.[Date]
+			WHEN INNA.[Status] = 12 THEN O.[Date]
 			WHEN INNA.[Status] = 13 THEN INND.FileClosedDate
-			ELSE INND.NotificationReceivedDate
 		END < dateadd(year, -3, getdate())
 	    AND INNA.[Status] IN (10,11,12,13)
 	    AND INN.CompetentAuthority IN (SELECT CompetentAuthority FROM [Person].[InternalUser] WHERE UserId = @UserID)
