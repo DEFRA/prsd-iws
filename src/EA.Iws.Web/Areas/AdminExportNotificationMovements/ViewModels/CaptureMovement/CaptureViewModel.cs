85bdeb5d34ad7e3b60a1833368680d8618d4cb5d
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2023-04-11 11:41:52 +0100

    Implemented the P:38027 - Adding the validation for Received Date & Actual Shipment Date changes

--- src/EA.Iws.Web/Areas/AdminExportNotificationMovements/ViewModels/CaptureMovement/CaptureViewModel.cs
+++ src/EA.Iws.Web/Areas/AdminExportNotificationMovements/ViewModels/CaptureMovement/CaptureViewModel.cs
@@ -174,6 +174,15 @@
                 {
                     yield return new ValidationResult(CaptureViewModelResources.ActualDateGreaterthanSixtyDays, new[] { "ActualShipmentDate" });
                 }
+                if (preNotificateDate > ActualShipmentDate.Date)
+                {
+                    yield return new ValidationResult(CaptureViewModelResources.ActualDateBeforePrenotification, new[] { "ActualShipmentDate" });
+                }
+            }
+
+            if (ActualShipmentDate.IsCompleted && Receipt.ReceivedDate.IsCompleted && ActualShipmentDate.Date < Receipt.ReceivedDate.Date)
+            {
+                yield return new ValidationResult(CaptureViewModelResources.ReceivedDateBeforeActualDate, new[] { "ActualShipmentDate" });
             }
 
             if (Receipt.ShipmentTypes == ShipmentType.Accepted)
