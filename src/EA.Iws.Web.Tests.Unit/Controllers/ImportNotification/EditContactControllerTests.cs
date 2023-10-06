16b7c940e27d7727b1ad484c36ae6f6266270eea
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2023-10-06 11:56:50 +0100

    Added more unit tests to the edit contact controller

--- src/EA.Iws.Web.Tests.Unit/Controllers/ImportNotification/EditContactControllerTests.cs
+++ src/EA.Iws.Web.Tests.Unit/Controllers/ImportNotification/EditContactControllerTests.cs
@@ -1,14 +1,15 @@
 ﻿namespace EA.Iws.Web.Tests.Unit.Controllers.ImportNotification
 {
     using System;
-    using System.Collections.Generic;
     using System.Threading.Tasks;
     using System.Web.Mvc;
     using Areas.ImportNotification.Controllers;
     using EA.Iws.Core.ImportNotification.Summary;
     using EA.Iws.Core.Shared;
     using EA.Iws.Requests.ImportNotification.Exporters;
-    using EA.Iws.Requests.Shared;
+    using EA.Iws.Requests.ImportNotification.Facilities;
+    using EA.Iws.Requests.ImportNotification.Importers;
+    using EA.Iws.Requests.ImportNotification.Producers;
     using EA.Iws.Web.Areas.ImportNotification.ViewModels.EditContact;
     using FakeItEasy;
     using Prsd.Core.Mediator;
@@ -29,54 +30,208 @@
         [Fact]
         public async Task Export_ReturnsView()
         {
-            A.CallTo(() => mediator.SendAsync(A<GetCountries>._)).Returns(new List<CountryData>
-            {
-                new CountryData
-                {
-                    Id = new Guid("4345FB05-F7DF-4E16-939C-C09FCA5C7D7B"),
-                    Name = "United Kingdom"
-                },
-                new CountryData
-                {
-                    Id = new Guid("29B0D09E-BA77-49FB-AF95-4171408C07C9"),
-                    Name = "Germany"
-                }
-            });
-
-            A.CallTo(() => mediator.SendAsync(A<GetExporterByImportNotificationId>._)).Returns(CreateValidExporterEditContact());
+            A.CallTo(() => mediator.SendAsync(A<GetExporterByImportNotificationId>._)).Returns((Exporter)CreateValidEditContact("Exporter"));
             editContactController = new EditContactController(mediator);
 
-            var model = ConvertToEditContactViewModel(CreateValidExporterEditContact());
+            var model = ConvertToEditContactViewModel(CreateValidEditContact("Exporter"));
             var result = await editContactController.Exporter(importNotificationId) as ViewResult;
+
             Assert.Equal(string.Empty, result.ViewName);
         }
 
-        private Exporter CreateValidExporterEditContact()
+        [Fact]
+        public async Task Exporter_ValidModel_RedirectsTo_NotificationOverview_Screen()
         {
-            return new Exporter
-            {
-                Address = new Address
-                {
-                    AddressLine2 = "address2",
-                    AddressLine1 = "address1",
-                    Country = "United Kingdom",
-                    PostalCode = "postcode",
-                    TownOrCity = "town"
-                },
-                Contact = new Contact
-                {
-                    Email = "email@address.com",
-                    Name = "first last",
-                    Telephone = "123",
-                    TelephonePrefix = "44"
-                },
-                BusinessType = BusinessType.SoleTrader,
-                Name = "Test Name",
-                RegistrationNumber = "4345435"
-            };
+            A.CallTo(() => mediator.SendAsync(A<GetExporterByImportNotificationId>._)).Returns((Exporter)CreateValidEditContact("Exporter"));
+            editContactController = new EditContactController(mediator);
+
+            var model = CreateValidEditContact("Exporter");
+            var editContactModel = ConvertToEditContactViewModel(model);
+            var result = await editContactController.Exporter(importNotificationId, editContactModel) as RedirectToRouteResult;
+
+            Assert.Equal("Index", result.RouteValues["action"]);
+            Assert.Equal("Home", result.RouteValues["controller"]);
         }
 
-        private EditContactViewModel ConvertToEditContactViewModel(Exporter data)
+        [Fact]
+        public async Task Producer_ReturnsView()
+        {
+            A.CallTo(() => mediator.SendAsync(A<GetProducerByImportNotificationId>._)).Returns((Producer)CreateValidEditContact("Producer"));
+            editContactController = new EditContactController(mediator);
+
+            var model = ConvertToEditContactViewModel(CreateValidEditContact("Producer"));
+            var result = await editContactController.Producer(importNotificationId) as ViewResult;
+
+            Assert.Equal(string.Empty, result.ViewName);
+        }
+
+        [Fact]
+        public async Task Producer_ValidModel_RedirectsTo_NotificationOverview_Screen()
+        {
+            A.CallTo(() => mediator.SendAsync(A<GetProducerByImportNotificationId>._)).Returns((Producer)CreateValidEditContact("Producer"));
+            editContactController = new EditContactController(mediator);
+
+            var model = CreateValidEditContact("Producer");
+            var editContactModel = ConvertToEditContactViewModel(model);
+            var result = await editContactController.Producer(importNotificationId, editContactModel) as RedirectToRouteResult;
+
+            Assert.Equal("Index", result.RouteValues["action"]);
+            Assert.Equal("Home", result.RouteValues["controller"]);
+        }
+
+        [Fact]
+        public async Task Importer_ReturnsView()
+        {
+            A.CallTo(() => mediator.SendAsync(A<GetImporterByImportNotificationId>._)).Returns((Importer)CreateValidEditContact("Importer"));
+            editContactController = new EditContactController(mediator);
+
+            var model = ConvertToEditContactViewModel(CreateValidEditContact("Importer"));
+            var result = await editContactController.Importer(importNotificationId) as ViewResult;
+
+            Assert.Equal(string.Empty, result.ViewName);
+        }
+
+        [Fact]
+        public async Task Importer_ValidModel_RedirectsTo_NotificationOverview_Screen()
+        {
+            A.CallTo(() => mediator.SendAsync(A<GetImporterByImportNotificationId>._)).Returns((Importer)CreateValidEditContact("Importer"));
+            editContactController = new EditContactController(mediator);
+
+            var model = CreateValidEditContact("Importer");
+            var editContactModel = ConvertToEditContactViewModel(model);
+            var result = await editContactController.Importer(importNotificationId, editContactModel) as RedirectToRouteResult;
+
+            Assert.Equal("Index", result.RouteValues["action"]);
+            Assert.Equal("Home", result.RouteValues["controller"]);
+        }
+
+        [Fact]
+        public async Task Facility_ReturnsView()
+        {
+            A.CallTo(() => mediator.SendAsync(A<GetFacilityByImportNotificationId>._)).Returns((Facility)CreateValidEditContact("Facility"));
+            editContactController = new EditContactController(mediator);
+
+            var model = ConvertToEditContactViewModel(CreateValidEditContact("Facility"));
+            var result = await editContactController.Facility(importNotificationId) as ViewResult;
+
+            Assert.Equal(string.Empty, result.ViewName);
+        }
+
+        [Fact]
+        public async Task Facility_ValidModel_RedirectsTo_NotificationOverview_Screen()
+        {
+            A.CallTo(() => mediator.SendAsync(A<GetFacilityByImportNotificationId>._)).Returns((Facility)CreateValidEditContact("Facility"));
+            editContactController = new EditContactController(mediator);
+
+            var model = CreateValidEditContact("Facility");
+            var editContactModel = ConvertToEditContactViewModel(model);
+            var result = await editContactController.Facility(importNotificationId, editContactModel) as RedirectToRouteResult;
+
+            Assert.Equal("Index", result.RouteValues["action"]);
+            Assert.Equal("Home", result.RouteValues["controller"]);
+        }
+
+        private dynamic CreateValidEditContact(string type)
+        {
+            if (type != null && type == "Exporter")
+            {
+                return new Exporter
+                {
+                    Address = new Address
+                    {
+                        AddressLine1 = "address1",
+                        AddressLine2 = "address2",                        
+                        Country = "United Kingdom",
+                        PostalCode = "postcode",
+                        TownOrCity = "town"
+                    },
+                    Contact = new Contact
+                    {
+                        Email = "email@address.com",
+                        Name = "first last",
+                        Telephone = "7448826833",
+                        TelephonePrefix = "44"
+                    },
+                    BusinessType = BusinessType.SoleTrader,
+                    Name = "Test Name",
+                    RegistrationNumber = "4345435"
+                };
+            }
+            else if (type != null && type == "Producer")
+            {
+                return new Producer
+                {
+                    Address = new Address
+                    {
+                        AddressLine1 = "address1",
+                        AddressLine2 = "address2",
+                        Country = "United Kingdom",
+                        PostalCode = "postcode",
+                        TownOrCity = "town"
+                    },
+                    Contact = new Contact
+                    {
+                        Email = "email@address.com",
+                        Name = "first last",
+                        Telephone = "7448826833",
+                        TelephonePrefix = "44"
+                    },
+                    BusinessType = BusinessType.SoleTrader,
+                    Name = "Test Name",
+                    RegistrationNumber = "4345435"
+                };
+            }
+            else if (type != null && type == "Importer")
+            {
+                return new Importer
+                {
+                    Address = new Address
+                    {
+                        AddressLine1 = "address1",
+                        AddressLine2 = "address2",
+                        Country = "United Kingdom",
+                        PostalCode = "postcode",
+                        TownOrCity = "town"
+                    },
+                    Contact = new Contact
+                    {
+                        Email = "email@address.com",
+                        Name = "first last",
+                        Telephone = "7448826833",
+                        TelephonePrefix = "44"
+                    },
+                    BusinessType = BusinessType.SoleTrader,
+                    Name = "Test Name",
+                    RegistrationNumber = "4345435"
+                };
+            }
+            else
+            {
+                return new Facility
+                {
+                    Address = new Address
+                    {
+                        AddressLine1 = "address1",
+                        AddressLine2 = "address2",
+                        Country = "United Kingdom",
+                        PostalCode = "postcode",
+                        TownOrCity = "town"
+                    },
+                    Contact = new Contact
+                    {
+                        Email = "email@address.com",
+                        Name = "first last",
+                        Telephone = "7448826833",
+                        TelephonePrefix = "44"
+                    },
+                    BusinessType = BusinessType.SoleTrader,
+                    Name = "Test Name",
+                    RegistrationNumber = "4345435"
+                };
+            }
+        }
+
+        private EditContactViewModel ConvertToEditContactViewModel(dynamic data)
         {
             return new EditContactViewModel()
             {
