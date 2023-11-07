22153d4f4b4a4fb525d7733ea1bdbdc7a664b4fc
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2023-11-07 16:16:42 +0000

    Worked on Import notification extra waste changes.

---
+++ src/EA.Iws.Database/scripts/Update/0007-Support/20231107-ImportNotification-Extra-Waste-Category-Changes.sql
@@ -0,0 +1,33 @@
+﻿ALTER TABLE [ImportNotification].[WasteType] ADD WasteCategoryType INT NULL;
+GO
+
+ALTER TABLE [ImportNotification].[WasteType]
+ADD CONSTRAINT FK_WasteCategoryType
+FOREIGN KEY (WasteCategoryType) REFERENCES [Lookup].[WasteCategoryType](Id)
+GO
+
+CREATE TABLE [ImportNotification].[WasteComponentInfo](
+	[Id] [uniqueidentifier]				NOT NULL,
+	[RowVersion] [timestamp]			NOT NULL,
+	[WasteComponentType] [int]			NOT NULL,	
+	[NotificationId] [uniqueidentifier] NOT NULL,
+ CONSTRAINT [PK_WasteComponentInfo] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [ImportNotification].[WasteComponentInfo]  WITH CHECK ADD  CONSTRAINT [FK_NotificationWasteComponentInfo_WasteComponentType] FOREIGN KEY([WasteComponentType])
+REFERENCES [Lookup].[WasteComponentType] ([Id])
+GO
+
+ALTER TABLE [ImportNotification].[WasteComponentInfo] CHECK CONSTRAINT [FK_NotificationWasteComponentInfo_WasteComponentType]
+GO
+
+ALTER TABLE [ImportNotification].[WasteComponentInfo]  WITH CHECK ADD  CONSTRAINT [FK_WasteComponentInfo_Notification] FOREIGN KEY([NotificationId])
+REFERENCES [ImportNotification].[Notification] ([Id])
+GO
+
+ALTER TABLE [ImportNotification].[WasteComponentInfo] CHECK CONSTRAINT [FK_WasteComponentInfo_Notification]
+GO
