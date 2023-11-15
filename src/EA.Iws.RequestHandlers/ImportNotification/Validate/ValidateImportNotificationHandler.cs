044f4c603d0f11fb84c11d0b9c4efdfe05c88953
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2023-11-15 10:54:20 +0000

    Added validation condition to avoid apart from the 'Other' chemical composition.

--- src/EA.Iws.RequestHandlers/ImportNotification/Validate/ValidateImportNotificationHandler.cs
+++ src/EA.Iws.RequestHandlers/ImportNotification/Validate/ValidateImportNotificationHandler.cs
@@ -3,6 +3,7 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using DataAccess.Draft;
+    using EA.Iws.Core.WasteType;
     using Prsd.Core.Mediator;
     using Requests.ImportNotification.Validate;
     using Validation;
@@ -37,7 +38,11 @@
             result.Add(await validator.ValidateAsync(notificationDraft.StateOfExport));
             result.Add(await validator.ValidateAsync(notificationDraft.StateOfImport));
             result.Add(await validator.ValidateAsync(notificationDraft.TransitStates));
-            result.Add(await validator.ValidateAsync(notificationDraft.WasteCategories));
+
+            if (notificationDraft.ChemicalComposition.Composition == ChemicalComposition.Other)
+            {
+                result.Add(await validator.ValidateAsync(notificationDraft.WasteCategories));
+            }
 
             return result;
         }
