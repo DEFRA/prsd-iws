d2f502cf3dad51e7a795d160b84053cab5df2f59
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2024-02-14 16:28:10 +0000

    Removed unwanted file

--- src/EA.Iws.Core/Notification/AdditionalCharge/NotificationAdditionalChargeTable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EA.Iws.Core.Notification.AdditionalCharge
-{
-    using System.Collections.Generic;
-
-    internal class NotificationAdditionalChargeTable
-    {
-        public List<NotificationAdditionalChargeForDisplay> TableData { get; set; }
-    }
-}
