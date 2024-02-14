ba42c6e905c1463fed175754b2106317a0fb7ac1
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2024-02-14 15:59:40 +0000

    Implemented Finance - Additional Charges changes

---
+++ src/EA.Iws.DataAccess/Repositories/NotificationAdditionalChargeRepository.cs
@@ -0,0 +1,31 @@
+﻿namespace EA.Iws.DataAccess.Repositories
+{
+    using EA.Iws.Domain.NotificationApplication;
+    using EA.Iws.Domain.Security;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    internal class NotificationAdditionalChargeRepository : INotificationAdditionalChargeRepository
+    {
+        private readonly IwsContext context;
+        private readonly INotificationApplicationAuthorization notificationApplicationAuthorization;
+
+        public NotificationAdditionalChargeRepository(IwsContext context, INotificationApplicationAuthorization notificationApplicationAuthorization)
+        {
+            this.context = context;
+            this.notificationApplicationAuthorization = notificationApplicationAuthorization;
+        }
+
+        public async Task<IEnumerable<AdditionalCharge>> GetPagedNotificationAdditionalChargesById(Guid notificationId)
+        {
+            await notificationApplicationAuthorization.EnsureAccessAsync(notificationId);
+
+            return await context.AdditionalCharges.Where(p => p.NotificationId == notificationId)
+                                                       .OrderByDescending(x => x.ChargeDate)
+                                                       .ToListAsync();
+        }
+    }
+}
