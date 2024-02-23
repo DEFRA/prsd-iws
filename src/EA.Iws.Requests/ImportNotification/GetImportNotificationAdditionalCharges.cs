e4b263d0cb635bf59814b5e74399eccbba1eab8d
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2024-02-23 13:48:55 +0000

    Added authorization to the request

--- src/EA.Iws.Requests/ImportNotification/GetImportNotificationAdditionalCharges.cs
+++ src/EA.Iws.Requests/ImportNotification/GetImportNotificationAdditionalCharges.cs
@@ -1,10 +1,13 @@
 ﻿namespace EA.Iws.Requests.ImportNotification
 {
+    using EA.Iws.Core.Authorization;
+    using EA.Iws.Core.Authorization.Permissions;
     using EA.Iws.Core.ImportNotification.AdditionalCharge;
     using EA.Prsd.Core.Mediator;
     using System;
     using System.Collections.Generic;
 
+    [RequestAuthorization(ImportNotificationPermissions.CanReadImportNotification)]
     public class GetImportNotificationAdditionalCharges : IRequest<IList<AdditionalChargeForDisplay>>
     {
         public GetImportNotificationAdditionalCharges(Guid notificationId)
