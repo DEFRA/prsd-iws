447c199f0e7df7b86fa75c69cc8ac0eafe5ef7f9
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2024-02-23 12:50:30 +0000

    Finance tab additional charges changes implemented.

---
+++ src/EA.Iws.DataAccess/Repositories/Imports/ImportNotificationAdditionalChargeRepository.cs
@@ -0,0 +1,40 @@
+﻿namespace EA.Iws.DataAccess.Repositories.Imports
+{
+    using EA.Iws.Domain.ImportNotification;
+    using EA.Iws.Domain.Security;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    internal class ImportNotificationAdditionalChargeRepository : IImportNotificationAdditionalChargeRepository
+    {
+        private readonly ImportNotificationContext context;
+        private readonly IImportNotificationApplicationAuthorization importNotificationApplicationAuthorization;
+
+        public ImportNotificationAdditionalChargeRepository(ImportNotificationContext context, IImportNotificationApplicationAuthorization importNotificationApplicationAuthorization)
+        {
+            this.context = context;
+            this.importNotificationApplicationAuthorization = importNotificationApplicationAuthorization;
+        }
+
+        public async Task<IEnumerable<AdditionalCharge>> GetImportNotificationAdditionalChargesById(Guid notificationId)
+        {
+            await importNotificationApplicationAuthorization.EnsureAccessAsync(notificationId);
+
+            return await context.AdditionalCharges.Where(p => p.NotificationId == notificationId)
+                                                       .OrderByDescending(x => x.ChargeDate)
+                                                       .ToListAsync();
+        }
+
+        public async Task AddImportNotificationAdditionalCharge(AdditionalCharge additionalCharge)
+        {
+            await importNotificationApplicationAuthorization.EnsureAccessAsync(additionalCharge.NotificationId);
+
+            context.AdditionalCharges.Add(additionalCharge);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/EA.Iws.Requests/ImportNotification/GetImportNotificationAdditionalCharges.cs
@@ -0,0 +1,17 @@
+﻿namespace EA.Iws.Requests.ImportNotification
+{
+    using EA.Iws.Core.ImportNotification.AdditionalCharge;
+    using EA.Prsd.Core.Mediator;
+    using System;
+    using System.Collections.Generic;
+
+    public class GetImportNotificationAdditionalCharges : IRequest<IList<AdditionalChargeForDisplay>>
+    {
+        public GetImportNotificationAdditionalCharges(Guid notificationId)
+        {
+            NotificationId = notificationId;
+        }
+
+        public Guid NotificationId { get; private set; }
+    }
+}
