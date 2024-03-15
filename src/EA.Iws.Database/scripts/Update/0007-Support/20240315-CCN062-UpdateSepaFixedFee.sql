03e8d5ca3b5fbd7b0c09f8b3f96127f1e01f9d66
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2024-03-15 11:11:13 +0000

    SEPA fixed fee changes

--- src/EA.Iws.Database/scripts/Update/0007-Support/20240315-CCN062-UpdateSepaFixedFee.sql
+++ src/EA.Iws.Database/scripts/Update/0007-Support/20240315-CCN062-UpdateSepaFixedFee.sql
@@ -1,2 +1,11 @@
-﻿UPDATE [Lookup].[PricingFixedFee] Set [Price]=9884 where Id='D39271D5-381C-43C0-A137-A190DB98F1AF' and CompetentAuthority=2 and WasteCategoryTypeId=11;
-UPDATE [Lookup].[PricingFixedFee] Set [Price]=9884 where Id='AA5D920C-2E15-4EA1-A68A-401DDD50D3CF' and CompetentAuthority=2 and WasteCategoryTypeId=12;
+﻿--SEPA Single ship price update
+UPDATE [Lookup].[PricingFixedFee] SET [Price]=9884 WHERE Id='D39271D5-381C-43C0-A137-A190DB98F1AF' AND CompetentAuthority=2 AND WasteCategoryTypeId=11;
+
+--SEPA Platform/rig price update
+UPDATE [Lookup].[PricingFixedFee] SET [Price]=9884 WHERE Id='AA5D920C-2E15-4EA1-A68A-401DDD50D3CF' AND CompetentAuthority=2 AND WasteCategoryTypeId=12;
+
+--SEPA Additional Charge per shipment for not self entering data price update
+UPDATE [Lookup].[SystemSettings] SET [Value]=27 WHERE Id=3;
+
+--SEPA fixed additional charge for each data changes price update
+UPDATE [Lookup].[SystemSettings] SET [Value]=190 WHERE Id=6;
