b9e072350642efc438188a85b749bcc7d41a1a40
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2024-07-19 12:56:34 +0100

    Import notifucation Shipment increase resubmit changes implemented.

--- src/EA.Iws.Database/scripts/Update/0007-Support/20240711-CCN067-Notification-Resubmitted-changes.sql
+++ src/EA.Iws.Database/scripts/Update/0007-Support/20240711-CCN067-Notification-Resubmitted-changes.sql
@@ -1,1 +1,2 @@
 ﻿INSERT INTO[Lookup].[NotificationStatus] ([Id], [Description]) VALUES(16, 'Resubmitted');
+INSERT INTO[Lookup].[ImportNotificationStatus] ([Id], [Description]) VALUES(14, 'Resubmitted');
