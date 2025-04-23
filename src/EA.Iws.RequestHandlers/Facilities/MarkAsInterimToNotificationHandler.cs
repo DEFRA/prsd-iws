e5cbffaade2ea5c317b221bddf66b86408f1fc87
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2025-04-23 15:10:05 +0100

    Export notification interim changes initial commit

---
+++ src/EA.Iws.RequestHandlers/Facilities/MarkAsInterimToNotificationHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace EA.Iws.RequestHandlers.Facilities
+{
+    using EA.Iws.DataAccess;
+    using EA.Iws.Domain.NotificationAssessment;
+    using EA.Iws.Requests.Facilities;
+    using EA.Prsd.Core.Mediator;
+    using System.Threading.Tasks;
+
+    internal class MarkAsInterimToNotificationHandler : IRequestHandler<MarkAsInterimToNotification, bool>
+    {
+        private readonly NotificationInterim notificationInterim;
+        private readonly IwsContext context;
+
+        public MarkAsInterimToNotificationHandler(NotificationInterim notificationInterim, IwsContext context)
+        {
+            this.notificationInterim = notificationInterim;
+            this.context = context;
+        }
+
+        public async Task<bool> HandleAsync(MarkAsInterimToNotification message)
+        {
+            await notificationInterim.SetValue(message.NotificationId, message.IsInterim);
+
+            await context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
