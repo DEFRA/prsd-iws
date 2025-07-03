004238e04fa534676b5d1180b1ba969d13b79231
CIVICA\sreedhar.bangarugari <sreedhar.bangarugari@civica.co.uk>
2025-07-03 11:09:01 +0100

    Added RequestAuthorization to GetSystemSettings.

--- src/EA.Iws.Requests/SystemSettings/GetSystemSettings.cs
+++ src/EA.Iws.Requests/SystemSettings/GetSystemSettings.cs
@@ -1,10 +1,13 @@
 ﻿namespace EA.Iws.Requests.SystemSettings
 {
+    using EA.Iws.Core.Authorization;
+    using EA.Iws.Core.Authorization.Permissions;
     using EA.Iws.Core.Notification;
     using EA.Iws.Core.SystemSetting;
     using EA.Iws.Core.SystemSettings;
     using EA.Prsd.Core.Mediator;
 
+    [RequestAuthorization(GeneralPermissions.CanViewSearchResults)]
     public class GetSystemSettings : IRequest<SystemSettingData>
     {
         public GetSystemSettings(UKCompetentAuthority competentAuthority, SystemSettingType priceTypeId)
