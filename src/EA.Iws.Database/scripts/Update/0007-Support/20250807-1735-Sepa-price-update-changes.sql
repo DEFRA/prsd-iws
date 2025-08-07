62c74fb145b3c4b15f912b2578f16b135324a825
SFWVDI13\SFW_SB <sreedhar.bangarugari@civica.co.uk>
2025-08-07 16:43:24 +0000

    Added Single Ship price for the SEPA.

---
+++ src/EA.Iws.Database/scripts/Update/0007-Support/20250807-1735-Sepa-price-update-changes.sql
@@ -0,0 +1,5 @@
+﻿--11 = Single ship
+INSERT INTO [Lookup].[PricingFixedFee] ([Id], [CompetentAuthority], [WasteComponentTypeId], [WasteCategoryTypeId], [Price], [ValidFrom])
+	VALUES ('43A335BC-2618-4D7B-81EE-9B3CE7741CB6', @CompetentAuthority, null, 11, 10151, @validFrom);
+
+UPDATE [Lookup].[CompetentAuthority] SET [Name] ='Scottish Environment Protection Agency' WHERE Abbreviation='SEPA' AND Code='GB02';
