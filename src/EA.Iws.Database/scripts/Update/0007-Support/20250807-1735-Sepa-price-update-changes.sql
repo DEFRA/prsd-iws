5006ea9c99ed180cd0d939c128935a039a025171
SFWVDI13\SFW_SB <sreedhar.bangarugari@civica.co.uk>
2025-08-07 16:52:01 +0000

    SEPA Price script updated

--- src/EA.Iws.Database/scripts/Update/0007-Support/20250807-1735-Sepa-price-update-changes.sql
+++ src/EA.Iws.Database/scripts/Update/0007-Support/20250807-1735-Sepa-price-update-changes.sql
@@ -1,5 +1,10 @@
-﻿--11 = Single ship
+﻿DECLARE @ValidFrom DATE;
+DECLARE @CompetentAuthority INT;
+SET @ValidFrom = '2025-04-01';
+SET @CompetentAuthority = 2; --SEPA(Scotland)
+
+--11 = Single ship
 INSERT INTO [Lookup].[PricingFixedFee] ([Id], [CompetentAuthority], [WasteComponentTypeId], [WasteCategoryTypeId], [Price], [ValidFrom])
-	VALUES ('43A335BC-2618-4D7B-81EE-9B3CE7741CB6', @CompetentAuthority, null, 11, 10151, @validFrom);
+	VALUES ('43A335BC-2618-4D7B-81EE-9B3CE7741CB6', @CompetentAuthority, null, 11, 10151, @ValidFrom);
 
 UPDATE [Lookup].[CompetentAuthority] SET [Name] ='Scottish Environment Protection Agency' WHERE Abbreviation='SEPA' AND Code='GB02';
