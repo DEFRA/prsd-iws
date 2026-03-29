59ef498d30a33b17860f60ac0de5fcd1e8eed77c
SFWVDI13\SFW_SB <sreedhar.bangarugari@civica.co.uk>
2026-03-29 18:50:26 +0100

    Removed unwanted code

--- src/EA.Iws.RequestHandlers/Admin/Reports/GetEADataReportHandler.cs
+++ src/EA.Iws.RequestHandlers/Admin/Reports/GetEADataReportHandler.cs
@@ -4,7 +4,6 @@
     using EA.Iws.Core.Notification;
     using EA.Iws.Domain.Reports;
     using EA.Iws.Requests.Admin.Reports;
-    using EA.Prsd.Core.Domain;
     using EA.Prsd.Core.Mapper;
     using Prsd.Core.Mediator;
     using System.Linq;
@@ -19,15 +18,11 @@
         private readonly IProducerRepository producerReportRepository;
         private readonly IFreedomOfInformationRepository foiRepository;
 
-        private readonly IUserContext userContext;
-        private readonly Domain.IInternalUserRepository internalUserRepository;
-
         public GetEADataReportHandler(IShipmentsRepository shipmentsRepository,
             IMapWithParameter<Shipment, UKCompetentAuthority, ShipmentData> shipmentMapper,
             IFinanceReportRepository financeReportRepository,
             IProducerRepository producerReportRepository,
             IFreedomOfInformationRepository foiRepository,
-            IUserContext userContext,
             Domain.IInternalUserRepository internalUserRepository)
         {
             this.shipmentsRepository = shipmentsRepository;
@@ -36,14 +31,10 @@
             this.financeReportRepository = financeReportRepository;
             this.producerReportRepository = producerReportRepository;
             this.foiRepository = foiRepository;
-
-            this.userContext = userContext;
-            this.internalUserRepository = internalUserRepository;
         }
 
         public async Task<EADataReportsData> HandleAsync(GetEADataReport message)
         {
-            var user = await internalUserRepository.GetByUserId(userContext.UserId);
             var shipmentData = await shipmentsRepository.GetEAShipmentData(message.FromDate, message.ToDate);
             var financeData = await financeReportRepository.GetFinanceReport(message.FromDate, message.ToDate);
             var producerData = await producerReportRepository.GetProducerReport(message.FromDate, message.ToDate);
@@ -51,7 +42,7 @@
 
             var reportsData = new EADataReportsData()
             {
-                ShipmentReportData = shipmentData.Select(x => shipmentMapper.Map(x, user.CompetentAuthority)).ToArray(),
+                ShipmentReportData = shipmentData.Select(x => shipmentMapper.Map(x, UKCompetentAuthority.England)).ToArray(),
                 FinanceReportData = financeData.ToArray(),
                 ProducerReportData = producerData.ToArray(),
                 FreedomOfInformationReportData = foiReportData.ToArray()
