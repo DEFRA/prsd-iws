403a1e71b4315b665a3d9256f55c82d4dcc8e240
ianalvaradoCivica <Ian.Alvarado@civica.com>
2026-07-10 14:26:24 +0100

    Update to remove STRING_SPLIT method and replace with XML

--- src/EA.Iws.Database/scripts/Everytime/03-StoredProcedures/0019-uspGetImportWorklist.sql
+++ src/EA.Iws.Database/scripts/Everytime/03-StoredProcedures/0019-uspGetImportWorklist.sql
@@ -6,23 +6,29 @@ ALTER PROCEDURE [ImportNotification].[uspGetImportWorklist]
     @CompetentAuthority INT,
     @NotificationNumber NVARCHAR(50) = NULL,
     @Officer NVARCHAR(256) = NULL,
-    @Statuses NVARCHAR(MAX) = NULL, -- Comma-separated list of status IDs
+    @Statuses NVARCHAR(MAX) = NULL,
     @PageNumber INT = 1,
     @PageSize INT = 20
 AS
 BEGIN
     SET NOCOUNT ON;
 
+    -- Parse statuses using XML (compatible with all SQL Server versions)
     DECLARE @StatusTable TABLE (StatusId INT);
     
     IF @Statuses IS NOT NULL AND LEN(@Statuses) > 0
     BEGIN
+        DECLARE @StatusesXml XML;
+        SET @StatusesXml = CAST('<i>' + REPLACE(@Statuses, ',', '</i><i>') + '</i>' AS XML);
+        
         INSERT INTO @StatusTable (StatusId)
-        SELECT CAST(value AS INT)
-        FROM STRING_SPLIT(@Statuses, ',');
+        SELECT CAST(T.c.value('.', 'INT') AS INT)
+        FROM @StatusesXml.nodes('/i') T(c)
+        WHERE LTRIM(RTRIM(T.c.value('.', 'NVARCHAR(10)'))) <> '';
     END
 
-    DECLARE @Offset INT = (@PageNumber - 1) * @PageSize;
+    DECLARE @Offset INT;
+    SET @Offset = (@PageNumber - 1) * @PageSize;
 
     ;WITH WorklistData AS
     (
